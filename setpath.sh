7e8dd01a192b80741f7478a23c7fec96344cb8f2
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-05-24 17:47:38 +0200

    script to automatically set up executable/python import paths

---
+++ setpath.sh
@@ -0,0 +1,24 @@
+#
+# This script adds Mitsuba to the current path.
+# It works with both Bash and Zsh.
+#
+# NOTE: this script must be sourced and not run, i.e.
+#    . setpath.sh        for Bash
+#    source setpath.sh   for Zsh or Bash
+#
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+	echo "The setpath.sh script must be sourced, not executed. In other words, run\n"
+	echo "$ source setpath.sh\n"
+	exit 0
+fi
+
+if [ "$BASH_VERSION" ]; then
+	MITSUBA_DIR=$(dirname "$BASH_SOURCE")
+	export MITSUBA_DIR=$(builtin cd "$MITSUBA_DIR"; builtin pwd)
+elif [ "$ZSH_VERSION" ]; then
+	export MITSUBA_DIR=$(dirname "$0:A")
+fi
+
+export PYTHONPATH="$MITSUBA_DIR/dist/python"
+export PATH="$MITSUBA_DIR/dist:$PATH"
