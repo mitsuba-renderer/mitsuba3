6130b7682f2f34fe8a9ea9ef17d295000fb28dce
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-10-09 13:21:55 +0200

    libpython: expose Mitsuba compile time float precision as dtype

--- src/libpython/vector.cpp
+++ src/libpython/vector.cpp
@@ -2,5 +2,11 @@
 #include "python.h"
 
 MTS_PY_EXPORT(vector) {
+#if defined(SINGLE_PRECISION)
+    m.attr("float_dtype") = py::dtype("f");
+#else
+    m.attr("float_dtype") = py::dtype("d");
+#endif
+
     m.def("coordinateSystem", &coordinateSystem, DM(coordinateSystem));
 }
