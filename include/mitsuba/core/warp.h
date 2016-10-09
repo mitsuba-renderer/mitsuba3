28321c76814dba5db8048d82bd28528ae959a1cc
Wenzel Jakob <wenzel.jakob@epfl.ch>
2016-10-09 13:22:44 +0200

    reorganization of warping code

--- include/mitsuba/core/warp.h
+++ include/mitsuba/core/warp.h
@@ -4,11 +4,6 @@
 #include <mitsuba/core/frame.h>
 #include <mitsuba/core/math.h>
 #include <mitsuba/core/vector.h>
-#include <Eigen/Core>
-
-namespace {
-  mitsuba::Float kDomainEpsilon = 1e-5;
-}  // end anonymous namespace
 
 NAMESPACE_BEGIN(mitsuba)
 
@@ -21,15 +16,6 @@ NAMESPACE_BEGIN(mitsuba)
  */
 NAMESPACE_BEGIN(warp)
 
-class WarpAdapter;
-
-/// Enum of available point sampling strategies
-enum SamplingType {
-    Independent = 0,
-    Grid,
-    Stratified
-};
-
 // =============================================================
 //! @{ \name Warping techniques related to spheres and subsets
 // =============================================================
@@ -38,32 +24,35 @@ enum SamplingType {
 extern MTS_EXPORT_CORE Vector3f squareToUniformSphere(const Point2f &sample);
 
 /// Density of \ref squareToUniformSphere() with respect to solid angles
-inline MTS_EXPORT_CORE Float squareToUniformSpherePdf(const Vector3f &v) {
-    if (std::abs(v.x() * v.x() + v.y() * v.y() + v.z() * v.z() - 1) < kDomainEpsilon)
-        return math::InvFourPi;
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToUniformSpherePdf(const Vector3f &v) {
+    if (TestDomain && std::abs(simd::squaredNorm(v) - 1.f) > math::Epsilon)
+        return 0.f;
+    return math::InvFourPi;
 }
 
 /// Uniformly sample a vector on the unit hemisphere with respect to solid angles
 extern MTS_EXPORT_CORE Vector3f squareToUniformHemisphere(const Point2f &sample);
 
 /// Density of \ref squareToUniformHemisphere() with respect to solid angles
-inline MTS_EXPORT_CORE Float squareToUniformHemispherePdf(const Vector3f &v) {
-    if (std::abs(v.x() * v.x() + v.y() * v.y() + v.z() * v.z() - 1) < kDomainEpsilon
-        && (v.z() >= 0))
-        return math::InvTwoPi;
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToUniformHemispherePdf(const Vector3f &v) {
+    if (TestDomain && (std::abs(simd::squaredNorm(v) - 1.f) > math::Epsilon ||
+                       Frame::cosTheta(v) < 0))
+        return 0.f;
+    return math::InvTwoPi;
 }
 
 /// Sample a cosine-weighted vector on the unit hemisphere with respect to solid angles
 extern MTS_EXPORT_CORE Vector3f squareToCosineHemisphere(const Point2f &sample);
 
 /// Density of \ref squareToCosineHemisphere() with respect to solid angles
-inline MTS_EXPORT_CORE Float squareToCosineHemispherePdf(const Vector3f &v) {
-    if (std::abs(v.x() * v.x() + v.y() * v.y() + v.z() * v.z() - 1) < kDomainEpsilon
-        && (v.z() >= 0))
-        return math::InvPi * Frame::cosTheta(v);
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToCosineHemispherePdf(const Vector3f &v) {
+    if (TestDomain && (std::abs(simd::squaredNorm(v) - 1.f) > math::Epsilon ||
+                       Frame::cosTheta(v) < 0))
+        return 0.f;
+    return math::InvPi * Frame::cosTheta(v);
 }
 
 /**
@@ -80,12 +69,42 @@ extern MTS_EXPORT_CORE Vector3f squareToUniformCone(const Point2f &sample, Float
  *
  * \param cosCutoff Cosine of the cutoff angle
  */
-inline MTS_EXPORT_CORE Float squareToUniformConePdf(const Vector3f &v, Float cosCutoff) {
-    if (std::abs(v.x() * v.x() + v.y() * v.y() + v.z() * v.z() - 1) < kDomainEpsilon
-        && Frame::cosTheta(v) - cosCutoff >= -kDomainEpsilon)
-        return math::InvTwoPi / (1 - cosCutoff);
-    return 0.0;
+template <bool TestDomain = false>
+inline Float squareToUniformConePdf(const Vector3f &v, Float cosCutoff) {
+    if (TestDomain && (std::abs(simd::squaredNorm(v) - 1.f) > math::Epsilon ||
+                       Frame::cosTheta(v) < cosCutoff))
+        return 0.0;
+    return math::InvTwoPi / (1 - cosCutoff);
 }
+
+/// Warp a uniformly distributed square sample to a Beckmann distribution *
+/// cosine for the given 'alpha' parameter
+extern MTS_EXPORT_CORE Vector3f squareToBeckmann(const Point2f &sample,
+                                                 Float alpha);
+
+/// Probability density of \ref squareToBeckmann()
+extern MTS_EXPORT_CORE Float squareToBeckmannPdf(const Vector3f &m,
+                                                 Float alpha);
+
+/// Warp a uniformly distributed square sample to a von Mises Fisher distribution
+extern MTS_EXPORT_CORE Vector3f squareToVonMisesFisher(const Point2f &sample,
+                                                       Float kappa);
+
+/// Probability density of \ref squareToVonMisesFisher()
+extern MTS_EXPORT_CORE Float squareToVonMisesFisherPdf(const Vector3f &v,
+                                                       Float kappa);
+
+/// Warp a uniformly distributed square sample to a rough fiber distribution
+extern MTS_EXPORT_CORE Vector3f squareToRoughFiber(const Point3f &sample,
+                                                   const Vector3f &wi,
+                                                   const Vector3f &tangent,
+                                                   Float kappa);
+
+/// Probability density of \ref squareToRoughFiber()
+extern MTS_EXPORT_CORE Float squareToRoughFiberPdf(const Vector3f &v,
+                                                   const Vector3f &wi,
+                                                   const Vector3f &tangent,
+                                                   Float kappa);
 
 //! @}
 // =============================================================
@@ -98,42 +117,36 @@ inline MTS_EXPORT_CORE Float squareToUniformConePdf(const Vector3f &v, Float cos
 extern MTS_EXPORT_CORE Point2f squareToUniformDisk(const Point2f &sample);
 
 /// Density of \ref squareToUniformDisk per unit area
-inline MTS_EXPORT_CORE Float squareToUniformDiskPdf(const Point2f &p) {
-    if (p.x() * p.x() + p[1] * p[1] <= 1)
-        return math::InvPi;
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToUniformDiskPdf(const Point2f &p) {
+    if (TestDomain && simd::squaredNorm(p) > 1)
+        return 0.f;
+    return math::InvPi;
 }
 
 /// Low-distortion concentric square to disk mapping by Peter Shirley (PDF: 1/PI)
 extern MTS_EXPORT_CORE Point2f squareToUniformDiskConcentric(const Point2f &sample);
 
 /// Density of \ref squareToUniformDisk per unit area
-inline MTS_EXPORT_CORE Float squareToUniformDiskConcentricPdf(const Point2f &p) {
-    if (p.x() * p.x() + p[1] * p[1] <= 1)
-        return math::InvPi;
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToUniformDiskConcentricPdf(const Point2f &p) {
+    if (TestDomain && simd::squaredNorm(p) > 1)
+        return 0.f;
+    return math::InvPi;
 }
 
 /// Inverse of the mapping \ref squareToUniformDiskConcentric
-// TODO: proper support for warping functions where starting sample != uniform square (?)
-extern MTS_EXPORT_CORE Point2f uniformDiskToSquareConcentric(const Point2f &p);
-
-inline MTS_EXPORT_CORE Point2f uniformDiskToSquareConcentricPdf(const Point2f &p) {
-    // TODO: verify this is correct
-    if (p.x() >= 0 && p.x() <= 1 && p[1] >= 0 && p[1] <= 1)
-        return 1.0;
-    return 0.0;
-}
+extern MTS_EXPORT_CORE Point2f diskToUniformSquareConcentric(const Point2f &p);
 
 /// Convert an uniformly distributed square sample into barycentric coordinates
 extern MTS_EXPORT_CORE Point2f squareToUniformTriangle(const Point2f &sample);
 
 /// Density of \ref squareToUniformTriangle per unit area.
-inline MTS_EXPORT_CORE Float squareToUniformTrianglePdf(const Point2f &p) {
-    if (p.x() >= 0 && p[1] >= 0 && p.x() <= 1 && p[1] <= 1
-        && p.x() + p[1] <= 1)
-        return 0.5;
-    return 0.0;
+template <bool TestDomain = false>
+Float squareToUniformTrianglePdf(const Point2f &p) {
+    if (TestDomain && (p.x() < 0 || p.y() < 0 || p.x() + p.y() > 1))
+        return 0.f;
+    return 0.5f;
 }
 
 /** \brief Sample a point on a 2D standard normal distribution
@@ -154,47 +167,6 @@ extern MTS_EXPORT_CORE Float squareToTentPdf(const Point2f &p);
  */
 extern MTS_EXPORT_CORE Float intervalToNonuniformTent(Float sample, Float a, Float b, Float c);
 
-//! @}
-// =============================================================
-
-// =============================================================
-//! @{ \name Extra functions related to sampling and testing
-//!    of those warping functions.
-// =============================================================
-NAMESPACE_BEGIN(detail)
-
-/**
- * For a given warping method captured by the passed \p warpAdater, and for
- * a given sampling strategy, runs a Chi^2  statistical test to check that the
- * warping function matches its associated PDF.
- * Also outputs the observed and expected histograms computed for the test.
- *
- * \return Pair (whether the test succeeded, an explanatory text).
- */
-extern MTS_EXPORT_CORE std::pair<bool, std::string>
-runStatisticalTestAndOutput(size_t pointCount, size_t gridWidth, size_t gridHeight,
-    SamplingType samplingType, WarpAdapter *warpAdapter,
-    double minExpFrequency, double significanceLevel,
-    std::vector<double> &observedHistogram, std::vector<double> &expectedHistogram);
-
-/**
- * For a given warping method captured by the passed \p warpAdater, and for
- * a given sampling strategy, runs a Chi^2 statistical test to check that
- * the warping function matches its associated PDF.
- *
- * \return Pair (whether the test succeeded, an explanatory text).
- */
-inline MTS_EXPORT_CORE std::pair<bool, std::string>
-runStatisticalTest(size_t pointCount, size_t gridWidth, size_t gridHeight,
-                   SamplingType samplingType, WarpAdapter *warpAdapter,
-                   double minExpFrequency, double significanceLevel) {
-    std::vector<double> observedHistogram, expectedHistogram;
-    return runStatisticalTestAndOutput(pointCount,
-        gridWidth, gridHeight, samplingType, warpAdapter,
-        minExpFrequency, significanceLevel, observedHistogram, expectedHistogram);
-}
-
-NAMESPACE_END(detail)
 //! @}
 // =============================================================
 
--- src/libcore/warp.cpp
+++ src/libcore/warp.cpp
@@ -1,5 +1,4 @@
 #include <mitsuba/core/warp.h>
-#include <mitsuba/core/warp_adapters.h>
 #include <mitsuba/core/vector.h>
 #include <mitsuba/core/transform.h>
 #include <hypothesis.h>
@@ -9,74 +8,69 @@ NAMESPACE_BEGIN(mitsuba)
 NAMESPACE_BEGIN(warp)
 
 Vector3f squareToUniformSphere(const Point2f &sample) {
-    Float z = 1.0f - 2.0f * sample.y();
-    Float r = math::safe_sqrt(1.0f - z*z);
+    Float z = 1.f - 2.f * sample.y();
+    Float r = math::safe_sqrt(1.f - z*z);
     Float sinPhi, cosPhi;
-    math::sincos(2.0f * math::Pi * sample.x(), &sinPhi, &cosPhi);
+    math::sincos(2.f * math::Pi * sample.x(), &sinPhi, &cosPhi);
     return Vector3f(r * cosPhi, r * sinPhi, z);
 }
 
 Vector3f squareToUniformHemisphere(const Point2f &sample) {
     Float z = sample.x();
-    Float tmp = math::safe_sqrt(1.0f - z*z);
+    Float tmp = math::safe_sqrt(1.f - z*z);
 
     Float sinPhi, cosPhi;
-    math::sincos(2.0f * math::Pi * sample.y(), &sinPhi, &cosPhi);
+    math::sincos(2.f * math::Pi * sample.y(), &sinPhi, &cosPhi);
 
     return Vector3f(cosPhi * tmp, sinPhi * tmp, z);
 }
 
 Vector3f squareToCosineHemisphere(const Point2f &sample) {
     Point2f p = squareToUniformDiskConcentric(sample);
-    Float z = math::safe_sqrt(1.0f - p.x()*p.x() - p.y()*p.y());
-
     /* Guard against numerical imprecisions */
-    if (z == 0)
-        z = 1e-10;
+    Float z = std::max(1e-10f, math::safe_sqrt(1.f - p.x() * p.x() - p.y() * p.y()));
 
     return Vector3f(p.x(), p.y(), z);
 }
 
 Vector3f squareToUniformCone(const Point2f &sample, Float cosCutoff) {
-    Float cosTheta = (1-sample.x()) + sample.x() * cosCutoff;
-    Float sinTheta = math::safe_sqrt(1.0f - cosTheta * cosTheta);
+    Float cosTheta = (1 - sample.x()) + sample.x() * cosCutoff;
+    Float sinTheta = math::safe_sqrt(1.f - cosTheta * cosTheta);
 
     Float sinPhi, cosPhi;
-    math::sincos(2.0f * math::Pi * sample.y(), &sinPhi, &cosPhi);
+    math::sincos(2.f * math::Pi * sample.y(), &sinPhi, &cosPhi);
 
-    return Vector3f(cosPhi * sinTheta,
-                    sinPhi * sinTheta, cosTheta);
+    return Vector3f(cosPhi * sinTheta, sinPhi * sinTheta, cosTheta);
 }
 
 Point2f squareToUniformDisk(const Point2f &sample) {
     Float r = std::sqrt(sample.x());
     Float sinPhi, cosPhi;
-    math::sincos(2.0f * math::Pi * sample.y(), &sinPhi, &cosPhi);
+    math::sincos(2.f * math::Pi * sample.y(), &sinPhi, &cosPhi);
 
-    return Point2f(cosPhi * r,
-                   sinPhi * r);
+    return Point2f(cosPhi * r, sinPhi * r);
 }
 
 Point2f squareToUniformTriangle(const Point2f &sample) {
-    Float a = math::safe_sqrt(1.0f - sample.x());
+    Float a = math::safe_sqrt(1.f - sample.x());
     return Point2f(1 - a, a * sample.y());
 }
 
 Point2f squareToUniformDiskConcentric(const Point2f &sample) {
-    Float r1 = 2.0f*sample.x() - 1.0f;
-    Float r2 = 2.0f*sample.y() - 1.0f;
+    Float r1 = 2.f * sample.x() - 1.f;
+    Float r2 = 2.f * sample.y() - 1.f;
 
     /* Modified concencric map code with less branching (by Dave Cline), see
      * http://psgraphics.blogspot.ch/2011/01/improved-code-for-concentric-map.html */
     Float phi, r;
     if (r1 == 0 && r2 == 0) {
         r = phi = 0;
-    } else if (r1*r1 > r2*r2) {
+    } else if (r1 * r1 > r2 * r2) {
         r = r1;
-        phi = (math::Pi/4.0f) * (r2/r1);
+        phi = (math::Pi / 4.f) * (r2 / r1);
     } else {
         r = r2;
-        phi = (math::Pi/2.0f) - (r1/r2) * (math::Pi/4.0f);
+        phi = (math::Pi / 2.f) - (r1 / r2) * (math::Pi / 4.f);
     }
 
     Float cosPhi, sinPhi;
@@ -85,7 +79,7 @@ Point2f squareToUniformDiskConcentric(const Point2f &sample) {
     return Point2f(r * cosPhi, r * sinPhi);
 }
 
-Point2f uniformDiskToSquareConcentric(const Point2f &p) {
+Point2f diskToUniformSquareConcentric(const Point2f &p) {
     Float r   = std::sqrt(p.x() * p.x() + p.y() * p.y()),
           phi = std::atan2(p.y(), p.x()),
           a, b;
@@ -121,7 +115,7 @@ Point2f squareToStdNormal(const Point2f &sample) {
 }
 
 Float squareToStdNormalPdf(const Point2f &p) {
-    return math::InvTwoPi * std::exp(-(p.x()*p.x() + p.y()*p.y())/2.0f);
+    return math::InvTwoPi * std::exp(-(p.x() * p.x() + p.y() * p.y()) / 2.f);
 }
 
 static Float intervalToTent(Float sample) {
@@ -139,57 +133,162 @@ static Float intervalToTent(Float sample) {
 }
 
 Point2f squareToTent(const Point2f &sample) {
-    return Point2f(intervalToTent(sample.x()),
-                   intervalToTent(sample.y()));
+    return Point2f(intervalToTent(sample.x()), intervalToTent(sample.y()));
 }
 
 Float squareToTentPdf(const Point2f &p) {
     if (p.x() >= -1 && p.x() <= 1 && p.y() >= -1 && p.y() <= 1)
         return (1 - std::abs(p.x())) * (1 - std::abs(p.y()));
+
     return 0.0;
 }
 
 Float intervalToNonuniformTent(Float a, Float b, Float c, Float sample) {
     Float factor;
 
-    if (sample * (c-a) < b-a) {
-        factor = a-b;
-        sample *= (a-c)/(a-b);
+    if (sample * (c - a) < b - a) {
+        factor = a - b;
+        sample *= (a - c) / (a - b);
     } else {
-        factor = c-b;
-        sample = (a-c)/(b-c) * (sample - (a-b)/(a-c));
+        factor = c - b;
+        sample = (a - c) / (b - c) * (sample - (a - b) / (a - c));
     }
 
-    return b + factor * (1-math::safe_sqrt(sample));
+    return b + factor * (1 - math::safe_sqrt(sample));
 }
 
+Vector3f squareToBeckmann(const Point2f &sample, Float alpha) {
+    Float sinPhi, cosPhi;
+    math::sincos(2.f * math::Pi * sample.x(), &sinPhi, &cosPhi);
 
+    Float tanThetaMSqr = -alpha * alpha * std::log(1.f - sample.y());
+    Float cosThetaM = 1.f / std::sqrt(1 + tanThetaMSqr);
+    Float sinThetaM = math::safe_sqrt(1.f - cosThetaM * cosThetaM);
 
-NAMESPACE_BEGIN(detail)
-
-std::pair<bool, std::string>
-runStatisticalTestAndOutput(size_t pointCount, size_t gridWidth, size_t gridHeight,
-    SamplingType samplingType, WarpAdapter *warpAdapter,
-    double minExpFrequency, double significanceLevel,
-    std::vector<double> &observedHistogram, std::vector<double> &expectedHistogram) {
-
-    const auto nBins = gridWidth * gridHeight;
-    Eigen::MatrixXf positions;
-    std::vector<Float> weights;
-
-    // TODO: should rather use the Sampler interface
-    pcg32 sampler;
-    observedHistogram = warpAdapter->generateObservedHistogram(
-        &sampler, samplingType, pointCount, gridWidth, gridHeight);
-    expectedHistogram = warpAdapter->generateExpectedHistogram(
-        pointCount, gridWidth, gridHeight);
-
-    return hypothesis::chi2_test(nBins,
-        observedHistogram.data(), expectedHistogram.data(),
-        pointCount, minExpFrequency, significanceLevel, 1);
+    return Vector3f(sinThetaM * cosPhi, sinThetaM * sinPhi, cosThetaM);
 }
 
-NAMESPACE_END(detail)
+Float squareToBeckmannPdf(const Vector3f &m, Float alpha) {
+    if (m.z() < 1e-9f)
+        return 0.f;
+
+    Float temp = Frame::tanTheta(m) / alpha,
+          ct = Frame::cosTheta(m), ct2 = ct*ct;
+
+    return std::exp(-temp*temp)
+        / (math::Pi * alpha * alpha * ct2 * ct);
+}
+
+Vector3f squareToVonMisesFisher(const Point2f &sample, Float kappa) {
+    /* Stable algorithm for sampling the von Mises Fisher distribution
+       https://www.mitsuba-renderer.org/~wenzel/files/vmf.pdf */
+    assert(kappa >= 0.f);
+
+    Float cosTheta;
+    if (kappa == 0.f) {
+        cosTheta = 1.f - 2.f * sample.y();
+    } else if (sample.y() > 0) {
+        cosTheta = 1 + (std::log(sample.y()) + std::log(
+            1 - std::exp(-2*kappa) * ((sample.y()-1) / sample.y()))) / kappa;
+    } else {
+        cosTheta = 1 + std::exp(-2 * kappa) / kappa;
+    }
+
+    Float sinPhi, cosPhi;
+    math::sincos(2.f * math::Pi * sample.x(), &sinPhi, &cosPhi);
+
+    Float sinTheta = math::safe_sqrt(1.f - cosTheta * cosTheta);
+    return Vector3f(cosPhi * sinTheta, sinPhi * sinTheta, cosTheta);
+}
+
+Float squareToVonMisesFisherPdf(const Vector3f &v, Float kappa) {
+    /* Stable algorithm for evaluating the von Mises Fisher distribution
+       https://www.mitsuba-renderer.org/~wenzel/files/vmf.pdf */
+
+    assert(kappa >= 0.f);
+	if (kappa == 0.f)
+		return math::InvFourPi;
+    else
+        return std::exp(kappa * (v.z() - 1)) * kappa /
+               (2 * math::Pi * (1 - std::exp(-2 * kappa)));
+}
+
+Vector3f squareToRoughFiber(const Point3f &sample, const Vector3f &wi_,
+                                  const Vector3f &tangent, Float kappa) {
+    Frame tframe(tangent);
+
+    /* Convert to local coordinate frame with Z = fiber tangent */
+    Vector3f wi = tframe.toLocal(wi_);
+
+    /* Sample a point on the reflection cone */
+    Float sinPhi, cosPhi;
+    math::sincos(2.f * math::Pi * sample.x(), &sinPhi, &cosPhi);
+
+    Float cosTheta = wi.z();
+    Float sinTheta = math::safe_sqrt(1 - cosTheta * cosTheta);
+
+    Vector3f wo(cosPhi * sinTheta, sinPhi * sinTheta, -cosTheta);
+
+    /* Sample a roughness perturbation from a vMF distribution */
+    Vector3f perturbation =
+        squareToVonMisesFisher(Point2f(sample.y(), sample.z()), kappa);
+
+    /* Express perturbation relative to 'wo' */
+    wo = Frame(wo).toWorld(perturbation);
+
+    /* Back to global coordinate frame */
+    return tframe.toWorld(wo);
+}
+
+/* Numerical approximations for the modified Bessel function
+   of the first kind (I0) and its logarithm */
+namespace {
+    Float i0(Float x) {
+        Float result = 1.f, x2 = x * x, xi = x2;
+        Float denom = 4.f;
+        for (int i = 1; i <= 10; ++i) {
+            Float factor = i + 1;
+            result += xi / denom;
+            xi *= x2;
+            denom *= 4.f * factor * factor;
+        }
+        return result;
+    }
+
+    Float logI0(Float x) {
+        if (x > 12.f)
+            return x + 0.5f * (std::log(1.f / (2 * math::Pi * x)) + 1.f / (8.f * x));
+        else
+            return std::log(i0(x));
+    }
+}
+
+Float squareToRoughFiberPdf(const Vector3f &v, const Vector3f &wi,
+                            const Vector3f &tangent, Float kappa) {
+    /**
+     * Analytic density function described in "An Energy-Conserving Hair Reflectance Model"
+     * by Eugene d’Eon, Guillaume Francois, Martin Hill, Joe Letteri, and Jean-Marie Aubry
+     *
+     * Includes modifications for numerical robustness described here:
+     * https://publons.com/publon/2803
+     */
+
+    Float cosThetaI = dot(wi, tangent);
+    Float cosThetaO = dot(v, tangent);
+    Float sinThetaI = math::safe_sqrt(1 - cosThetaI * cosThetaI);
+    Float sinThetaO = math::safe_sqrt(1 - cosThetaO * cosThetaO);
+
+    Float c = cosThetaI * cosThetaO * kappa;
+    Float s = sinThetaI * sinThetaO * kappa;
+
+    Float result;
+    if (kappa > 10.f)
+        result = std::exp(-c + logI0(s) - kappa + 0.6931f + std::log(kappa / 2.f));
+    else
+        result = std::exp(-c) * i0(s) * kappa / (2.f * std::sinh(kappa));
+
+    return result * 1.f / (2.f * math::Pi);
+}
 
 NAMESPACE_END(warp)
 NAMESPACE_END(mitsuba)
