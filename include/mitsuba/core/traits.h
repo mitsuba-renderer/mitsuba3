f8b5b35c74789adee3a2efe3a14792d43f125396
Wenzel Jakob <wenzel.jakob@epfl.ch>
2017-02-22 11:52:22 +0100

    type traits support to inspect function arguments and return values

---
+++ include/mitsuba/core/traits.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <mitsuba/mitsuba.h>
+#include <type_traits>
+#include <tuple>
+
+NAMESPACE_BEGIN(mitsuba)
+
+/// Type trait to inspect the return and argument types of functions
+template <typename T, typename SFINAE = void> struct function_traits { };
+
+/// Vanilla function
+template <typename R, typename... A> struct function_traits<R(*)(A...)> {
+    using Args = std::tuple<A...>;
+    using Return = R;
+};
+
+/// Method
+template <typename C, typename R, typename... A> struct function_traits<R(C::*)(A...)> {
+    using Class = C;
+    using Args = std::tuple<A...>;
+    using Return = R;
+};
+
+/// Method (const)
+template <typename C, typename R, typename... A> struct function_traits<R(C::*)(A...) const> {
+    using Class = C;
+    using Args = std::tuple<A...>;
+    using Return = R;
+};
+
+/// Lambda function -- strip lambda closure and delegate back to ``function_traits``
+template <typename F>
+struct function_traits<
+    F, std::enable_if_t<std::is_member_function_pointer<decltype(
+           &std::remove_reference_t<F>::operator())>::value>>
+    : function_traits<decltype(&std::remove_reference_t<F>::operator())> { };
+
+NAMESPACE_END(mitsuba)
