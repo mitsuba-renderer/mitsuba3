0bceff23882d8401f9d414b52f18a7cfd5de2d60
speierers <sebastien.speierer@epfl.ch>
2017-10-06 13:38:06 +0200

    Port BSDF: initial commit, BSDFSamplingRecord structure

--- src/librender/bsdf.cpp
+++ src/librender/bsdf.cpp
@@ -2,6 +2,9 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
+template struct MTS_EXPORT_RENDER BSDFSample<Point3f>;
+template struct MTS_EXPORT_RENDER BSDFSample<Point3fP>;
+
 BSDF::~BSDF() { }
 
 MTS_IMPLEMENT_CLASS(BSDF, Object)
