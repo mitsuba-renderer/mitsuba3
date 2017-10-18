26b6b5338bdfe69cdca79052e55c5de295b08c6e
speierers <sebastien.speierer@epfl.ch>
2017-10-18 15:00:00 +0200

    Port Film: ImageBlock

---
+++ include/mitsuba/render/imageblock.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <mitsuba/mitsuba.h>
+#include <mitsuba/core/bitmap.h>
+#include <mitsuba/core/rfilter.h>
+#include <mitsuba/core/spectrum.h>
+#include <mitsuba/core/vector.h>
+#include <mitsuba/core/fwd.h>
+#include <mitsuba/render/fwd.h>
+
+NAMESPACE_BEGIN(mitsuba)
+
+/**
+* \brief Storage for an image sub-block (a.k.a render bucket)
+*
+* This class is used by image-based parallel processes and encapsulates
+* computed rectangular regions of an image. This allows for easy and efficient
+* distributed rendering of large images. Image blocks usually also include a
+* border region storing contributions that are slightly outside of the block,
+* which is required to support image reconstruction filters.
+*/
+class MTS_EXPORT_RENDER ImageBlock : public Object {
+public:
+    /**
+    * Construct a new image block of the requested properties
+    *
+    * \param fmt
+    *    Specifies the pixel format -- see \ref Bitmap::EPixelFormat
+    *    for a list of possibilities
+    * \param size
+    *    Specifies the block dimensions (not accounting for additional
+    *    border pixels required to support image reconstruction filters)
+    * \param channels
+    *    Specifies the number of output channels. This is only necessary
+    *    when \ref Bitmap::EMultiChannel is chosen as the pixel format
+    * \param warn
+    *    Warn when writing bad sample values?
+    */
+    ImageBlock(Bitmap::EPixelFormat fmt,
+               const Vector2i &size,
+               const ReconstructionFilter *filter = NULL,
+               int channels = -1,
+               bool warn = true);
+
+    /// Accumulate another image block into this one
+    void put(const ImageBlock *block) {
+        m_bitmap->accumulate(block->bitmap(),
+            Point2i(
+                block->offset() - m_offset
+                - Vector2i(block->border_size() - m_border_size)
+            )
+        );
+    }
+
+    /**
+    * \brief Store a single sample / packets of samples inside the
+    * image block
+    *
+    * This variant assumes that the image block stores spectrum,
+    * alpha, and reconstruction filter weight values.
+    *
+    * \param pos
+    *    Denotes the sample position in fractional pixel coordinates
+    * \param spec
+    *    Spectrum value assocated with the sample
+    * \param alpha
+    *    Alpha value assocated with the sample
+    * \return \c false if one of the sample values was \a invalid, e.g.
+    *    NaN or negative. A warning is also printed if \c m_warn is enabled.
+    */
+    template<typename Point2,
+             typename Spectrum,
+             typename Value = typename Point2::Value>
+    bool put(const Point2 &pos, const Spectrum &spec, Value alpha) {
+        Value temp[MTS_WAVELENGTH_SAMPLES + 2];
+        for (int i = 0; i < MTS_WAVELENGTH_SAMPLES; ++i)
+            temp[i] = spec[i];
+        temp[MTS_WAVELENGTH_SAMPLES] = alpha;
+        temp[MTS_WAVELENGTH_SAMPLES + 1] = 1.0f;
+        return put(pos, temp);
+    }
+
+    /**
+    * \brief Store a single sample inside the block
+    *
+    * \param _pos
+    *    Denotes the sample position in fractional pixel coordinates
+    * \param value
+    *    Pointer to an array containing each channel of the sample values.
+    *    The array must match the length given by \ref channel_count()
+    * \return \c false if one of the sample values was \a invalid, e.g.
+    *    NaN or negative. A warning is also printed if \c m_warn is enabled.
+    */
+    bool put(const Point2f &_pos, const Float *value);
+
+    /**
+    * \brief Store a packet of samples inside the block
+    *
+    * \param _pos
+    *    Denotes the samples positions in fractional pixel coordinates
+    * \param value
+    *    Pointer to an array containing packets for each channel of the sample
+    *    values. The array must match the length given by \ref channel_count()
+    * \return \c false if one of the sample values was \a invalid, e.g.
+    *    NaN or negative. A warning is also printed if \c m_warn is enabled.
+    */
+    bool put(const Point2fP &_pos, const FloatP *value);
+
+    /// Create a clone of the entire image block
+    ref<ImageBlock> clone() const {
+        ref<ImageBlock> clone = new ImageBlock(m_bitmap->pixel_format(),
+            m_bitmap->size() - Vector2i(2 * m_border_size, 2 * m_border_size), m_filter, m_bitmap->channel_count());
+        copy_to(clone);
+        return clone;
+    }
+
+    /// Copy the contents of this image block to another one with the same configuration
+    void copy_to(ImageBlock *copy) const {
+        memcpy(copy->bitmap()->uint8_data(), m_bitmap->uint8_data(), m_bitmap->buffer_size());
+        copy->m_size = m_size;
+        copy->m_offset = m_offset;
+        copy->m_warn = m_warn;
+    }
+
+    // =============================================================
+    //! @{ \name Setter and accessor functions
+    // =============================================================
+
+    /// Set the current block offset
+    void set_offset(const Point2i &offset) { m_offset = offset; }
+
+    /// Return the current block offset
+    const Point2i &offset() const { return m_offset; }
+
+    /// Set the current block size
+    void set_size(const Vector2i &size) { m_size = size; }
+
+    /// Return the current block size
+    const Vector2i &size() const { return m_size; }
+
+    /// Return the bitmap's width in pixels
+    size_t width() const { return m_size.x(); }
+
+    /// Return the bitmap's height in pixels
+    size_t height() const { return m_size.y(); }
+
+    /// Warn when writing bad sample values?
+    bool warns() const { return m_warn; }
+
+    /// Warn when writing bad sample values?
+    void set_warn(bool warn) { m_warn = warn; }
+
+    /// Return the border region used by the reconstruction filter
+    size_t border_size() const { return m_border_size; }
+
+    /// Return the number of channels stored by the image block
+    size_t channel_count() const { return m_bitmap->channel_count(); }
+
+    /// Return the underlying pixel format
+    Bitmap::EPixelFormat pixel_format() const { return m_bitmap->pixel_format(); }
+
+    /// Return a pointer to the underlying bitmap representation
+    Bitmap *bitmap() { return m_bitmap; }
+
+    /// Return a pointer to the underlying bitmap representation (const version)
+    const Bitmap *bitmap() const { return m_bitmap.get(); }
+
+    /// Clear everything to zero
+    void clear() { m_bitmap->clear(); }
+
+    //! @}
+    // =============================================================
+
+    std::string to_string() const override;
+
+    MTS_DECLARE_CLASS()
+protected:
+    /// Virtual destructor
+    virtual ~ImageBlock();
+protected:
+    ref<Bitmap> m_bitmap;
+    Point2i m_offset;
+    Vector2i m_size;
+    int m_border_size;
+    const ReconstructionFilter *m_filter;
+    Float *m_weights_x, *m_weights_y;
+    bool m_warn;
+};
+
+NAMESPACE_END(mitsuba)
---
+++ src/librender/imageblock.cpp
@@ -0,0 +1,188 @@
+#include <mitsuba/render/imageblock.h>
+
+NAMESPACE_BEGIN(mitsuba)
+
+ImageBlock::ImageBlock(Bitmap::EPixelFormat fmt, const Vector2i &size,
+    const ReconstructionFilter *filter, int channels, bool warn)
+    : m_offset(0), m_size(size), m_filter(filter),
+    m_weights_x(nullptr), m_weights_y(nullptr), m_warn(warn) {
+    m_border_size = filter ? filter->border_size() : 0;
+
+    // Allocate a small bitmap data structure for the block
+    m_bitmap = new Bitmap(fmt, Struct::EType::EFloat,
+        size + Vector2i(2 * m_border_size), channels);
+
+    if (filter) {
+        // Temporary buffers used in put()
+        int temp_buffer_size = (int)std::ceil(2 * filter->radius()) + 1;
+        m_weights_x = new Float[2 * temp_buffer_size];
+        m_weights_y = m_weights_x + temp_buffer_size;
+
+        // TODO Pre_allocate vectorized weights buffers
+    }
+}
+
+ImageBlock::~ImageBlock() {
+    if (m_weights_x)
+        delete[] m_weights_x;
+}
+
+bool ImageBlock::put(const Point2f &_pos, const Float *value) {
+    const int channels = m_bitmap->channel_count();
+
+    // Check if all sample values are valid
+    bool valid_sample = true;
+    if (m_warn) {
+        for (int i = 0; i < channels; ++i) {
+            if (unlikely((!std::isfinite(value[i]) || value[i] < 0))) {
+                valid_sample = false;
+                break;
+            }
+        }
+    }
+
+    if (unlikely(!valid_sample)) {
+        std::ostringstream oss;
+        oss << "Invalid sample value : [";
+        for (int i = 0; i < channels; ++i) {
+            oss << value[i];
+            if (i + 1 < channels)
+                oss << ", ";
+        }
+        oss << "]";
+        Log(EWarn, "%s", oss.str());
+        return false;
+    } else {
+        const Float filter_radius = m_filter->radius();
+        const Vector2i &size = m_bitmap->size();
+
+        // Convert to pixel coordinates within the image block
+        const Point2f pos(
+            _pos.x() - 0.5f - (m_offset.x() - m_border_size),
+            _pos.y() - 0.5f - (m_offset.y() - m_border_size)
+        );
+
+        // Determine the affected range of pixels
+        const Point2i lo(std::max((int)std::ceil(pos.x() - filter_radius), 0),
+            std::max((int)std::ceil(pos.y() - filter_radius), 0));
+        const Point2i hi(std::min((int)std::floor(pos.x() + filter_radius), size.x() - 1),
+            std::min((int)std::floor(pos.y() + filter_radius), size.y() - 1));
+
+        // Lookup values from the pre-rasterized filter
+        for (int x = lo.x(), idx = 0; x <= hi.x(); ++x)
+            m_weights_x[idx++] = m_filter->eval_discretized(x - pos.x());
+        for (int y = lo.y(), idx = 0; y <= hi.y(); ++y)
+            m_weights_y[idx++] = m_filter->eval_discretized(y - pos.y());
+
+        // Rasterize the filtered sample into the framebuffer
+        for (int y = lo.y(), yr = 0; y <= hi.y(); ++y, ++yr) {
+            const Float weightY = m_weights_y[yr];
+            auto dest = static_cast<Float *>(m_bitmap->data())
+                + (y * (size_t)size.x() + lo.x()) * channels;
+
+            for (int x = lo.x(), xr = 0; x <= hi.x(); ++x, ++xr) {
+                const Float weight = m_weights_x[xr] * weightY;
+
+                for (int k = 0; k < channels; ++k)
+                    *dest++ += weight * value[k];
+            }
+        }
+        return true;
+    }
+}
+
+bool ImageBlock::put(const Point2fP &_pos, const FloatP *value) {
+    using Mask = mask_t<FloatP>;
+
+    const int channels = m_bitmap->channel_count();
+
+    // Check if all sample values are valid
+    Mask is_valid(true);
+    if (m_warn) {
+        for (int i = 0; i < channels; ++i)
+            is_valid &= (enoki::isfinite(value[i]) & value[i] >= 0);
+    }
+
+    if (any(is_valid)) {
+        const Float filter_radius = m_filter->radius();
+        const Vector2i &size = m_bitmap->size();
+
+        // Convert to pixel coordinates within the image block
+        const Point2fP pos(
+            _pos.x() - 0.5f - (m_offset.x() - m_border_size),
+            _pos.y() - 0.5f - (m_offset.y() - m_border_size)
+        );
+
+        // Determine the affected range of pixels
+        const Point2iP lo(max((Int32P)ceil(pos.x() - filter_radius), 0),
+            max((Int32P)ceil(pos.y() - filter_radius), 0));
+        const Point2iP hi(min((Int32P)floor(pos.x() + filter_radius), size.x() - 1),
+            min((Int32P)floor(pos.y() + filter_radius), size.y() - 1));
+
+        // Lookup values from the pre-rasterized filter
+        Vector2iP pixel_range = hi - lo;
+
+        int max_range_x = hmax(pixel_range.x());
+        int max_range_y = hmax(pixel_range.y());
+
+        std::vector<FloatP> weights_x(max_range_x);
+        for (int idx = 0; idx <= max_range_x; ++idx) {
+            weights_x[idx] = m_filter->eval_discretized(lo.x() + idx - pos.x());
+        }
+
+        std::vector<FloatP> weights_y(max_range_y);
+        for (int idx = 0; idx <= max_range_y; ++idx) {
+            weights_y[idx] = m_filter->eval_discretized(lo.y() + idx - pos.y());
+        }
+
+        // Rasterize the filtered sample into the framebuffer
+        for (int yr = 0; yr <= max_range_y; ++yr) {
+            const FloatP &weight_y = weights_y[yr];
+
+            SizeP offset_y = ((lo.y() + yr) * (size_t)size.x() + lo.x()) * channels;
+
+            for (int xr = 0; xr <= max_range_x; ++xr) {
+                const FloatP weight = weights_x[xr] * weight_y;
+
+                // Ensure we are not writing out of the filter range
+                Mask invalid_store = pixel_range.y() < yr || pixel_range.x() < xr || !is_valid;
+                if (all(invalid_store)) continue;
+
+                for (int k = 0; k < channels; ++k) {
+                    FloatP val = gather<FloatP>(m_bitmap->data(), k + xr + offset_y, !invalid_store);
+                    val += select(invalid_store, 0.f, weight * value[k]);
+                    scatter(m_bitmap->data(), val, k + xr + offset_y, !invalid_store);
+                }
+            }
+        }
+    }
+
+    if (m_warn && any(!is_valid)) {
+        std::ostringstream oss;
+        oss << "Invalid sample value : [";
+        for (int i = 0; i < channels; ++i) {
+            oss << value[i];
+            if (i + 1 < channels)
+                oss << ", ";
+        }
+        oss << "]";
+        Log(EWarn, "%s", oss.str());
+
+        return false;
+    }
+
+    return true;
+}
+
+std::string ImageBlock::to_string() const {
+    std::ostringstream oss;
+    oss << "ImageBlock[" << std::endl
+        << "  offset = " << m_offset << "," << std::endl
+        << "  size = "   << m_size   << "," << std::endl
+        << "  border_size = " << m_border_size << std::endl
+        << "]";
+    return oss.str();
+}
+
+MTS_IMPLEMENT_CLASS(ImageBlock, Object)
+NAMESPACE_END(mitsuba)
