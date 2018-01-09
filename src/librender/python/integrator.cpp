71dd1b1d335eb57738245c9684579ebdfe9078ac
Merlin Nimier-David <merlin.nimier@gmail.com>
2018-01-09 16:47:50 +0100

    Integrator: make interruptible when called through Python
    Only supported on macOS and Linux.
    When the interrupt signal is received, `Integrator::cancel()` is called so as to wind everything down properly.
    The previously installed signal handler is also called, and restored at the end.

--- src/librender/python/integrator.cpp
+++ src/librender/python/integrator.cpp
@@ -1,10 +1,49 @@
+#include <mitsuba/core/tls.h>
 #include <mitsuba/render/integrator.h>
 #include <mitsuba/python/python.h>
 
+// TODO: move all signal-related mechanisms to libcore?
+#define MTS_INSTALL_HANDLER defined(__APPLE__) || defined(__linux__)
+
+#if MTS_INSTALL_HANDLER
+#include <signal.h>
+
+/// Integrator currently in use (has to be global)
+ThreadLocal<Integrator *> current_integrator;
+/// Previously installed signal handler
+static void (*sigint_handler_prev)(int) = nullptr;
+/// Custom signal handler definition
+static void sigint_handler(int sig) {
+    Log(EWarn, "Received interrupt signal, winding down..");
+    (*current_integrator).cancel();
+    signal(sig, sigint_handler_prev);
+    raise(sig);
+}
+
+#endif
+
+#include <chrono>
+#include <thread>
 
 MTS_PY_EXPORT(Integrator) {
     MTS_PY_CLASS(Integrator, Object)
-        .mdef(Integrator, render, "scene"_a, "vectorize"_a)
+        .def("render", [&](Integrator &integrator, Scene *scene, bool vectorize) {
+                #if MTS_INSTALL_HANDLER
+                // Install new signal handler
+                current_integrator = &integrator;
+                sigint_handler_prev = signal(SIGINT, sigint_handler);
+                #endif
+
+                bool res = integrator.render(scene, vectorize);
+
+                #if MTS_INSTALL_HANDLER
+                // Restore previous signal handler
+                signal(SIGINT, sigint_handler_prev);
+                #endif
+
+                return res;
+             },
+             D(Integrator, render), "scene"_a, "vectorize"_a)
         .mdef(Integrator, cancel);
 
     MTS_PY_CLASS(SamplingIntegrator, Integrator)
@@ -17,3 +56,5 @@ MTS_PY_EXPORT(Integrator) {
 
     MTS_PY_CLASS(MonteCarloIntegrator, SamplingIntegrator);
 }
+
+#undef MTS_INSTALL_HANDLER
