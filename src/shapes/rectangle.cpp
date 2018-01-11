678f07caa253cfc756632a4d59f3e770588f26ed
Merlin Nimier-David <merlin.nimier@gmail.com>
2018-01-11 12:07:46 +0100

    Rectangle shape
    Ported from Mitsuba1 by @speierers.
    Also includes basic unit tests.

---
+++ src/shapes/rectangle.cpp
@@ -0,0 +1,212 @@
+#include <mitsuba/core/fwd.h>
+#include <mitsuba/core/math.h>
+#include <mitsuba/core/properties.h>
+#include <mitsuba/core/string.h>
+#include <mitsuba/core/transform.h>
+#include <mitsuba/core/util.h>
+#include <mitsuba/render/bsdf.h>
+#include <mitsuba/render/emitter.h>
+#include <mitsuba/render/fwd.h>
+#include <mitsuba/render/interaction.h>
+#include <mitsuba/render/shape.h>
+
+NAMESPACE_BEGIN(mitsuba)
+
+/**
+ * \brief Simple analytic rectangle, for use e.g. as a ground plane or area emitter.
+ *
+ * By default, the rectangle covers the XY-range [-1, 1]^2 and has a surface
+ * normal that points into the positive $Z$ direction.
+ * To change the rectangle scale, rotation, or translation, use the
+ * \ref to_world parameter.
+ */
+class Rectangle : public Shape {
+public:
+    Rectangle(const Properties &props) : Shape(props) {
+        m_object_to_world = props.transform("to_world", Transform4f());
+        if (props.bool_("flip_normals", false))
+            m_object_to_world = m_object_to_world * Transform4f::scale(Vector3f(1.0f, 1.0f, -1.0f));
+
+        m_world_to_object = m_object_to_world.inverse();
+
+        m_dp_du = m_object_to_world * Vector3f(2.0f, 0.0f, 0.0f);
+        m_dp_dv = m_object_to_world * Vector3f(0.0f, 2.0f, 0.0f);
+        Normal3f normal = normalize(m_object_to_world * Normal3f(0.0f, 0.0f, 1.0f));
+        m_frame = Frame<Vector3f>(normalize(m_dp_du), normalize(m_dp_dv), normal);
+
+        m_inv_surface_area = 1.0f / surface_area();
+        if (abs(dot(normalize(m_dp_du), normalize(m_dp_dv))) > math::Epsilon)
+            Throw("The `to_world` transformation contains shear, which is not"
+                  " supported by the Rectangle shape.");
+
+        if (is_emitter())
+            emitter()->set_shape(this);
+    }
+
+    BoundingBox3f bbox() const override {
+        BoundingBox3f bbox;
+        bbox.expand(m_object_to_world * Point3f(-1.0f, -1.0f, 0.0f));
+        bbox.expand(m_object_to_world * Point3f( 1.0f, -1.0f, 0.0f));
+        bbox.expand(m_object_to_world * Point3f( 1.0f,  1.0f, 0.0f));
+        bbox.expand(m_object_to_world * Point3f(-1.0f,  1.0f, 0.0f));
+        return bbox;
+    }
+
+    Float surface_area() const override {
+        return norm(m_dp_du) * norm(m_dp_dv);
+    }
+
+    // =============================================================
+    //! @{ \name Sampling routines
+    // =============================================================
+    template <typename Point2, typename Value = value_t<Point2>>
+    auto sample_position_impl(Value time, const Point2 &sample,
+                              const mask_t<Value> &/*active*/) const {
+        using Point3   = point3_t<Point2>;
+        using Normal3  = normal3_t<Point2>;
+
+        PositionSample<Point3> ps;
+        ps.p = m_object_to_world * Point3(sample.x() * 2.0f - 1.0f,
+                                          sample.y() * 2.0f - 1.0f,
+                                          0.0f);
+        ps.n    = Normal3(m_frame.n);
+        ps.pdf  = m_inv_surface_area;
+        ps.uv   = sample;
+        ps.time = time;
+
+        return ps;
+    }
+
+    template <typename PositionSample3,
+              typename Value = typename PositionSample3::Value>
+    Value pdf_position_impl(PositionSample3 &/*p_rec*/,
+                            const mask_t<Value> &/*active*/) const {
+        return m_inv_surface_area;
+    }
+
+    template <typename Interaction3, typename Point2,
+              typename Value = value_t<Point2>>
+    auto sample_direction_impl(const Interaction3 &it, const Point2 &sample,
+                               const mask_t<Value> &active) const {
+        using DirectionSample = DirectionSample<typename Interaction3::Point3>;
+
+        DirectionSample ds = sample_position_impl(it.time, sample, active);
+        ds.d = ds.p - it.p;
+
+        Value dist_squared = squared_norm(ds.d);
+        ds.dist  = sqrt(dist_squared);
+        ds.d    /= ds.dist;
+        Value dp = abs_dot(ds.d, ds.n);
+        ds.pdf  *= select(neq(dp, 0.0f), dist_squared / dp, Value(0.0f));
+
+        return ds;
+    }
+
+    template <typename Interaction3, typename DirectionSample3,
+              typename Value = typename Interaction3::Value>
+    Value pdf_direction_impl(const Interaction3 &/*it*/, const DirectionSample3 &ds,
+                             const mask_t<Value> &active) const {
+        Value pdf = pdf_position_impl(ds, active),
+              dp  = abs_dot(ds.d, ds.n);
+
+        return pdf * select(neq(dp, 0.0f), (ds.dist * ds.dist) / dp, 0.0f);
+    }
+
+    //! @}
+    // =============================================================
+
+    // =============================================================
+    //! @{ \name Ray tracing routines
+    // =============================================================
+
+    template <typename Ray3, typename Value = typename Ray3::Value>
+    std::pair<mask_t<Value>, Value> ray_intersect_impl(
+            const Ray3 &ray_, Value *cache, mask_t<Value> active) const {
+        using Point3 = Point<Value, 3>;
+
+        Ray3 ray     = m_world_to_object * ray_;
+        Value t      = -ray.o.z() / ray.d.z();
+        Point3 local = ray(t);
+
+        // Intersection is within ray segment and within rectangle.
+        active = active && (t >= ray.mint && t <= ray.maxt)
+                        && (abs(local.x()) <= 1.0f) && (abs(local.y()) <= 1.0f);
+        t = select(active, t, Value(math::Infinity));
+
+        if (cache) {
+            masked(cache[0], active) = local.x();
+            masked(cache[1], active) = local.y();
+        }
+
+        return { active, t };
+    }
+
+    template <typename Ray3, typename Value = typename Ray3::Value>
+    mask_t<Value> ray_test_impl(const Ray3 &ray_, mask_t<Value> active) const {
+        using Point3 = Point<Value, 3>;
+
+        Ray3 ray     = m_world_to_object * ray_;
+        Value t      = -ray.o.z() / ray.d.z();
+        Point3 local = ray(t);
+        // Intersection is within ray segment and within rectangle.
+        return active && (t >= ray.mint && t <= ray.maxt)
+                      && (abs(local.x()) <= 1.0f) && (abs(local.y()) <= 1.0f);
+    }
+
+    template <typename Ray3,
+              typename SurfaceInteraction3,
+              typename Value = typename SurfaceInteraction3::Value>
+    void fill_intersection_record_impl(const Ray3 &ray, const Value *cache,
+                                       SurfaceInteraction3 &si,
+                                       const mask_t<Value> &active) const {
+        using Point2  = typename SurfaceInteraction3::Point2;
+
+        masked(si.n,          active) = m_frame.n;
+        masked(si.sh_frame.n, active) = m_frame.n;
+        masked(si.shape,      active) = this;
+        masked(si.dp_du,      active) = m_dp_du;
+        masked(si.dp_dv,      active) = m_dp_dv;
+        masked(si.p,          active) = ray(si.t);
+        masked(si.time,       active) = ray.time;
+        masked(si.instance,   active) = nullptr;
+        masked(si.uv,         active) = Point2(0.5f * (cache[0] + 1.0f),
+                                               0.5f * (cache[1] + 1.0f));
+    }
+
+    template <typename SurfaceInteraction3,
+              typename Value   = typename SurfaceInteraction3::Value,
+              typename Vector3 = typename SurfaceInteraction3::Vector3>
+    std::pair<Vector3, Vector3> normal_derivative_impl(
+            const SurfaceInteraction3 &/*si*/, bool, const mask_t<Value> &) const {
+        return { Vector3(0.0f), Vector3(0.0f) };
+    }
+
+    Size primitive_count() const override { return 1; }
+
+    Size effective_primitive_count() const override { return 1; }
+
+    std::string to_string() const override {
+        std::ostringstream oss;
+        oss << "Rectangle[" << std::endl
+            << "  object_to_world = " << string::indent(m_object_to_world) << "," << std::endl
+            << "  frame = " << string::indent(m_frame) << "," << std::endl
+            << "  inv_surface_area = " << m_inv_surface_area << "," << std::endl
+            << "  bsdf = " << string::indent(bsdf()->to_string()) << std::endl
+            << "]";
+        return oss.str();
+    }
+
+    MTS_IMPLEMENT_SHAPE()
+    MTS_DECLARE_CLASS()
+private:
+    Transform4f m_object_to_world;
+    Transform4f m_world_to_object;
+    Frame<Vector3f> m_frame;
+    Vector3f m_dp_du, m_dp_dv;
+    Float m_inv_surface_area;
+};
+
+MTS_IMPLEMENT_CLASS(Rectangle, Shape)
+MTS_EXPORT_PLUGIN(Rectangle, "Rectangle intersection primitive");
+
+NAMESPACE_END(mitsuba)
---
+++ src/shapes/tests/test_rectangle.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pytest
+from mitsuba.core      import Ray3f, Ray3fX
+from mitsuba.core.xml  import load_string
+
+def example_rectangle(scale = (1, 1, 1), translate = (0, 0, 0)):
+    return load_string("""<shape version="2.0.0" type="rectangle">
+        <transform name="to_world">
+            <scale x="{}" y="{}" z="{}"/>
+            <translate x="{}" y="{}" z="{}"/>
+        </transform>
+    </shape>""".format(scale[0], scale[1], scale[2],
+                       translate[0], translate[1], translate[2]))
+
+def test01_create():
+    s = example_rectangle()
+    assert s is not None
+    assert s.primitive_count() == 1
+    assert np.allclose(s.surface_area(), 4.0)
+
+def test02_bbox():
+    sy = 2.5
+    for sx in [1, 2, 4]:
+        for translate in [np.array([1.3, -3.0, 5]),
+                          np.array([-10000, 3.0, 31])]:
+            s = example_rectangle((sx, sy, 1.0), translate)
+            b = s.bbox()
+
+            assert np.allclose(s.surface_area(), sx * sy * 4)
+
+            assert b.valid()
+            assert np.allclose(b.center(), translate)
+            assert np.allclose(b.min, translate - np.array([sx, sy, 0.0]))
+            assert np.allclose(b.max, translate + np.array([sx, sy, 0.0]))
+
+def test03_ray_intersect():
+    scene = load_string("""<scene version="2.0.0">
+        <shape type="rectangle">
+            <transform name="to_world">
+                <scale x="2" y="0.5" z="1"/>
+            </transform>
+        </shape>
+    </scene>""")
+
+    # Scalar
+    n = 15
+    coords = np.linspace(-1, 1, n)
+    rays = [Ray3f(o=[a, a, 5], d=[0, 0, -1], time=0.0,
+                  wavelengths=[400, 500, 600, 700]) for a in coords]
+    si_scalar = []
+    valid_count = 0
+    for i in range(n):
+        # print(rays[i])
+        its_found = scene.ray_test(rays[i])
+        si = scene.ray_intersect(rays[i])
+
+        assert its_found == (abs(coords[i]) <= 0.5)
+        assert si.is_valid() == its_found
+        si_scalar.append(si)
+        valid_count += its_found
+
+    assert valid_count == 7
+
+    packet = Ray3fX(n)
+    for i in range(n):
+        packet[i] = rays[i]
+    si_p = scene.ray_intersect(packet)
+    its_found_p = scene.ray_test(packet)
+
+    assert np.all(si_p.is_valid() == its_found_p)
+    for i in range(n):
+        assert np.allclose(si_p[i].t, si_scalar[i].t)
