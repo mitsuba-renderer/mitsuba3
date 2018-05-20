7360111c28c580a4e011d091220478af3d668aae
Wenzel Jakob <wenzel.jakob@epfl.ch>
2018-05-20 17:55:40 +0200

    better error messages when importing mitsuba fails

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,3 +1,18 @@
 """ Mitsuba Python extension library """
 
-from . import core, render
+import sys
+
+try:
+    import numpy
+except ImportError:
+    raise ImportError("NumPy could not be found! (To install it, run %s -m pip install numpy)" % sys.executable)
+
+try:
+    from . import core, render
+except ImportError as e:
+    from .config import PYTHON_EXECUTABLE
+
+    raise ImportError("The 'mitsuba' native modules could not be imported. You're likely trying "
+                      "to use Mitsuba within a Python binary (%s) that is different from the "
+                      " one for which the native module was compiled (%s)."
+                      % (sys.executable, PYTHON_EXECUTABLE))
