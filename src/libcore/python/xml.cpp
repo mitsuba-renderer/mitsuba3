1943f4b875d8a1d2e473ceb424e2d7808d6d970e
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-01-21 19:32:18 +0100

    XML (Python): release GIL when loading scenes

--- src/libcore/python/xml.cpp
+++ src/libcore/python/xml.cpp
@@ -1,5 +1,5 @@
-#include <mitsuba/core/xml.h>
 #include <mitsuba/core/filesystem.h>
+#include <mitsuba/core/xml.h>
 #include <mitsuba/python/python.h>
 
 extern py::object py_cast(Object *o);
@@ -7,16 +7,19 @@ extern py::object py_cast(Object *o);
 MTS_PY_EXPORT(xml) {
     MTS_PY_IMPORT_MODULE(xml, "mitsuba.core.xml");
 
-    xml.def("load_file",
-            [](const std::string &name, const xml::ParameterList &param) {
-                return py_cast(xml::load_file(name, param));
-            },
-            "path"_a, "parameters"_a = py::list(), D(xml, load_file));
+    xml.def(
+        "load_file",
+        [](const std::string &name, const xml::ParameterList &param) {
+            py::gil_scoped_release release;
+            return py_cast(xml::load_file(name, param));
+        },
+        "path"_a, "parameters"_a = py::list(), D(xml, load_file));
 
-    xml.def("load_string",
-            [](const std::string &name, const xml::ParameterList &param) {
-                return py_cast(xml::load_string(name, param));
-            },
-            "string"_a, "parameters"_a = py::list(), D(xml, load_string));
+    xml.def(
+        "load_string",
+        [](const std::string &name, const xml::ParameterList &param) {
+            py::gil_scoped_release release;
+            return py_cast(xml::load_string(name, param));
+        },
+        "string"_a, "parameters"_a = py::list(), D(xml, load_string));
 }
-
