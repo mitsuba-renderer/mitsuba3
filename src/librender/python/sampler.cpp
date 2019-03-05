5cf787850b346a9b93cfc5c070e35c37fa8d792f
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-03-05 14:44:22 +0100

    Sampler: overloaded Python bindings

--- src/librender/python/sampler.cpp
+++ src/librender/python/sampler.cpp
@@ -9,15 +9,25 @@ MTS_PY_EXPORT(Sampler) {
              D(Sampler, next_1d))
         .def("next_1d_p", [](Sampler &sampler) { return sampler.next_1d_p(); },
              D(Sampler, next_1d_p))
+        .def("next_1d_p",
+             [](Sampler &sampler, const MaskP &active) {
+                 return sampler.next_1d_p(active);
+             },
+             D(Sampler, next_1d_p))
 #if defined(MTS_ENABLE_AUTODIFF)
-        .def("next_1d_d", [](Sampler &sampler) { return sampler.next_1d_d(); })
+        .def("next_1d_d", &Sampler::next_1d_d, "active"_a)
 #endif
         .def("next_2d", py::overload_cast<>(&Sampler::next_2d),
              D(Sampler, next_2d))
         .def("next_2d_p", [](Sampler &sampler) { return sampler.next_2d_p(); },
              D(Sampler, next_2d_p))
+        .def("next_2d_p",
+             [](Sampler &sampler, const MaskP &active) {
+                 return sampler.next_2d_p(active);
+             },
+             D(Sampler, next_2d_p))
 #if defined(MTS_ENABLE_AUTODIFF)
-        .def("next_2d_d", [](Sampler &sampler) { return sampler.next_2d_d(); })
+        .def("next_2d_d", &Sampler::next_2d_d, "active"_a)
 #endif
         .mdef(Sampler, sample_count);
 }
