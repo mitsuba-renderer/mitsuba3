2d5f753d9d3225e997af26c90c0b5749655c86f6
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-17 16:59:23 +0200

    CatmullRomFilter: templatize

--- src/rfilters/catmullrom.cpp
+++ src/rfilters/catmullrom.cpp
@@ -7,20 +7,26 @@ NAMESPACE_BEGIN(mitsuba)
  * to match the Catmull-Rom spline. It usually does a better job at at preserving sharp
  * features at the cost of more ringing.
  */
-class CatmullRomFilter final : public ReconstructionFilter {
+template <typename Float, typename Spectrum = void>
+class CatmullRomFilter final : public ReconstructionFilter<Float> {
 public:
-    CatmullRomFilter(const Properties &props) : ReconstructionFilter(props) {
+    MTS_DECLARE_CLASS();
+    using Base = mitsuba::ReconstructionFilter<Float>;
+    using Base::init_discretization;
+    using Base::m_radius;
+
+    CatmullRomFilter(const Properties &props) : Base(props) {
         m_radius = 2.f;
         init_discretization();
     }
 
-    template <typename Value> Value eval_impl(Value x) const {
+    Float eval(Float x) const override {
         x = abs(x);
 
-        Value x2 = sqr(x), x3 = x2*x,
+        Float x2 = sqr(x), x3 = x2*x,
               B = 0.f, C = .5f;
 
-        Value result = (1.f / 6.f) * select(
+        Float result = (1.f / 6.f) * select(
            x < 1,
            (12.f - 9.f * B - 6.f * C) * x3 +
                (-18.f + 12.f * B + 6.f * C) * x2 + (6.f - 2.f * B),
@@ -35,10 +41,7 @@ public:
         return tfm::format("CatmullRomFilter[radius=%f]", m_radius);
     }
 
-    MTS_IMPLEMENT_RFILTER_ALL()
-    MTS_DECLARE_CLASS()
 };
 
-MTS_IMPLEMENT_CLASS(CatmullRomFilter, ReconstructionFilter);
-MTS_EXPORT_PLUGIN(CatmullRomFilter, "Catmull-Rom filter");
+MTS_IMPLEMENT_PLUGIN(CatmullRomFilter, ReconstructionFilter, "Catmull-Rom filter");
 NAMESPACE_END(mitsuba)
