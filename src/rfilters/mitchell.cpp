06c423a45abdac234f84fdee619f5b6680fd7167
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-17 17:03:12 +0200

    MitchellNetravaliFilter: templatize

--- src/rfilters/mitchell.cpp
+++ src/rfilters/mitchell.cpp
@@ -10,23 +10,29 @@ NAMESPACE_BEGIN(mitsuba)
  * D. Mitchell, A. Netravali, Reconstruction filters for computer graphics,
  * Proceedings of SIGGRAPH 88, Computer Graphics 22(4), pp. 221-228, 1988.
  */
-class MitchellNetravaliFilter final : public ReconstructionFilter {
+template <typename Float, typename Spectrum = void>
+class MitchellNetravaliFilter final : public ReconstructionFilter<Float> {
 public:
-    MitchellNetravaliFilter(const Properties &props) : ReconstructionFilter(props) {
-        /* Filter radius */
+    MTS_DECLARE_CLASS();
+    using Base = mitsuba::ReconstructionFilter<Float>;
+    using Base::init_discretization;
+    using Base::m_radius;
+
+    MitchellNetravaliFilter(const Properties &props) : Base(props) {
+        // Filter radius
         m_radius = 2.0f;
-        /* B parameter from the paper */
+        // B parameter from the paper
         m_b = props.float_("B", 1.f / 3.f);
-        /* C parameter from the paper */
+        // C parameter from the paper
         m_c = props.float_("C", 1.f / 3.f);
         init_discretization();
     }
 
-    template <typename Value> Value eval_impl(Value x) const {
+    Float eval(Float x) const override {
         x = abs(x);
-        Value x2 = sqr(x), x3 = x2*x;
+        Float x2 = sqr(x), x3 = x2*x;
 
-        Value result = (1.f / 6.f) * select(
+        Float result = (1.f / 6.f) * select(
            x < 1,
            (12.f - 9.f * m_b - 6.f * m_c) * x3 +
                (-18.f + 12.f * m_b + 6.f * m_c) * x2 + (6.f - 2.f * m_b),
@@ -41,12 +47,9 @@ public:
         return tfm::format("MitchellNetravaliFilter[radius=%f, B=%f, C=%f]", m_radius, m_b, m_c);
     }
 
-    MTS_IMPLEMENT_RFILTER_ALL()
-    MTS_DECLARE_CLASS()
 protected:
     Float m_b, m_c;
 };
 
-MTS_IMPLEMENT_CLASS(MitchellNetravaliFilter, ReconstructionFilter);
-MTS_EXPORT_PLUGIN(MitchellNetravaliFilter, "Mitchell-Netravali filter");
+MTS_IMPLEMENT_PLUGIN(MitchellNetravaliFilter, ReconstructionFilter, "Mitchell-Netravali filter");
 NAMESPACE_END(mitsuba)
