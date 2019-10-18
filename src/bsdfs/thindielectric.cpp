46e6ddb8cc2a945a5d47a8f2f5b452ab197697f0
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-18 13:54:30 +0200

    ThinDielectricBSDF: templatize

--- src/bsdfs/thindielectric.cpp
+++ src/bsdfs/thindielectric.cpp
@@ -8,9 +8,14 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
-class ThinDielectric final : public BSDF {
+template <typename Float, typename Spectrum>
+class ThinDielectric final : public BSDF<Float, Spectrum> {
 public:
-    ThinDielectric(const Properties &props) : BSDF(props) {
+    MTS_DECLARE_PLUGIN();
+    MTS_USING_BASE(BSDF, Base, m_flags, m_components)
+    using ContinuousSpectrum = typename Aliases::ContinuousSpectrum;
+
+    ThinDielectric(const Properties &props) : Base(props) {
         // Specifies the internal index of refraction at the interface
         Float int_ior = lookup_ior(props, "int_ior", "bk7");
         // Specifies the external index of refraction at the interface
@@ -22,40 +27,32 @@ public:
 
         m_eta = int_ior / ext_ior;
 
-        m_specular_reflectance   = props.spectrum("specular_reflectance",   1.f);
-        m_specular_transmittance = props.spectrum("specular_transmittance", 1.f);
+        m_specular_reflectance   = props.spectrum<Float, Spectrum>("specular_reflectance", 1.f);
+        m_specular_transmittance = props.spectrum<Float, Spectrum>("specular_transmittance", 1.f);
 
-        m_flags = ENull | EDeltaReflection | EFrontSide | EBackSide;
-        m_components.push_back(EDeltaReflection | EFrontSide | EBackSide);
-        m_components.push_back(ENull | EFrontSide | EBackSide);
+        m_components.push_back(BSDFFlags::DeltaReflection | BSDFFlags::FrontSide |
+                               BSDFFlags::BackSide);
+        m_components.push_back(BSDFFlags::Null | BSDFFlags::FrontSide | BSDFFlags::BackSide);
+        m_flags = m_components[0] | m_components[1];
     }
 
-    template <typename SurfaceInteraction,
-              typename BSDFSample = BSDFSample<typename SurfaceInteraction::Point3>,
-              typename Value      = typename SurfaceInteraction::Value,
-              typename Point2     = typename SurfaceInteraction::Point2,
-              typename Spectrum   = Spectrum<Value>>
     MTS_INLINE
-    std::pair<BSDFSample, Spectrum> sample_impl(
-            const BSDFContext &ctx, const SurfaceInteraction &si,
-            Value sample1, const Point2 &/*sample2*/, mask_t<Value> active) const {
-        using Index   = typename BSDFSample::Index;
-        using Vector3 = typename BSDFSample::Vector3;
-        using Frame   = Frame<Vector3>;
-        using Mask    = mask_t<Value>;
+    std::pair<BSDFSample3f, Spectrum> sample(const BSDFContext &ctx, const SurfaceInteraction3f &si,
+                                           Float sample1, const Point2f & /*sample2*/,
+                                           Mask active) const override {
 
-        bool has_reflection   = ctx.is_enabled(EDeltaReflection, 0),
-             has_transmission = ctx.is_enabled(ENull, 1);
+        bool has_reflection   = ctx.is_enabled(BSDFFlags::DeltaReflection, 0),
+             has_transmission = ctx.is_enabled(BSDFFlags::Null, 1);
 
-        Value r = std::get<0>(fresnel(abs(Frame::cos_theta(si.wi)), Value(m_eta)));
+        Float r = std::get<0>(fresnel(abs(Frame3f::cos_theta(si.wi)), Float(m_eta)));
 
-        /* Account for internal reflections: r' = r + trt + tr^3t + .. */
+        // Account for internal reflections: r' = r + trt + tr^3t + ..
         r *= 2.f / (1.f + r);
 
-        Value t = 1.f - r;
+        Float t = 1.f - r;
 
-        /* Select the lobe to be sampled */
-        BSDFSample bs;
+        // Select the lobe to be sampled
+        BSDFSample3f bs;
         Spectrum weight;
         Mask selected_r;
         if (likely(has_reflection && has_transmission)) {
@@ -74,8 +71,8 @@ public:
 
         bs.wo = select(selected_r, reflect(si.wi), -si.wi);
         bs.eta = 1.f;
-        bs.sampled_component = select(selected_r, Index(0), Index(1));
-        bs.sampled_type = select(selected_r, Index(EDeltaReflection), Index(ENull));
+        bs.sampled_component = select(selected_r, UInt32(0), UInt32(1));
+        bs.sampled_type      = select(selected_r, +BSDFFlags::DeltaReflection, +BSDFFlags::Null);
 
         if (any_or<true>(selected_r))
             weight[selected_r] *= m_specular_reflectance->eval(si, selected_r);
@@ -87,20 +84,14 @@ public:
         return { bs, select(active, weight, 0.f) };
     }
 
-    template <typename SurfaceInteraction, typename Vector3,
-              typename Value    = typename SurfaceInteraction::Value,
-              typename Spectrum = Spectrum<Value>>
     MTS_INLINE
-    Spectrum eval_impl(const BSDFContext &/* ctx */, const SurfaceInteraction &/* si */,
-                       const Vector3 &/* wo */, mask_t<Value> /* active */) const {
+    Spectrum eval(const BSDFContext & /*ctx*/, const SurfaceInteraction3f & /*si*/,
+                  const Vector3f & /*wo*/, Mask /*active*/) const override {
         return 0.f;
     }
 
-    template <typename SurfaceInteraction, typename Vector3,
-              typename Value = value_t<Vector3>>
-    MTS_INLINE
-    Value pdf_impl(const BSDFContext &/* ctx */, const SurfaceInteraction & /* si */,
-                   const Vector3 &/* wo */, mask_t<Value> /* active */) const {
+    MTS_INLINE Float pdf(const BSDFContext & /*ctx*/, const SurfaceInteraction3f & /*si*/,
+                         const Vector3f & /*wo*/, Mask /*active*/) const override {
         return 0.f;
     }
 
@@ -114,15 +105,11 @@ public:
         return oss.str();
     }
 
-    MTS_IMPLEMENT_BSDF_ALL()
-    MTS_DECLARE_CLASS()
 private:
     Float m_eta;
     ref<ContinuousSpectrum> m_specular_transmittance;
     ref<ContinuousSpectrum> m_specular_reflectance;
 };
 
-MTS_IMPLEMENT_CLASS(ThinDielectric, BSDF)
-MTS_EXPORT_PLUGIN(ThinDielectric, "Thin dielectric")
-
+MTS_IMPLEMENT_PLUGIN(ThinDielectric, BSDF, "Thin dielectric")
 NAMESPACE_END(mitsuba)
