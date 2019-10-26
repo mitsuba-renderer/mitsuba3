e19f1dd0f1db150ae7cfbe7279aa5eca58dc982b
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-26 15:19:34 +0200

    PathIntegrator: templatize

--- src/integrators/path.cpp
+++ src/integrators/path.cpp
@@ -9,38 +9,31 @@
 
 NAMESPACE_BEGIN(mitsuba)
 
-class PathIntegrator : public MonteCarloIntegrator {
+template <typename Float, typename Spectrum>
+class PathIntegrator : public MonteCarloIntegrator<Float, Spectrum> {
 public:
-    PathIntegrator(const Properties &props) : MonteCarloIntegrator(props) { }
+    MTS_DECLARE_PLUGIN()
+    MTS_IMPORT_OBJECT_TYPES()
+    MTS_USING_BASE(MonteCarloIntegrator, m_max_depth, m_rr_depth)
 
-    template <typename RayDifferential,
-              typename Point3 = typename RayDifferential::Point,
-              typename RadianceSample = RadianceSample<Point3>,
-              typename Value = typename RayDifferential::Value>
-    auto eval_impl(RayDifferential ray, RadianceSample &rs,
-                   mask_t<Value> active) const {
-        using Mask                = mask_t<Value>;
-        using Spectrum            = mitsuba::Spectrum<Value>;
-        using DirectionSample     = mitsuba::DirectionSample<Point3>;
-        using SurfaceInteraction3 = SurfaceInteraction<Point3>;
-        using BSDFPtr             = replace_scalar_t<Value, const BSDF *>;
-        using EmitterPtr          = replace_scalar_t<Value, const Emitter *>;
-        using Vector3             = vector3_t<Point3>;
+    PathIntegrator(const Properties &props) : Base(props) { }
 
-        const Scene *scene = rs.scene;
+    std::pair<Spectrum, Mask> sample(const Scene *scene, Sampler *sampler,
+                                     const RayDifferential3f &ray_, Mask active) const override {
+        RayDifferential3f ray = ray_;
 
-        /* Tracks radiance scaling due to index of refraction changes */
-        Value eta(1.f);
+        // Tracks radiance scaling due to index of refraction changes
+        Float eta(1.f);
 
-        /* MIS weight for intersected emitters (set by prev. iteration) */
-        Value emission_weight(1.f);
+        // MIS weight for intersected emitters (set by prev. iteration)
+        Float emission_weight(1.f);
 
         Spectrum throughput(1.f), result(0.f);
 
         /* ---------------------- First intersection ---------------------- */
 
-        SurfaceInteraction3 si = rs.ray_intersect(ray, active);
-        rs.alpha = select(si.is_valid(), Value(1.f), Value(0.f));
+        SurfaceInteraction3f si = scene->ray_intersect(ray, active);
+        Mask valid_ray = select(si.is_valid(), Float(1.f), Float(0.f));
         EmitterPtr emitter = si.emitter(scene);
 
         for (int depth = 1;; ++depth) {
@@ -57,8 +50,8 @@ public:
                index boundaries. Stop with at least some probability to avoid
                getting stuck (e.g. due to total internal reflection) */
             if (depth > m_rr_depth) {
-                Value q = min(hmax(throughput) * sqr(eta), .95f);
-                active &= rs.next_1d(active) < q;
+                Float q = min(hmax(depolarize(throughput)) * sqr(eta), .95f);
+                active &= sampler->next_1d(active) < q;
                 throughput *= rcp(q);
             }
 
@@ -69,32 +62,30 @@ public:
 
             BSDFContext ctx;
             BSDFPtr bsdf = si.bsdf(ray);
-            Mask active_e = active && neq(bsdf->flags() & BSDF::ESmooth, 0u);
+            Mask active_e = active && has_flag(bsdf->flags(), BSDFFlags::Smooth);
 
             if (likely(any_or<true>(active_e))) {
                 auto [ds, emitter_val] = scene->sample_emitter_direction(
-                    si, rs.next_2d(active_e), true, active_e);
+                    si, sampler->next_2d(active_e), true, active_e);
                 active_e &= neq(ds.pdf, 0.f);
 
-                /* Query the BSDF for that emitter-sampled direction */
-                Vector3 wo = si.to_local(ds.d);
+                // Query the BSDF for that emitter-sampled direction
+                Vector3f wo = si.to_local(ds.d);
                 Spectrum bsdf_val = bsdf->eval(ctx, si, wo, active_e);
 
-                /* Determine probability of having sampled that same
-                   direction using BSDF sampling. */
-                Value bsdf_pdf = bsdf->pdf(ctx, si, wo, active_e);
+                // Determine probability of having sampled that same direction using BSDF sampling.
+                Float bsdf_pdf = bsdf->pdf(ctx, si, wo, active_e);
 
-                Value mis = select(ds.delta, Value(1.f),
-                    mis_weight(ds.pdf, bsdf_pdf));
+                Float mis = select(ds.delta, Float(1.f), mis_weight(ds.pdf, bsdf_pdf));
                 result[active_e] += throughput * emitter_val * bsdf_val * mis;
             }
 
             /* ----------------------- BSDF sampling ---------------------- */
             // Sample BSDF * cos(theta)
-            auto [bs, bsdf_val] = bsdf->sample(ctx, si, rs.next_1d(active),
-                                               rs.next_2d(active), active);
+            auto [bs, bsdf_val] = bsdf->sample(ctx, si, sampler->next_1d(active),
+                                               sampler->next_2d(active), active);
             throughput *= bsdf_val;
-            active &= any(neq(throughput, 0.f));
+            active &= any(neq(depolarize(throughput), 0.f));
             if (none_or<false>(active))
                 break;
 
@@ -102,17 +93,17 @@ public:
 
             // Intersect the BSDF ray against the scene geometry
             ray = si.spawn_ray(si.to_world(bs.wo));
-            SurfaceInteraction3 si_bsdf = scene->ray_intersect(ray, active);
+            SurfaceInteraction3f si_bsdf = scene->ray_intersect(ray, active);
 
             /* Determine probability of having sampled that same
                direction using emitter sampling. */
             emitter = si_bsdf.emitter(scene, active);
-            DirectionSample ds(si_bsdf, si);
+            DirectionSample3f ds(si_bsdf, si);
             ds.object = emitter;
 
             if (any_or<true>(neq(emitter, nullptr))) {
-                Value emitter_pdf =
-                    select(neq(bs.sampled_type & BSDF::EDelta, 0u), 0.f,
+                Float emitter_pdf =
+                    select(has_flag(bs.sampled_type, BSDFFlags::Delta), 0.f,
                            scene->pdf_emitter_direction(si, ds, active));
 
                 emission_weight = mis_weight(bs.pdf, emitter_pdf);
@@ -121,7 +112,7 @@ public:
             si = std::move(si_bsdf);
         }
 
-        return result;
+        return { result, valid_ray };
     }
 
     //! @}
@@ -134,18 +125,12 @@ public:
             "]", m_max_depth, m_rr_depth);
     }
 
-    MTS_IMPLEMENT_INTEGRATOR_ALL()
-    MTS_DECLARE_CLASS()
-
-protected:
-    template <typename Value> Value mis_weight(Value pdf_a, Value pdf_b) const {
+    Float mis_weight(Float pdf_a, Float pdf_b) const {
         pdf_a *= pdf_a;
         pdf_b *= pdf_b;
-        return select(pdf_a > 0.f, pdf_a / (pdf_a + pdf_b), Value(0.f));
+        return select(pdf_a > 0.f, pdf_a / (pdf_a + pdf_b), Float(0.f));
     };
 };
 
-
-MTS_IMPLEMENT_CLASS(PathIntegrator, MonteCarloIntegrator);
-MTS_EXPORT_PLUGIN(PathIntegrator, "Path Tracer integrator");
+MTS_IMPLEMENT_PLUGIN(PathIntegrator, MonteCarloIntegrator, "Path Tracer integrator");
 NAMESPACE_END(mitsuba)
