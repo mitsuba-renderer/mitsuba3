c180ec207b34caa4503986f458875a6d99080272
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-10-26 14:57:51 +0200

    DepthIntegrator: templatize

--- src/integrators/depth.cpp
+++ src/integrators/depth.cpp
@@ -8,22 +8,21 @@ NAMESPACE_BEGIN(mitsuba)
  * camera to the closest intersected object, or 0 if no intersection was found.
  */
 template <typename Float, typename Spectrum>
-class DepthIntegrator : public SamplingIntegrator<Float, Spectrum> {
-    /// Declare runtime type information for Mitsuba's object model
-    MTS_DECLARE_CLASS()
-
-    /// Import types derived from Float and Spectrum (e.g. Vector3f, etc.)
-    MTS_IMPORT_TYPES()
+class DepthIntegrator final : public SamplingIntegrator<Float, Spectrum> {
 public:
-    DepthIntegrator(const Properties &props) : SamplingIntegrator(props) {}
+    MTS_DECLARE_PLUGIN()
+    MTS_IMPORT_OBJECT_TYPES()
+    using Base = SamplingIntegrator<Float, Spectrum>;
 
-    std::pair<Spectrum, Mask> eval(const Scene *scene, Sampler *sampler,
-                                   const RayDifferential3f &ray, Mask active) const {
+
+    DepthIntegrator(const Properties &props) : Base(props) {}
+
+    std::pair<Spectrum, Mask> sample(const Scene *scene, Sampler * /*sampler*/,
+                                     const RayDifferential3f &ray, Mask active) const override {
         SurfaceInteraction3f si = scene->ray_intersect(ray, active);
         return { select(si.is_valid(), si.t, 0.f), si.is_valid() };
     }
 };
 
-MTS_IMPLEMENT_CLASS(DepthIntegrator, SamplingIntegrator);
-MTS_EXPORT_PLUGIN(DepthIntegrator, "Depth integrator");
+MTS_IMPLEMENT_PLUGIN(DepthIntegrator, SamplingIntegrator, "Depth integrator");
 NAMESPACE_END(mitsuba)
