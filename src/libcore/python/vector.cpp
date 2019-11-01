7f51d3876f52579276e2ee4b56b68bcd001733c1
Sebastien Speierer <sebastieneps@gmail.com>
2019-11-01 10:56:39 +0100

    Update vector bindings

--- src/libcore/python/vector.cpp
+++ src/libcore/python/vector.cpp
@@ -1,11 +1,9 @@
 #include <mitsuba/core/vector.h>
 #include <mitsuba/python/python.h>
 
-MTS_PY_EXPORT(vector) {
-    m.def("coordinate_system", &coordinate_system<Vector3f>,
-          "n"_a, D(coordinate_system));
-
+MTS_PY_EXPORT_FLOAT_VARIANTS(vector) {
+    MTS_IMPORT_CORE_TYPES()
     m.def("coordinate_system",
-          vectorize_wrapper(&coordinate_system<Vector3fP>),
-          "n"_a);
+          vectorize<Float>(&coordinate_system<Vector3f>),
+          "n"_a, D(coordinate_system));
 }
