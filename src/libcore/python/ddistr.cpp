a32e35c6bab5cdece162b42f64462fb823c26a6e
Sebastien Speierer <sebastieneps@gmail.com>
2019-11-01 15:18:44 +0100

    Update DDistr bindings

--- src/libcore/python/ddistr.cpp
+++ src/libcore/python/ddistr.cpp
@@ -2,10 +2,13 @@
 #include <mitsuba/python/python.h>
 
 MTS_PY_EXPORT(DiscreteDistribution) {
+    // TODO bind for all different Float types
+    using Float = float;
+    MTS_IMPORT_CORE_TYPES()
     MTS_PY_STRUCT(DiscreteDistribution)
         .def(py::init<size_t>(), py::arg("n_entries") = 0,
              D(DiscreteDistribution, DiscreteDistribution))
-        .def(py::init([](py::array_t<Float> data) {
+        .def(py::init([](py::array_t<float> data) {
             if (data.ndim() != 1)
                 throw std::domain_error("array has incorrect dimension");
             return DiscreteDistribution(
@@ -17,52 +20,21 @@ MTS_PY_EXPORT(DiscreteDistribution) {
         .def_method(DiscreteDistribution, reserve)
         .def_method(DiscreteDistribution, append)
         .def_method(DiscreteDistribution, size)
-
-        .def("eval", [](const DiscreteDistribution &d, size_t i) {
-            return d.eval(i);
-        }, D(DiscreteDistribution, eval), "index"_a)
-
-        .def("eval", vectorize_wrapper(
-            [](const DiscreteDistribution &d, SizeP i, mask_t<SizeP> active) {
-                return d.eval(i, active);
-            }
-        ), D(DiscreteDistribution, eval), "i"_a, "active"_a = true)
-
+        .def("eval", vectorize<Float>(&DiscreteDistribution::eval<UInt32>),
+             "index"_a , "active"_a = true, D(DiscreteDistribution, eval))
         .def_method(DiscreteDistribution, normalized)
         .def_method(DiscreteDistribution, sum)
         .def_method(DiscreteDistribution, normalization)
         .def_method(DiscreteDistribution, cdf)
         .def_method(DiscreteDistribution, normalize)
-
-        // ---------------------------------------------------------------------
-
-        .def("sample", [](const DiscreteDistribution &d, Float arg) { return d.sample(arg); },
-             "sample_value"_a, D(DiscreteDistribution, sample))
-        .def("sample", vectorize_wrapper(&DiscreteDistribution::sample<FloatP>),
+        .def("sample", vectorize<Float>(&DiscreteDistribution::sample<Float>),
              "sample_value"_a, "active"_a = true)
-
-        // ---------------------------------------------------------------------
-
-        .def("sample_pdf", [](const DiscreteDistribution &d, Float arg) { return d.sample_pdf(arg); },
-             "sample_value"_a, D(DiscreteDistribution, sample_pdf))
-        .def("sample_pdf", vectorize_wrapper(&DiscreteDistribution::sample_pdf<FloatP>),
+        .def("sample_pdf", vectorize<Float>(&DiscreteDistribution::sample_pdf<Float>),
              "sample_value"_a, "active"_a = true)
-
-        // ---------------------------------------------------------------------
-
-        .def("sample_reuse", [](const DiscreteDistribution &d, Float arg) { return d.sample_reuse(arg); },
-             "sample_value"_a, D(DiscreteDistribution, sample_reuse))
-        .def("sample_reuse", vectorize_wrapper(&DiscreteDistribution::sample_reuse<FloatP>),
+        .def("sample_reuse", vectorize<Float>(&DiscreteDistribution::sample_reuse<Float>),
              "sample_value"_a, "active"_a = true)
-
-        // ---------------------------------------------------------------------
-
-        .def("sample_reuse_pdf", [](const DiscreteDistribution &d, Float arg) { return d.sample_reuse_pdf(arg); },
-             "sample_value"_a, D(DiscreteDistribution, sample_reuse_pdf))
-        .def("sample_reuse_pdf", vectorize_wrapper(&DiscreteDistribution::sample_reuse_pdf<FloatP>),
+        .def("sample_reuse_pdf", vectorize<Float>(&DiscreteDistribution::sample_reuse_pdf<Float>),
              "sample_value"_a, "active"_a = true)
-
-        // ---------------------------------------------------------------------
-
-        .def_repr(DiscreteDistribution);
+        .def_repr(DiscreteDistribution)
+        ;
 }
