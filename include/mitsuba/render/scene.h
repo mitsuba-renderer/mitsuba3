ed037053bfa2a61f04e8642b3212d9b63b12fa4f
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-11-07 14:04:40 +0100

    Scene: fix missing alias

--- include/mitsuba/render/scene.h
+++ include/mitsuba/render/scene.h
@@ -11,7 +11,7 @@ NAMESPACE_BEGIN(mitsuba)
 template <typename Float, typename Spectrum>
 class MTS_EXPORT_RENDER Scene : public Object {
 public:
-    MTS_DECLARE_CLASS_VARIANT(Scene, Object)
+    MTS_DECLARE_CLASS_VARIANT(Scene, Object, "scene")
     MTS_IMPORT_TYPES();
     using Sampler              = typename RenderAliases::Sampler;
     using Shape                = typename RenderAliases::Shape;
