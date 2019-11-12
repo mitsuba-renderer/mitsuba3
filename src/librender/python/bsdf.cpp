0607f60e9c67356a6ce131e92d5a31fd320576af
Sebastien Speierer <sebastieneps@gmail.com>
2019-11-12 17:42:17 +0100

    Fix BSDF python bindings

--- src/librender/python/bsdf.cpp
+++ src/librender/python/bsdf.cpp
@@ -8,7 +8,40 @@ MTS_PY_EXPORT_VARIANTS(BSDF) {
     using BSDFP = mitsuba::BSDF<FloatP, SpectrumP>;
     using Mask = typename BSDF::Mask;
 
-    MTS_PY_CHECK_ALIAS(BSDFSample3, m) {
+    MTS_PY_CHECK_ALIAS(TransportMode, m) {
+        py::enum_<TransportMode>(m, "TransportMode", D(TransportMode))
+            .value("Radiance", TransportMode::Radiance, D(TransportMode, Radiance))
+            .value("Importance", TransportMode::Importance, D(TransportMode, Importance))
+            .export_values();
+    }
+
+    MTS_PY_CHECK_ALIAS(BSDFFlags, m) {
+        py::enum_<BSDFFlags>(m, "BSDFFlags", D(BSDFFlags), py::arithmetic())
+            .value("None", BSDFFlags::None, D(BSDFFlags, None))
+            .value("Null", BSDFFlags::Null, D(BSDFFlags, Null))
+            .value("DiffuseReflection", BSDFFlags::DiffuseReflection, D(BSDFFlags, DiffuseReflection))
+            .value("DiffuseTransmission", BSDFFlags::DiffuseTransmission, D(BSDFFlags, DiffuseTransmission))
+            .value("GlossyReflection", BSDFFlags::GlossyReflection, D(BSDFFlags, GlossyReflection))
+            .value("GlossyTransmission", BSDFFlags::GlossyTransmission, D(BSDFFlags, GlossyTransmission))
+            .value("DeltaReflection", BSDFFlags::DeltaReflection, D(BSDFFlags, DeltaReflection))
+            .value("DeltaTransmission", BSDFFlags::DeltaTransmission, D(BSDFFlags, DeltaTransmission))
+            .value("Anisotropic", BSDFFlags::Anisotropic, D(BSDFFlags, Anisotropic))
+            .value("SpatiallyVarying", BSDFFlags::SpatiallyVarying, D(BSDFFlags, SpatiallyVarying))
+            .value("NonSymmetric", BSDFFlags::NonSymmetric, D(BSDFFlags, NonSymmetric))
+            .value("FrontSide", BSDFFlags::FrontSide, D(BSDFFlags, FrontSide))
+            .value("BackSide", BSDFFlags::BackSide, D(BSDFFlags, BackSide))
+            .value("Reflection", BSDFFlags::Reflection, D(BSDFFlags, Reflection))
+            .value("Transmission", BSDFFlags::Transmission, D(BSDFFlags, Transmission))
+            .value("Diffuse", BSDFFlags::Diffuse, D(BSDFFlags, Diffuse))
+            .value("Glossy", BSDFFlags::Glossy, D(BSDFFlags, Glossy))
+            .value("Smooth", BSDFFlags::Smooth, D(BSDFFlags, Smooth))
+            .value("Delta", BSDFFlags::Delta, D(BSDFFlags, Delta))
+            .value("Delta1D", BSDFFlags::Delta1D, D(BSDFFlags, Delta1D))
+            .value("All", BSDFFlags::All, D(BSDFFlags, All))
+            .export_values();
+    }
+
+    MTS_PY_CHECK_ALIAS(BSDFContext, m) {
         py::class_<BSDFContext>(m, "BSDFContext", D(BSDFContext))
             .def(py::init<TransportMode>(),
                 "mode"_a = TransportMode::Radiance, D(BSDFContext, BSDFContext))
@@ -54,38 +87,5 @@ MTS_PY_EXPORT_VARIANTS(BSDF) {
             .def_method(BSDF, component_count)
             .def_method(BSDF, id)
             .def("__repr__", &BSDF::to_string);
-    }
-
-    MTS_PY_CHECK_ALIAS(BSDF, m) {
-        py::enum_<TransportMode>(m, "TransportMode", D(TransportMode))
-            .value("Radiance", TransportMode::Radiance, D(TransportMode, Radiance))
-            .value("Importance", TransportMode::Importance, D(TransportMode, Importance))
-            .export_values();
-    }
-
-    MTS_PY_CHECK_ALIAS(BSDF, m) {
-        py::enum_<BSDFFlags>(m, "BSDFFlags", D(BSDFFlags), py::arithmetic())
-            .value("None", BSDFFlags::None, D(BSDFFlags, None))
-            .value("Null", BSDFFlags::Null, D(BSDFFlags, Null))
-            .value("DiffuseReflection", BSDFFlags::DiffuseReflection, D(BSDFFlags, DiffuseReflection))
-            .value("DiffuseTransmission", BSDFFlags::DiffuseTransmission, D(BSDFFlags, DiffuseTransmission))
-            .value("GlossyReflection", BSDFFlags::GlossyReflection, D(BSDFFlags, GlossyReflection))
-            .value("GlossyTransmission", BSDFFlags::GlossyTransmission, D(BSDFFlags, GlossyTransmission))
-            .value("DeltaReflection", BSDFFlags::DeltaReflection, D(BSDFFlags, DeltaReflection))
-            .value("DeltaTransmission", BSDFFlags::DeltaTransmission, D(BSDFFlags, DeltaTransmission))
-            .value("Anisotropic", BSDFFlags::Anisotropic, D(BSDFFlags, Anisotropic))
-            .value("SpatiallyVarying", BSDFFlags::SpatiallyVarying, D(BSDFFlags, SpatiallyVarying))
-            .value("NonSymmetric", BSDFFlags::NonSymmetric, D(BSDFFlags, NonSymmetric))
-            .value("FrontSide", BSDFFlags::FrontSide, D(BSDFFlags, FrontSide))
-            .value("BackSide", BSDFFlags::BackSide, D(BSDFFlags, BackSide))
-            .value("Reflection", BSDFFlags::Reflection, D(BSDFFlags, Reflection))
-            .value("Transmission", BSDFFlags::Transmission, D(BSDFFlags, Transmission))
-            .value("Diffuse", BSDFFlags::Diffuse, D(BSDFFlags, Diffuse))
-            .value("Glossy", BSDFFlags::Glossy, D(BSDFFlags, Glossy))
-            .value("Smooth", BSDFFlags::Smooth, D(BSDFFlags, Smooth))
-            .value("Delta", BSDFFlags::Delta, D(BSDFFlags, Delta))
-            .value("Delta1D", BSDFFlags::Delta1D, D(BSDFFlags, Delta1D))
-            .value("All", BSDFFlags::All, D(BSDFFlags, All))
-            .export_values();
     }
 }
