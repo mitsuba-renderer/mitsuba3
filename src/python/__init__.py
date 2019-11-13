a71d097a7167a032c2efdbd8fe64f38b52066c70
Merlin Nimier-David <merlin.nimier@gmail.com>
2019-11-13 13:50:25 +0100

    Python: fix error message when importing with different version

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -13,9 +13,9 @@ except ImportError as e:
     from .config import PYTHON_EXECUTABLE
 
     if PYTHON_EXECUTABLE != sys.executable:
-        extra_msg = "You're likely trying to use Mitsuba within a Python "
+        extra_msg = ("You're likely trying to use Mitsuba within a Python "
         "binary (%s) that is different from the one for which the native "
-        "module was compiled (%s)." % (sys.executable, PYTHON_EXECUTABLE)
+        "module was compiled (%s).") % (sys.executable, PYTHON_EXECUTABLE)
     else:
         extra_msg = ""
 
