970290e995f5fb339e58810e02bbb2937fdf1784
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-03-02 14:51:38 +0100

    Separated out ParameterMap into top-level class

--- src/python/python/util.py
+++ src/python/python/util.py
@@ -1,32 +1,111 @@
-def traverse(node):
+import mitsuba
+
+
+class ParameterMap:
+    """
+    Dictionary-like object that references various parameters used in a Mitsuba
+    scene graph. Parameters can be read and written using standard syntax
+    (``parameter_map[key]``). The class exposes several non-standard functions,
+    specifically :py:meth:`~mitsuba.python.autodiff.ParameterMap.torch()`,
+    :py:meth:`~mitsuba.python.autodiff.ParameterMap.update()`, and
+    :py:meth:`~mitsuba.python.autodiff.ParameterMap.keep()`.
+    """
+
+    def __init__(self, properties, hierarchy):
+        """
+        Private constructor (use
+        :py:func:`mitsuba.python.autodiff.traverse()` instead)
+        """
+        self.properties = properties
+        self.hierarchy = hierarchy
+        self.update_list = []
+
+        from mitsuba.core import set_property, get_property
+        self.set_property = set_property
+        self.get_property = get_property
+
+    def __contains__(self, key: str):
+        return self.properties.__contains__(key)
+
+    def __getitem__(self, key: str):
+        return self.get_property(*(self.properties[key]))
+
+    def __setitem__(self, key: str, value):
+        item = self.properties[key]
+        node = item[2]
+        while node is not None:
+            parent, depth = self.hierarchy[node]
+            self.update_list.append((depth, node))
+            node = parent
+        return self.set_property(item[0], item[1], value)
+
+    def __delitem__(self, key: str) -> None:
+        del self.properties[key]
+
+    def __len__(self) -> int:
+        return len(self.properties)
+
+    def __repr__(self) -> str:
+        return 'ParameterMap[\n    ' + ',\n    '.join(self.keys()) + '\n]'
+
+    def keys(self):
+        return self.properties.keys()
+
+    def items(self):
+        class ParameterMapItemIterator:
+            def __init__(self, pmap):
+                self.pmap = pmap
+                self.it = pmap.keys().__iter__()
+
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                key = next(self.it)
+                return (key, self.pmap[key])
+
+        return ParameterMapItemIterator(self)
+
+    def torch(self) -> dict:
+        """
+        Converts all Enoki arrays into PyTorch arrays and return them as a
+        dictionary. This is mainly useful when using PyTorch to optimize a
+        Mitsuba scene.
+        """
+        return {k: v.torch().requires_grad_() for k, v in self.items()}
+
+    def update(self) -> None:
+        """
+        This function should be called at the end of a sequence of writes
+        to the dictionary. It automatically notifies all modified Mitsuba
+        objects and their parent objects that they should refresh their
+        internal state. For instance, the scene may rebuild the kd-tree
+        when a shape was modified, etc.
+        """
+        work_list = sorted(set(self.update_list), key=lambda x: x[0])
+        for depth, node in reversed(work_list):
+            node.parameters_changed()
+        self.update_list.clear()
+
+    def keep(self, keys: list) -> None:
+        """
+        Reduce the size of the dictionary by only keeping elements,
+        whose keys are part of the provided list 'keys'.
+        """
+        keys = set(keys)
+        self.properties = {
+            k: v for k, v in self.properties.items() if k in keys
+        }
+
+
+def traverse(node: 'mitsuba.core.Object') -> ParameterMap:
     """
     Traverse a node of Mitsuba's scene graph and return a dictionary-like
     object that can be used to read and write associated scene parameters.
 
-    This dictionary exposes multiple non-standard methods:
-
-    1. ``keep(self, keys: list) -> None``:
-
-       Reduce the size of the dictionary by only keeping elements,
-       whose keys are part of the provided list 'keys'.
-
-    2. ``update(self) -> None``:
-
-       This function should be called at the end of a sequence of writes
-       to the dictionary. It automatically notifies all modified Mitsuba
-       objects and their parent objects that they should refresh their
-       internal state. For instance, the scene may rebuild the kd-tree
-       when a shape was modified, etc.
-
-    3. ``torch(self) -> dict``:
-
-       Converts all Enoki arrays into PyTorch arrays and return them as a
-       dictionary. This is mainly useful when using PyTorch to optimize a
-       Mitsuba scene.
-
+    See also :py:class:`mitsuba.python.autodiff.ParameterMap`.
     """
-    from mitsuba.core import TraversalCallback, \
-        set_property, get_property
+    from mitsuba.core import TraversalCallback
 
     class SceneTraversal(TraversalCallback):
         def __init__(self, node, parent=None, properties=None,
@@ -68,68 +147,5 @@ def traverse(node):
 
     cb = SceneTraversal(node)
     node.traverse(cb)
-
-    class ParameterMap:
-        def __init__(self, properties, hierarchy):
-            self.properties = properties
-            self.hierarchy = hierarchy
-            self.update_list = []
-
-        def __contains__(self, key):
-            return self.properties.__contains__(key)
-
-        def __getitem__(self, key):
-            return get_property(*(self.properties[key]))
-
-        def __setitem__(self, key, value):
-            item = self.properties[key]
-            node = item[2]
-            while node is not None:
-                parent, depth = self.hierarchy[node]
-                self.update_list.append((depth, node))
-                node = parent
-            return set_property(item[0], item[1], value)
-
-        def __delitem__(self, key):
-            del self.properties[key]
-
-        def __len__(self):
-            return len(self.properties)
-
-        def __repr__(self):
-            return 'ParameterMap[\n    ' + ',\n    '.join(self.keys()) + '\n]'
-
-        def keys(self):
-            return self.properties.keys()
-
-        def items(self):
-            class ParameterMapItemIterator:
-                def __init__(self, pmap):
-                    self.pmap = pmap
-                    self.it = pmap.keys().__iter__()
-
-                def __iter__(self):
-                    return self
-
-                def __next__(self):
-                    key = next(self.it)
-                    return (key, self.pmap[key])
-
-            return ParameterMapItemIterator(self)
-
-        def torch(self):
-            return {k: v.torch().requires_grad_() for k, v in self.items()}
-
-        def update(self):
-            work_list = sorted(set(self.update_list), key=lambda x: x[0])
-            for depth, node in reversed(work_list):
-                node.parameters_changed()
-            self.update_list.clear()
-
-        def keep(self, keys):
-            keys = set(keys)
-            self.properties = {
-                k: v for k, v in self.properties.items() if k in keys
-            }
 
     return ParameterMap(cb.properties, cb.hierarchy)
