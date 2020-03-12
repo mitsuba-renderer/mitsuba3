74b2bf658c7fd83271a2cce50e7c774bcdc73641
Sebastien Speierer <sebastieneps@gmail.com>
2020-03-12 17:11:37 +0100

    Moment integrator and Z-test (part 1)

---
+++ src/integrators/moment.cpp
@@ -0,0 +1,132 @@
+#include <mitsuba/render/integrator.h>
+#include <mitsuba/render/records.h>
+
+NAMESPACE_BEGIN(mitsuba)
+
+
+/**!
+
+.. _integrator-moment:
+
+Moment integrator (:monosp:`moment`)
+-----------------------------------------------------
+
+.. pluginparameters::
+
+ * - (Nested plugin)
+   - :paramtype:`integrator`
+   - Sub-integrators (can have more than one) which will be sampled along the AOV integrator. Their
+     respective XYZ output will be put into distinct images.
+
+
+This integrator returns one AOVs recording the second moment of the samples of the nested
+integrator.
+
+ */
+
+template <typename Float, typename Spectrum>
+class MomentIntegrator final : public SamplingIntegrator<Float, Spectrum> {
+public:
+    MTS_IMPORT_BASE(SamplingIntegrator)
+    MTS_IMPORT_TYPES(Scene, Sampler)
+
+    MomentIntegrator(const Properties &props) : Base(props) {
+        // Get the nested integrators and their AOVs
+        for (auto &kv : props.objects()) {
+            Base *integrator = dynamic_cast<Base *>(kv.second.get());
+            if (!integrator)
+                Throw("Child objects must be of type 'SamplingIntegrator'!");
+            std::vector<std::string> aovs = integrator->aov_names();
+            for (auto name: aovs)
+                m_aov_names.push_back(kv.first + "." + name);
+            m_integrators.push_back({ integrator, aovs.size() });
+
+            m_aov_names.push_back(kv.first + ".X");
+            m_aov_names.push_back(kv.first + ".Y");
+            m_aov_names.push_back(kv.first + ".Z");
+        }
+
+        // For every AOV, add a corresponding "m2_" AOV
+        size_t aov_count = m_aov_names.size();
+        for (size_t i = 0; i < aov_count; i++)
+            m_aov_names.push_back("m2_" + m_aov_names[i]);
+    }
+
+    std::pair<Spectrum, Mask> sample(const Scene *scene,
+                                     Sampler * sampler,
+                                     const RayDifferential3f &ray,
+                                     Float *aovs,
+                                     Mask active) const override {
+        MTS_MASKED_FUNCTION(ProfilerPhase::SamplingIntegratorSample, active);
+
+        std::pair<Spectrum, Mask> result { 0.f, false };
+
+        size_t offset = m_aov_names.size() / 2;
+
+        for (size_t i = 0; i < m_integrators.size(); i++) {
+            std::pair<Spectrum, Mask> result_sub =
+                m_integrators[i].first->sample(scene, sampler, ray, aovs, active);
+            aovs += m_integrators[i].second;
+
+            UnpolarizedSpectrum spec_u = depolarize(result_sub.first);
+
+            Color3f xyz;
+            if constexpr (is_monochromatic_v<Spectrum>) {
+                xyz = spec_u.x();
+            } else if constexpr (is_rgb_v<Spectrum>) {
+                xyz = srgb_to_xyz(spec_u, active);
+            } else {
+                static_assert(is_spectral_v<Spectrum>);
+                /// Note: this assumes that sensor used sample_rgb_spectrum() to generate 'ray.wavelengths'
+                auto pdf = pdf_rgb_spectrum(ray.wavelengths);
+                spec_u *= select(neq(pdf, 0.f), rcp(pdf), 0.f);
+                xyz = spectrum_to_xyz(spec_u, ray.wavelengths, active);
+            }
+
+            *aovs++ = xyz.x(); *aovs++ = xyz.y(); *aovs++ = xyz.z();
+
+            // Write second moment AOVs
+            for (size_t j = 0; j < m_integrators[i].second + 3; j++)
+                *(aovs - j + offset - 1) = sqr(*(aovs - j - 1));
+
+            if (i == 0)
+                result = result_sub;
+        }
+
+        return result;
+    }
+
+    std::vector<std::string> aov_names() const override {
+        return m_aov_names;
+    }
+
+    void traverse(TraversalCallback *callback) override {
+        for (size_t i = 0; i < m_integrators.size(); ++i)
+            callback->put_object("integrator_" + std::to_string(i), m_integrators[i].first.get());
+    }
+
+    std::string to_string() const override {
+        std::ostringstream oss;
+        oss << "Scene[" << std::endl
+            << "  aovs = " << m_aov_names << "," << std::endl
+            << "  integrators = [" << std::endl;
+        for (size_t i = 0; i < m_integrators.size(); ++i) {
+            oss << "    " << string::indent(m_integrators[i].first->to_string(), 4);
+            if (i + 1 < m_integrators.size())
+                oss << ",";
+            oss << std::endl;
+        }
+        oss << "  ]"<< std::endl
+            << "]";
+        return oss.str();
+    }
+
+    MTS_DECLARE_CLASS()
+private:
+    std::vector<std::string> m_aov_names;
+    std::vector<std::pair<ref<Base>, size_t>> m_integrators;
+};
+
+MTS_IMPLEMENT_CLASS_VARIANT(MomentIntegrator, SamplingIntegrator)
+MTS_EXPORT_PLUGIN(MomentIntegrator, "Moment integrator");
+NAMESPACE_END(mitsuba)
--- src/librender/tests/test_renders.py
+++ src/librender/tests/test_renders.py
@@ -1,5 +1,5 @@
 import os
-from os.path import join, realpath, dirname, basename
+from os.path import join, realpath, dirname, basename, splitext
 import argparse
 import glob
 import mitsuba
@@ -20,23 +20,65 @@ def variants_all(request):
 
 color_modes = ['mono', 'rgb', 'spectral_polarized', 'spectral']
 
-TEST_SCENE_DIR = realpath(join(os.path.dirname(__file__), '../../../resources/data/tests/scenes'))
+TEST_SCENE_DIR = realpath(join(os.path.dirname(
+    __file__), '../../../resources/data/tests/scenes'))
 scenes = glob.glob(join(TEST_SCENE_DIR, '*', '*.xml'))
 
 # Exclude certain tests for now
 EXCLUDE_FOLDERS = ['participating_media']
 
 
-def get_ref_fname(scene_path):
+def get_ref_fname(scene_fname):
     for color_mode in color_modes:
         if color_mode in mitsuba.variant():
-            return join(dirname(scene_path), 'refs', os.path.splitext(basename(scene_path))[0] + '_ref_' + color_mode + '.exr')
+            ref_fname = join(dirname(scene_fname), 'refs', splitext(
+                basename(scene_fname))[0] + '_ref_' + color_mode + '.exr')
+            var_fname = ref_fname.replace('.exr', '_var.exr')
+            return ref_fname, var_fname
     assert False
+
+
+def xyz_to_rgb_bmp(arr):
+    from mitsuba.core import Bitmap, Struct
+    xyz_bmp = Bitmap(arr, Bitmap.PixelFormat.XYZ)
+    return xyz_bmp.convert(Bitmap.PixelFormat.RGB, Struct.Type.Float32, False)
+
+
+def read_rgb_bmp_to_xyz(fname):
+    from mitsuba.core import Bitmap, Struct
+    return Bitmap(fname).convert(Bitmap.PixelFormat.XYZ, Struct.Type.Float32, False)
+
+
+def bitmap_extract(bmp):
+    # AVOs from the moment integrator are in XYZ (float32)
+    split = bmp.split()
+    img = np.array(split[1][1], copy=False)
+    img_m2 = np.array(split[2][1], copy=False)
+    return img, img_m2 - img * img
+
+
+def z_test(mean, sample_count, reference, reference_var):
+    # Sanitize the variance images
+    reference_var = np.maximum(reference_var, 1e-4)
+
+    # Compute Z statistic
+    z_stat = np.abs(mean - reference) * np.sqrt(sample_count / reference_var)
+
+    # Cumulative distribution function of the standard normal distribution
+    def stdnormal_cdf(x):
+        shape = x.shape
+        cdf = (1.0 - ek.erf(-x.flatten() / ek.sqrt(2.0))) * 0.5
+        return np.array(cdf).reshape(shape)
+
+    # Compute p-value
+    p_value = 2.0 * (1.0 - stdnormal_cdf(z_stat))
+
+    return p_value
 
 
 @pytest.mark.parametrize(*['scene_fname', scenes])
 def test_render(variants_all, scene_fname):
-    from mitsuba.core import Bitmap, Struct, Thread
+    from mitsuba.core import Bitmap, Struct, Thread, set_thread_count
 
     scene_dir = dirname(scene_fname)
 
@@ -45,31 +87,74 @@ def test_render(variants_all, scene_fname):
 
     Thread.thread().file_resolver().append(scene_dir)
 
-    ref_fname = get_ref_fname(scene_fname)
+    ref_fname, ref_var_fname = get_ref_fname(scene_fname)
     assert os.path.exists(ref_fname)
 
-    scene = mitsuba.core.xml.load_file(scene_fname, spp=32)
+    ref_bmp = read_rgb_bmp_to_xyz(ref_fname)
+    ref_img = np.array(ref_bmp, copy=False)
+
+    ref_var_bmp = read_rgb_bmp_to_xyz(ref_var_fname)
+    ref_var_img = np.array(ref_var_bmp, copy=False)
+
+    significance_level = 0.01
+
+    # Compute spp budget
+    sample_budget = int(2e6)
+    pixel_count = ek.hprod(ref_bmp.size())
+    spp = sample_budget // pixel_count
+
+    # Load and render
+    scene = mitsuba.core.xml.load_file(scene_fname, spp=spp)
     scene.integrator().render(scene, scene.sensors()[0])
 
-    film = scene.sensors()[0].film()
+    # Compute variance image
+    bmp = scene.sensors()[0].film().bitmap(raw=False)
+    img, var_img = bitmap_extract(bmp)
 
-    cur_bitmap = film.bitmap(raw=True).convert(Bitmap.PixelFormat.RGB, Struct.Type.Float32, False)
-    cur_image = np.array(cur_bitmap, copy=False)
+    # Compute Z-test p-value
+    p_value = z_test(img, spp, ref_img, ref_var_img)
 
-    ref_bitmap = Bitmap(ref_fname).convert(Bitmap.PixelFormat.RGB, Struct.Type.Float32, False)
-    ref_image = np.array(ref_bitmap, copy=False)
+    # Apply the Sidak correction term, since we'll be conducting multiple independent
+    # hypothesis tests. This accounts for the fact that the probability of a failure
+    # increases quickly when several hypothesis tests are run in sequence.
+    alpha = 1.0 - (1.0 - significance_level) ** (1.0 / pixel_count)
 
-    error = np.mean(np.mean(np.abs(ref_image - cur_image)))
-    threshold = 0.5 * np.mean(np.mean(ref_image))
-    success = error < threshold
+    success = (p_value > alpha)
 
-    if not success:
-        print("Failed. error: {} // threshold: {}".format(error, threshold))
+    if (np.count_nonzero(success) / 3) >= (0.9975 * pixel_count):
+    # if np.all(success):
+        print('Accepted the null hypothesis (min(p-value) = %f, significance level = %f)' %
+              (np.min(p_value), alpha))
+    else:
+        print('Reject the null hypothesis (min(p-value) = %f, significance level = %f)' %
+              (np.min(p_value), alpha))
 
-        # Write rendered image to a file
-        cur_fname = os.path.splitext(scene_fname)[0] + '_render_' + mitsuba.variant() + '.exr'
-        cur_bitmap.write(cur_fname)
-        print('Saved rendered image to: ' + cur_fname)
+        output_dir = join(scene_dir, 'error_output')
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        output_prefix = join(output_dir, splitext(basename(scene_fname))[0] + '_' + mitsuba.variant())
+
+        img_rgb_bmp = xyz_to_rgb_bmp(img)
+
+        fname = output_prefix + '_img.exr'
+        img_rgb_bmp.write(fname)
+        print('Saved rendered image to: ' + fname)
+
+        var_fname = output_prefix + '_var.exr'
+        xyz_to_rgb_bmp(var_img).write(var_fname)
+        print('Saved variance image to: ' + var_fname)
+
+        err_fname = output_prefix + '_error.exr'
+        err_img = 0.02 * np.array(img_rgb_bmp)
+        err_img[~success] = 1.0
+        err_bmp = Bitmap(err_img).write(err_fname)
+        print('Saved error image to: ' + err_fname)
+
+        pvalue_fname = output_prefix + '_pvalue.exr'
+        xyz_to_rgb_bmp(p_value).write(pvalue_fname)
+        print('Saved error image to: ' + pvalue_fname)
 
         assert False
 
@@ -83,8 +168,8 @@ def main():
     parser = argparse.ArgumentParser(prog='RenderReferenceImages')
     parser.add_argument('--overwrite', action='store_true',
                         help='Force rerendering of all reference images. Otherwise, only missing references will be rendered.')
-    parser.add_argument('--spp', default=256, type=int,
-                        help='Samples per pixel')
+    parser.add_argument('--spp', default=32000, type=int,
+                        help='Samples per pixel. Default value: 32000')
     args = parser.parse_args()
 
     ref_spp = args.spp
@@ -93,27 +178,36 @@ def main():
     for scene_fname in scenes:
         scene_dir = dirname(scene_fname)
 
+        if os.path.split(scene_dir)[1] in EXCLUDE_FOLDERS:
+            continue
+
         for variant in mitsuba.variants():
             if not variant.split('_')[0] == 'scalar' or variant.endswith('double'):
                 continue
 
             mitsuba.set_variant(variant)
-            from mitsuba.core import Bitmap, Struct, Thread
+            from mitsuba.core import Bitmap, Struct, Thread, set_thread_count
 
-            ref_fname = get_ref_fname(scene_fname)
-            if os.path.exists(ref_fname) and not overwrite:
+            ref_fname, var_fname = get_ref_fname(scene_fname)
+            if os.path.exists(ref_fname) and os.path.exists(var_fname) and not overwrite:
                 continue
 
             Thread.thread().file_resolver().append(scene_dir)
-            scene = mitsuba.core.xml.load_file(scene_fname, spp=ref_spp)
-            scene.integrator().render(scene, scene.sensors()[0])
 
-            film = scene.sensors()[0].film()
-            cur_bitmap = film.bitmap(raw=True).convert(Bitmap.PixelFormat.RGB, Struct.Type.Float32, False)
+            if True:
+                scene = mitsuba.core.xml.load_file(scene_fname, spp=ref_spp)
+                scene.integrator().render(scene, scene.sensors()[0])
 
-            # Write rendered image to a file
-            cur_bitmap.write(ref_fname)
-            print('Saved rendered image to: ' + ref_fname)
+                bmp = scene.sensors()[0].film().bitmap(raw=False)
+                img, var_img = bitmap_extract(bmp)
+
+                # Write rendered image to a file
+                xyz_to_rgb_bmp(img).write(ref_fname)
+                print('Saved rendered image to: ' + ref_fname)
+
+                # Write variance image to a file
+                xyz_to_rgb_bmp(var_img).write(var_fname)
+                print('Saved variance image to: ' + var_fname)
 
 
 if __name__ == '__main__':
