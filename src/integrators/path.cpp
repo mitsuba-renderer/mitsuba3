6efb4abd60dcdc354b7a2ce98bdae92df9da5e38
Sebastien Speierer <sebastieneps@gmail.com>
2020-03-31 12:51:01 +0200

    Fix link to doc in README and type in path.cpp

--- src/integrators/path.cpp
+++ src/integrators/path.cpp
@@ -141,7 +141,7 @@ public:
 
             // Stop if we've exceeded the number of requested bounces, or
             // if there are no more active lanes. Only do this latter check
-            // in GPU mode when the number of requested bounces infinite
+            // in GPU mode when the number of requested bounces is infinite
             // since it causes a costly synchronization.
             if ((uint32_t) depth >= (uint32_t) m_max_depth ||
                 ((!is_cuda_array_v<Float> || m_max_depth < 0) && none(active)))
