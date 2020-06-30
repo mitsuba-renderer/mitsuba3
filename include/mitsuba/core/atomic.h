78a1b9a6df09ad9b73f9552a4ff746c1e328bb47
Wenzel Jakob <wenzel.jakob@epfl.ch>
2020-06-30 14:21:23 +0200

    atomic float improvements

--- include/mitsuba/core/atomic.h
+++ include/mitsuba/core/atomic.h
@@ -40,12 +40,21 @@ public:
     /// Atomically divide by a floating point value
     AtomicFloat &operator/=(Type arg) { return do_atomic([arg](Type value) { return value / arg; }); }
 
+    /// Atomically compute the minimum
+    AtomicFloat &min(Type arg) { return do_atomic([arg](Type value) { return std::min(value, arg); }); }
+
+    /// Atomically compute the maximum
+    AtomicFloat &max(Type arg) { return do_atomic([arg](Type value) { return std::max(value, arg); }); }
+
+
 protected:
     /// Apply a FP operation atomically (verified that this will be nicely inlined in the above operators)
     template <typename Func> AtomicFloat& do_atomic(Func func) {
-        Storage old_bits = m_bits, new_bits;
+        Storage old_bits = m_bits.load(std::memory_order::memory_order_relaxed), new_bits;
         do {
             new_bits = memcpy_cast<Storage>(func(memcpy_cast<Type>(old_bits)));
+            if (new_bits == old_bits)
+                break;
         } while (!m_bits.compare_exchange_weak(old_bits, new_bits));
         return *this;
     }
