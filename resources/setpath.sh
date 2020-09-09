0ff1b8ba982e80ed5e426d7a74c7387b86499ac9
Sebastien Speierer <sebastieneps@gmail.com>
2020-09-09 10:36:31 +0200

    Add bash automatic completion for variants

--- resources/setpath.sh
+++ resources/setpath.sh
@@ -23,3 +23,20 @@ fi
 
 export PYTHONPATH="$MITSUBA_DIR/python:$PYTHONPATH"
 export PATH="$MITSUBA_DIR:$PATH"
+
+if [ "$BASH_VERSION" ]; then
+    _mitsuba()
+    {
+        local cur prev opts
+        COMPREPLY=()
+        cur="${COMP_WORDS[COMP_CWORD]}"
+        prev="${COMP_WORDS[COMP_CWORD-1]}"
+        opts="@MTS_VARIANT_NAMES_STR@"
+
+        if [[ ${prev} == -m ]] ; then
+            COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+            return 0
+        fi
+    }
+    complete -F _mitsuba mitsuba
+fi
