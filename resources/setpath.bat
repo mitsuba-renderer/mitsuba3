f432e5f74fa89727950a1102ec4a181b4bedb2d9
Sebastien Speierer <sebastieneps@gmail.com>
2021-02-23 09:59:12 +0100

    Fix setpath.sh/bat for Windows

--- resources/setpath.bat
+++ resources/setpath.bat
@@ -2,11 +2,10 @@
 
 REM ***************************************************************
 REM * This script adds Mitsuba to the current path on Windows.
-REM * It assumes that Mitsuba is either compiled within the 
-REM * source tree or within a subdirectory named 'build'.
+REM * It assumes that Mitsuba is either compiled within a
+REM * subdirectory named 'build'.
 REM ***************************************************************
 
 set MITSUBA_DIR=%~dp0
-set MITSUBA_DIR=%MITSUBA_DIR:~0,-1%
 set PATH=%MITSUBA_DIR%;%PATH%
 set PYTHONPATH=%MITSUBA_DIR%/python;%PYTHONPATH%
