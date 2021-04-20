ff340a0d72dcd9163577ccbd569b4f999c01ee13
Sebastien Speierer <sebastieneps@gmail.com>
2021-04-20 15:25:09 +0200

    Change "Loading plugin" log level to Debug

--- src/libcore/plugin.cpp
+++ src/libcore/plugin.cpp
@@ -105,7 +105,7 @@ struct PluginManager::PluginManagerPrivate {
         fs::path resolved = resolver->resolve(filename);
 
         if (fs::exists(resolved)) {
-            Log(Info, "Loading plugin \"%s\" ..", filename.string());
+            Log(Debug, "Loading plugin \"%s\" ..", filename.string());
             Plugin *plugin = new Plugin(resolved);
             // New classes must be registered within the class hierarchy
             Class::static_initialization();
