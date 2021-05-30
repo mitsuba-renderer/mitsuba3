41f877398b7745dc5284b4380163cbb536b41b2f
Vincent Tournier <vincent.tournier@epfl.ch>
2021-05-30 11:25:29 +0200

    added non-alpha bitmap support

--- include/mitsuba/render/denoiser.h
+++ include/mitsuba/render/denoiser.h
@@ -6,7 +6,6 @@
 #include <enoki-jit/optix.h>
 #include <enoki/array.h>
 #include <iostream>
-#include <exception>
 
 #define optix_check(call) if(call != 0) {std::cerr << "Optix call failed" << std::endl; return Float(0.0);}                  
 
@@ -35,10 +34,6 @@ Float denoise (const Bitmap& noisy, Bitmap* albedo, Bitmap* normals) {
         optix_check(optixDenoiserComputeMemoryResources(denoiser, noisy.width(), noisy.height(), &sizes));
         scratch_size = static_cast<uint32_t>(sizes.withoutOverlapScratchSizeInBytes);
         state_size = static_cast<uint32_t>(sizes.stateSizeInBytes);
-        // const uint64_t frame_size = noisy.pixel_count() * noisy.bytes_per_pixel();
-
-        std::cout << scratch_size << std::endl;
-        std::cout << state_size << std::endl;
 
         Float intensity_float = ek::zero<Float>(1);
         Float scratch_float = ek::zero<Float>(scratch_size / sizeof(float));
@@ -57,23 +52,18 @@ Float denoise (const Bitmap& noisy, Bitmap* albedo, Bitmap* normals) {
 
         output.width = noisy.width();
         output.height = noisy.height();
-        output.format = OPTIX_PIXEL_FORMAT_FLOAT4;
+        output.format = noisy.has_alpha() ? OPTIX_PIXEL_FORMAT_FLOAT4 : OPTIX_PIXEL_FORMAT_FLOAT3;
         output.rowStrideInBytes = noisy.width() * noisy.bytes_per_pixel();
         output.pixelStrideInBytes = noisy.bytes_per_pixel();
+
         DynamicBuffer<Float> noisy_data = ek::load<DynamicBuffer<Float>>(noisy.data(), noisy.pixel_count() * noisy.channel_count());
         inputs[0].data = noisy_data.data();
         inputs[0].width = noisy.width();
         inputs[0].height = noisy.height();
-        inputs[0].format = OPTIX_PIXEL_FORMAT_FLOAT4;
+        inputs[0].format = noisy.has_alpha() ? OPTIX_PIXEL_FORMAT_FLOAT4 : OPTIX_PIXEL_FORMAT_FLOAT3;
         inputs[0].rowStrideInBytes = noisy.width() * noisy.bytes_per_pixel();
         inputs[0].pixelStrideInBytes = noisy.bytes_per_pixel();
-
-        // std::cout << noisy.pixel_format() << std::endl;
-        // std::cout << noisy.has_alpha() << std::endl;
-        // std::cout << noisy.bytes_per_pixel() << std::endl;
-        std::cout << noisy_data << std::endl;
         
-
         unsigned int nb_channels = 1;
         if(albedo != nullptr) {
 
@@ -81,7 +71,7 @@ Float denoise (const Bitmap& noisy, Bitmap* albedo, Bitmap* normals) {
             inputs[1].data = albedo_data.data();
             inputs[1].width = albedo->width();
             inputs[1].height = albedo->height();
-            inputs[1].format = OPTIX_PIXEL_FORMAT_FLOAT4;
+            inputs[1].format = albedo->has_alpha() ? OPTIX_PIXEL_FORMAT_FLOAT4 : OPTIX_PIXEL_FORMAT_FLOAT3;
             inputs[1].rowStrideInBytes = albedo->width() * albedo->bytes_per_pixel();
             inputs[1].pixelStrideInBytes = albedo->bytes_per_pixel();
             nb_channels++;
@@ -91,7 +81,7 @@ Float denoise (const Bitmap& noisy, Bitmap* albedo, Bitmap* normals) {
             inputs[2].data = normals_data.data();
             inputs[2].width = normals->width();
             inputs[2].height = normals->height();
-            inputs[2].format = OPTIX_PIXEL_FORMAT_FLOAT4;
+            inputs[2].format = normals->has_alpha() ? OPTIX_PIXEL_FORMAT_FLOAT4 : OPTIX_PIXEL_FORMAT_FLOAT3;
             inputs[2].rowStrideInBytes = normals->width() * normals->bytes_per_pixel();
             inputs[2].pixelStrideInBytes = normals->bytes_per_pixel();
             nb_channels++;
@@ -108,12 +98,9 @@ Float denoise (const Bitmap& noisy, Bitmap* albedo, Bitmap* normals) {
                                         0, 0, &output, scratch, scratch_size));
 
         Float denoised_data = ek::map<Float>(output.data, noisy.pixel_count() * noisy.channel_count());
-        // Float denoised_data = ek::map<Float>(output.data, 114688);
-
         ek::eval();
         ek::sync_thread();
 
-        std::cout << denoised_data << std::endl;
         optix_check(optixDenoiserDestroy(denoiser));
         return denoised_data;
     }
