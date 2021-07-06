88be06e88749c4eceb0c89e1402266a15b194302
Sebastien Speierer <sebastieneps@gmail.com>
2021-07-06 16:04:53 +0200

    Restructure ad python package, add WIP RBP integrator

---
+++ src/python/python/ad/integrators/__init__.py
@@ -0,0 +1,89 @@
+# -------------------------------------------------------------------
+#        Default implementation for Integrator adjoint methods
+# -------------------------------------------------------------------
+
+
+import mitsuba
+import mitsuba.python.util
+
+
+def render_adjoint_impl(self: mitsuba.render.SamplingIntegrator,
+                        scene: mitsuba.render.Scene,
+                        params: mitsuba.python.util.SceneParameters,
+                        image_adj: mitsuba.core.Spectrum,
+                        sensor_index: int=0) -> None:
+    """
+    Performed the adjoint rendering integration, backpropagating the
+    image gradients to the scene parameters.
+
+    The default implementation relies on Automatic Differentiation to
+    compute the scene parameters gradients w.r.t. the adjoint image.
+
+    Parameter ``scene``:
+        The scene to render
+
+    Parameter ``params`` (``mitsuba.python.utils.SceneParameters``):
+        Scene parameters expecting gradients.
+
+    Parameter ``image_adj`` (``mitsuba.core.ImageBuffer``):
+        Gradient image to backpropagate during the adjoint integration.
+
+    Parameter ``sensor_index`` (``int``):
+        Optional parameter to specify which sensor to use for rendering.
+    """
+    ek.enable_grad(params)
+    image = self.render(scene, sensor_index)
+    ek.set_grad(image, image_adj)
+    ek.enqueue(image)
+    ek.traverse(type(image_adj), reverse=True, retain_graph=False)
+
+
+def sample_adjoint_impl(self: mitsuba.render.SamplingIntegrator,
+                        scene: mitsuba.render.Scene,
+                        sampler: mitsuba.render.Sampler,
+                        ray: mitsuba.core.Ray3f,
+                        params: mitsuba.python.util.SceneParameters,
+                        grad: mitsuba.core.Spectrum,
+                        medium: mitsuba.render.Medium=None,
+                        active: mitsuba.core.Mask=True) -> None:
+    """
+    Propagate adjoint radiance along a ray.
+
+    Parameter ``scene``:
+        The underlying scene in which the adjoint radiance should be propagated
+
+    Parameter ``sampler``:
+        A source of (pseudo-/quasi-) random numbers
+
+    Parameter ``ray`` (``mitsuba.core.Ray3f``):
+        Rays along which the adjoint radiance should be propagated
+
+    Parameter ``params`` (``mitsuba.python.utils.SceneParameters``):
+        Scene parameters expecting gradients
+
+    Parameter ``grad`` (``mitsuba.core.Spectrum``):
+        Gradient value to be backpropagated
+
+    Parameter ``medium`` (``mitsuba.render.MediumPtr``):
+         If the ray starts inside a medium, this argument holds a pointer
+         to that medium
+    """
+
+    raise NotImplementedError(
+        "SamplingIntegrator.sample_adjoint is not implemented!")
+
+
+# Bind adjoint methods to the SamplingIntegrator class
+mitsuba.render.SamplingIntegrator.render_adjoint = render_adjoint_impl
+mitsuba.render.SamplingIntegrator.sample_adjoint = sample_adjoint_impl
+
+
+# -------------------------------------------------------------------
+#        Import all integrators implemented in this folder
+# -------------------------------------------------------------------
+
+
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
