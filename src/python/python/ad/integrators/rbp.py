4d67bc026e435babbee46477ff008372247abc2d
Sebastien Speierer <sebastieneps@gmail.com>
2021-07-07 14:05:10 +0200

    Fix RBP integrator (works now)

--- src/python/python/ad/integrators/rbp.py
+++ src/python/python/ad/integrators/rbp.py
@@ -24,10 +24,10 @@ def sample_sensor_rays(sensor):
     pos += sampler.next_2d()
 
     rays, weights = sensor.sample_ray_differential(
-        time=0,
+        time=0.0,
         sample1=sampler.next_1d(),
         sample2=pos * scale,
-        sample3=0
+        sample3=0.0
     )
 
     return rays, weights, pos, pos_idx
@@ -57,27 +57,30 @@ class RBPIntegrator(mitsuba.render.SamplingIntegrator):
         if spp > 0:
             sampler.set_sample_count(spp)
         rays, weights, _, pos_idx = sample_sensor_rays(sensor)
-        grad = weights * ek.gather(type(image_adj), image_adj, pos_idx)
+
+        grad_values = ek.gather(mitsuba.core.Spectrum, ek.detach(image_adj), pos_idx)
+        grad_values *= weights / sampler.sample_count()
 
         for k, v in params.items():
             ek.enable_grad(v)
 
-        self.sample_adjoint(scene, sensor.sampler(), rays, params, grad)
+        self.sample_adjoint(scene, sensor.sampler(), rays, params, grad_values)
 
     def sample(self, scene, sampler, rays, medium, active):
         return *self.Li(scene, sampler, rays, params=None), []
 
     def sample_adjoint(self, scene, sampler, rays, params, grad):
-        self.Li(scene, sampler, rays, params, grad)
+        self.Li(scene, sampler, rays, params=params, grad=grad)
 
     def Li(self: mitsuba.render.SamplingIntegrator,
            scene: mitsuba.render.Scene,
            sampler: mitsuba.render.Sampler,
            rays: mitsuba.core.Ray3f,
+           depth: mitsuba.core.UInt32=1,
            params: mitsuba.python.util.SceneParameters=None,
            grad: mitsuba.core.Spectrum=None,
-           emission_weight: mitsuba.core.Spectrum=None,
-           active: mitsuba.core.Mask=True): # TODO should take depth as arg
+           emission_weight: mitsuba.core.Float=None,
+           active_: mitsuba.core.Mask=True):
         """
         Performs a recursive step of a random walk to construct a light path.
 
@@ -92,56 +95,43 @@ class RBPIntegrator(mitsuba.render.SamplingIntegrator):
         from mitsuba.core import Spectrum, Float, UInt32, Mask, Ray3f, Loop
         from mitsuba.render import DirectionSample3f, BSDFContext, BSDFFlags, has_flag
 
+        assert params is None or ek.detached_t(grad) or grad.index_ad() == 0
         is_primal = params is None
 
         def mis_weight(a, b):
-            return ek.select(a > 0.0, (a**2) / (a**2 + b**2), 0.0)
+            return ek.detach(ek.select(a > 0.0, (a**2) / (a**2 + b**2), 0.0))
 
-        def adjoint(variables, active):
-            # Clean NaN is variable's weights
-            for v, w in variables.items():
-                w[ek.isnan(w)] = 0.0
-
+        def adjoint(var, weight, active):
             if is_primal:
-                result = 0
-                for v, w in variables.items():
-                    result += ek.select(active, v * w, 0.0)
-                return result
+                return ek.select(active, var * weight, 0.0)
             else:
-                # Detach all variable's weights
-                for v, w in variables.items():
-                    w = ek.detach(w)
-
-                # Backward mode, we need to accumulate parameter gradients
-                for v, w in variables.items():
-                    tmp_w = w * grad
-                    if ek.is_dynamic_array_v(v) and ek.is_static_array_v(tmp_w):
-                        tmp_w = ek.hsum(tmp_w)
-                    ek.set_grad(v, ek.detach(tmp_w))
-                    ek.enqueue(v)
+                ek.set_grad(var, ek.select(active, weight * grad, 0.0))
+                ek.enqueue(var)
                 ek.traverse(Float, reverse=True, retain_graph=False)
                 return 0
 
         rays = Ray3f(rays)
 
         # Primary ray intersection
-        si = scene.ray_intersect(rays, active)
-        valid_rays = active & si.is_valid()
+        si = scene.ray_intersect(rays, active_)
+        valid_rays = active_ & si.is_valid()
 
         # Initialize loop variables
         result     = Spectrum(0.0)
         throughput = Spectrum(1.0)
-        active     = active & si.is_valid()
+        active     = active_ & si.is_valid()
 
         if emission_weight is None:
-            emission_weight = Spectrum(1.0)
+            emission_weight = Float(1.0)
+        else:
+            emission_weight = Float(emission_weight) # TODO is this needed to copy in recursive call?
 
         if not is_primal:
             params.set_grad_suspended(False)
 
-        i = UInt32(0)
+        depth_i = UInt32(depth)
         loop = Loop("RBPLoop" + '_recursive_li' if is_primal else '')
-        loop.put(lambda:(i, active, rays, emission_weight, throughput, si, result))
+        loop.put(lambda:(depth_i, active, rays, emission_weight, throughput, si, result))
         sampler.loop_register(loop)
         loop.init()
         while loop(active):
@@ -149,24 +139,24 @@ class RBPIntegrator(mitsuba.render.SamplingIntegrator):
             # ---------------------- Direct emission ----------------------
 
             emitter = si.emitter(scene, active)
-            result += adjoint({
-                emitter.eval(si, active) : throughput * emission_weight
-            }, active)
+            result += adjoint(
+                emitter.eval(si, active),
+                throughput * emission_weight,
+                active
+            )
 
             if not is_primal:
                 params.set_grad_suspended(True)
 
             active &= si.is_valid()
-            active &= i < self.max_depth
+            active &= depth_i < self.max_depth
 
             ctx = BSDFContext()
             bsdf = si.bsdf()
 
             # ---------------------- Emitter sampling ----------------------
 
-            is_smooth_bsdf = has_flag(bsdf.flags(), BSDFFlags.Smooth)
-            active_e = active & is_smooth_bsdf
-
+            active_e = active & has_flag(bsdf.flags(), BSDFFlags.Smooth)
             ds, emitter_val = scene.sample_emitter_direction(si, sampler.next_2d(active_e), True, active_e)
             active_e &= ek.neq(ds.pdf, 0.0)
             wo = si.to_local(ds.d)
@@ -175,51 +165,59 @@ class RBPIntegrator(mitsuba.render.SamplingIntegrator):
                 params.set_grad_suspended(False)
 
             bsdf_val, bsdf_pdf = bsdf.eval_pdf(ctx, si, wo, active_e)
-            mis = ek.select(ek.detach(ds.delta), 1.0, mis_weight(ds.pdf, bsdf_pdf))
+            mis = ek.select(ds.delta, 1.0, mis_weight(ds.pdf, bsdf_pdf))
 
-            bsdf_val[~active_e] = 0.0
-            emitter_val[~active_e] = 0.0
-
-            result += adjoint({
-                bsdf_val : throughput * mis * emitter_val
-            }, active_e)
+            result += adjoint(
+                bsdf_val,
+                throughput * mis * emitter_val,
+                active_e
+            )
 
             # ---------------------- BSDF sampling ----------------------
 
+            if not is_primal:
+                params.set_grad_suspended(True)
+
             bs, bsdf_val = bsdf.sample(ctx, si, sampler.next_1d(active),
-                                    sampler.next_2d(active), active)
+                                       sampler.next_2d(active), active)
+
             active &= bs.pdf > 0.0
-            rays = si.spawn_ray(si.to_world(ek.detach(bs.wo)))
+            rays = ek.detach(si.spawn_ray(si.to_world(bs.wo)))
             si_bsdf = scene.ray_intersect(rays, active)
 
             # Compute MIS weights for the BSDF sampling
-            emitter = si_bsdf.emitter(scene, active)
             ds = DirectionSample3f(scene, si_bsdf, si)
-            ds.emitter = emitter
+            ds.emitter = si_bsdf.emitter(scene, active)
             delta = has_flag(bs.sampled_type, BSDFFlags.Delta)
-            active_b = active & ek.neq(emitter, None) & ~delta
-            emitter_pdf = ek.select(active_b, scene.pdf_emitter_direction(si, ds), 0.0)
-            emission_weight = Spectrum(ek.detach(mis_weight(bs.pdf, emitter_pdf)))
+            active_b = active & ek.neq(ds.emitter, None) & ~delta
+            emitter_pdf = scene.pdf_emitter_direction(si, ds, active_b)
+            emission_weight = ek.select(active_b, mis_weight(bs.pdf, emitter_pdf), 1.0)
 
-            # Account for incoming radiance
-            li = emitter.eval(si_bsdf, active_b)
-            if self.recursive_li:
-                li += self.Li(scene, sampler, ek.detach(rays),
-                            emission_weight=emission_weight,
-                            active=active_b)[0]
-            else:
-                li += Float(1.0)
+            if not is_primal:
+                # Account for incoming radiance
+                if self.recursive_li:
+                    li = self.Li(scene, sampler, ek.detach(rays), depth_i+1,
+                                 emission_weight=emission_weight,
+                                 active_=active_b)[0]
+                else:
+                    li = ds.emitter.eval(si_bsdf, active_b) * emission_weight
 
-            adjoint({
-                bsdf_val : throughput * li / bs.pdf
-            }, active_b)
+                params.set_grad_suspended(False)
+
+                bsdf_eval = bsdf.eval(ctx, si, ek.detach(bs.wo), active_b)
+
+                adjoint(
+                    bsdf_eval,
+                    throughput * li / bs.pdf,
+                    active_b
+                )
 
             # ------------------- Recurse to the next bounce -------------------
 
             si = si_bsdf
             throughput *= ek.detach(bsdf_val)
 
-            i += UInt32(1)
+            depth_i += UInt32(1)
 
         return result, valid_rays
 
