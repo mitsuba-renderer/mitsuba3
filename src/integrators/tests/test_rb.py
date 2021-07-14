8f189fecedde3dd2271b1401a2134497b7dd41d6
Sebastien Speierer <sebastieneps@gmail.com>
2021-07-14 16:28:33 +0200

    Update integrator/test_rb .py

--- src/integrators/tests/test_rb.py
+++ src/integrators/tests/test_rb.py
@@ -25,7 +25,9 @@ def make_simple_scene(res=1):
                 'sample_count': 1,
             },
         },
-        'whitewall': { 'type': 'rectangle' },
+        'whitewall': {
+            'type': 'rectangle',
+        },
         'redwall': {
             'type': 'rectangle',
             'bsdf' : {
@@ -34,106 +36,97 @@ def make_simple_scene(res=1):
             },
             'to_world': T.translate([-1, 0, 0]) * T.rotate([0, 1, 0], 90)
         },
-        # 'arealight': {
-        #     'type': 'rectangle',
-        #     'bsdf' : {
-        #         'type' : 'diffuse',
-        #         'reflectance' : { 'type' : 'rgb', 'value' : [0.0, 0.0, 0.0] }
-        #     },
-        #     'to_world': T.translate([1, 0, 0]) * T.rotate([0, 1, 0], -90),
-        #     'emitter': { 'type': 'area' }
-        # },
+        'arealight': {
+            'type': 'rectangle',
+            'bsdf' : {
+                'type' : 'diffuse',
+                'reflectance' : { 'type' : 'rgb', 'value' : [0.0, 0.0, 0.0] }
+            },
+            'to_world': T.translate([1, 0, 0]) * T.rotate([0, 1, 0], -90),
+            'emitter': { 'type': 'area' }
+        },
         'emitter': {
             'type': 'point',
             'position': [0, 0, 1]
         }
     })
 
-# if hasattr(ek, 'JitFlag'):
-#     jit_flags_options = [
-#         {ek.JitFlag.VCallRecord: 0, ek.JitFlag.LoopRecord: 0},
-#         {ek.JitFlag.VCallRecord: 1, ek.JitFlag.LoopRecord: 0},
-#         {ek.JitFlag.VCallRecord: 1, ek.JitFlag.LoopRecord: 1},
-#     ]
-# else:
-#     jit_flags_options = []
+if hasattr(ek, 'JitFlag'):
+    jit_flags_options = [
+        {ek.JitFlag.VCallRecord: 0, ek.JitFlag.LoopRecord: 0},
+        {ek.JitFlag.VCallRecord: 1, ek.JitFlag.LoopRecord: 0},
+        {ek.JitFlag.VCallRecord: 1, ek.JitFlag.LoopRecord: 1, ek.JitFlag.LoopOptimize: 0},
+        {ek.JitFlag.VCallRecord: 1, ek.JitFlag.LoopRecord: 1, ek.JitFlag.LoopOptimize: 1},
+    ]
+else:
+    jit_flags_options = []
 
-# def test_gradients(variants_all_ad_rgb):
-# @pytest.mark.skip()
-def test_gradient(variant_cuda_ad_rgb):
+@pytest.mark.slow
+@pytest.mark.parametrize("jit_flags", jit_flags_options)
+def test_gradients(variants_all_ad_rgb, jit_flags):
     from mitsuba.core import xml, Color3f
-    from mitsuba.python.util import traverse
-    from mitsuba.python.autodiff import write_bitmap
+    from mitsuba.python.util import traverse, write_bitmap
 
-    scene = make_simple_scene(res=16)
+    # Set enoki JIT flags
+    for k, v in jit_flags.items():
+        ek.set_flag(k, v)
+
+    scene = make_simple_scene(res=1)
     crop_size = scene.sensors()[0].film().crop_size()
 
-    spp_primal = 4024 * 1
-    spp_adjoint = 4024 * 1
+    spp_primal = 4024 * 16
+    spp_adjoint = 4024 * 16
     max_depth = 3
+    epsilon = 1e-2
 
-    # ek.set_flag(ek.JitFlag.VCallRecord, False)
-    # ek.set_flag(ek.JitFlag.LoopRecord,  False)
-    # ek.set_flag(ek.JitFlag.LoopOptimize,  False)
+    ek.set_flag(ek.JitFlag.VCallRecord,   True)
+    ek.set_flag(ek.JitFlag.VCallOptimize, True)
+    ek.set_flag(ek.JitFlag.LoopRecord,    True)
+    ek.set_flag(ek.JitFlag.LoopOptimize,  True)
 
     rb_integrator = xml.load_dict({
         'type': 'rb',
-        'max_depth': max_depth
-    })
-    path_integrator = xml.load_dict({
-        'type': 'path',
-        'max_depth': max_depth
+        'max_depth': max_depth,
+        'recursive_li': True
     })
 
     # Rendering reference image
     scene.sensors()[0].sampler().seed(0)
-    image_ref = rb_integrator.render(scene, spp=spp_primal)
-    write_bitmap('path_ref.exr', image_ref, crop_size)
-
-    params = traverse(scene)
+    image_1 = rb_integrator.render(scene, spp=spp_primal)
+    # write_bitmap('image_1.exr', image_1, crop_size)
 
     # Update scene parameter
+    params = traverse(scene)
     key = 'redwall.bsdf.reflectance.value'
     params.keep([key])
-    params[key] = Color3f([0.1, 0.9, 0.9])
+    params[key] += epsilon
     params.update()
 
     # Render primal image
     scene.sensors()[0].sampler().seed(0)
-    image = rb_integrator.render(scene, spp=spp_primal)
-    write_bitmap('path_init.exr', image, crop_size)
+    image_2 = rb_integrator.render(scene, spp=spp_primal)
+    # write_bitmap('image_2.exr', image_2, crop_size)
 
     # Compute adjoint image (MSE objective function)
-    ek.enable_grad(image)
-    ob_val = ek.hsum_async(ek.sqr(image - image_ref)) / len(image)
+    ek.enable_grad(image_2)
+    ob_val = ek.hsum_async(ek.sqr(image_2 - image_1)) / len(image_2)
     ek.backward(ob_val)
-    image_adj = ek.grad(image)
-    ek.set_grad(image, 0.0)
-    rgb_image_adj = ek.unravel(Color3f, ek.detach(image_adj))
-    write_bitmap('path_adj.exr', image_adj, crop_size)
+    image_adj = ek.grad(image_2)
+    ek.set_grad(image_2, 0.0)
+    # write_bitmap('image_adj.exr', image_adj, crop_size)
 
-    # print('Adjoint rendering ----')
     # scene.sensors()[0].sampler().seed(0)
     rb_integrator.render_adjoint(scene, params, image_adj, spp=spp_adjoint)
     grad_backward = ek.grad(params[key])
 
-    # # Forward render with path tracer
-    # print('Forward rendering ----')
+    # Finite difference
+    fd_image_grad = ek.abs(image_1 - image_2) / epsilon
+    rgb_image_adj = ek.unravel(Color3f, ek.detach(image_adj))
+    rgb_fd_image_grad = ek.unravel(Color3f, ek.detach(fd_image_grad))
+    grad_fd = rgb_fd_image_grad * rgb_image_adj
 
-    ek.set_flag(ek.JitFlag.VCallRecord, False)
-    ek.set_flag(ek.JitFlag.LoopRecord,  False)
-
-    params = traverse(scene)
-    ek.set_grad(params[key], 0.0)
-    ek.enable_grad(params[key])
-    # scene.sensors()[0].sampler().seed(0)
-    image = path_integrator.render(scene, spp=spp_primal)
-    fwd_image = ek.unravel(Color3f, image)
-    ek.forward(params[key])
-    fwd_diff_grad_image_rgb = ek.grad(fwd_image)
-
-    grad_forward = Color3f([ek.dot(fwd_diff_grad_image_rgb[i], rgb_image_adj[i]) for i in range(3)])
-
-    print(f'grad_forward:  {grad_forward}')
+    # Compare the gradients
+    print(f'grad_fd:  {grad_fd}')
     print(f'grad_backward: {grad_backward}')
-    # assert ek.allclose(grad_forward, grad_backward)
+
+    assert ek.allclose(grad_fd, grad_backward, rtol=0.05, atol=1e-10)
