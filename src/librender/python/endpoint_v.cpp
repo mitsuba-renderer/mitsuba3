1750a64755b641d3c0567bfb77ce60a44813d2db
Vincent Leroy <vincent.leroy@rayference.eu>
2021-09-01 10:55:47 +0200

    Add minimal EndpointPtr bindings
    foo

--- src/librender/python/endpoint_v.cpp
+++ src/librender/python/endpoint_v.cpp
@@ -28,4 +28,38 @@ MTS_PY_EXPORT(Endpoint) {
         .def_method(Endpoint, set_shape, "shape"_a)
         .def_method(Endpoint, set_medium, "medium"_a)
         .def_method(Endpoint, bbox);
+
+    if constexpr (ek::is_array_v<EndpointPtr>) {
+        py::object ek       = py::module_::import("enoki"),
+                   ek_array = ek.attr("ArrayBase");
+
+        py::class_<EndpointPtr> cls(m, "EndpointPtr", ek_array);
+
+        cls.def("sample_ray",
+                [](EndpointPtr ptr, Float time, Float sample1, const Point2f &sample2,
+                const Point2f &sample3, Mask active) {
+                    return ptr->sample_ray(time, sample1, sample2, sample3, active);
+                },
+                "time"_a, "sample1"_a, "sample2"_a, "sample3"_a, "active"_a = true,
+                D(Endpoint, sample_ray))
+        .def("sample_direction",
+                [](EndpointPtr ptr, const Interaction3f &it, const Point2f &sample, Mask active) {
+                    return ptr->sample_direction(it, sample, active);
+                },
+                "it"_a, "sample"_a, "active"_a = true,
+                D(Endpoint, sample_direction))
+        .def("pdf_direction",
+                [](EndpointPtr ptr, const Interaction3f &it, const DirectionSample3f &ds, Mask active) {
+                    return ptr->pdf_direction(it, ds, active);
+                },
+                "it"_a, "ds"_a, "active"_a = true,
+                D(Endpoint, pdf_direction))
+        .def("eval",
+                [](EndpointPtr ptr, const SurfaceInteraction3f &si, Mask active) {
+                    return ptr->eval(si, active);
+                },
+                "si"_a, "active"_a = true, D(Endpoint, eval));
+
+        bind_enoki_ptr_array(cls);
+    }
 }
