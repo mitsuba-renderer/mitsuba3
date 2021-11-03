mitsuba-renderer/mitsuba3

commit e4ded745c436bfc464dd07fc7a9fc7446e17443e

chi2: extra assertion check

src/python/python/chi2.py
@@ -128,8 +128,9 @@ class ChiSquareTest:
         samples_out = self.sample_func(samples_in)
 
         if type(samples_out) is tuple:
-            weights_out = samples_out[1]
             samples_out = samples_out[0]
+            weights_out = samples_out[1]
+            assert ek.array_depth_v(weights_out) == 1
         else:
             weights_out = Float(1.0)
 

ext/CMakeLists.txt
@@ -391,6 +391,14 @@ set_target_properties(rgb2spec rgb2spec_opt rgb2spec_opt_run PROPERTIES FOLDER "
 # ----------------------------------------------------------
 
 add_subdirectory(fastfloat)
+mark_as_advanced(
+  FASTFLOAT_SANITIZE
+  FASTFLOAT_TEST
+)
+
+# ----------------------------------------------------------
+#  Hide a few more settings that aren't relevant for users
+# ----------------------------------------------------------
 
 mark_as_advanced(
   BUILD_SHARED_LIBS BUILD_TESTING CMAKE_DEBUG_POSTFIX CMAKE_INSTALL_PREFIX

commit 8aee24bc6ef9a8629afc26e0145e31076912a25c

fix regression introduced in commit e4ded745c436bfc4

src/python/python/chi2.py
@@ -128,8 +128,8 @@ class ChiSquareTest:
         samples_out = self.sample_func(samples_in)
 
         if type(samples_out) is tuple:
-            samples_out = samples_out[0]
             weights_out = samples_out[1]
+            samples_out = samples_out[0]
             assert ek.array_depth_v(weights_out) == 1
         else:
             weights_out = Float(1.0)