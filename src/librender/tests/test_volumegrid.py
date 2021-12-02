Fix VolumeGrid in double precision modes

src/librender/tests/test_volumegrid.py
@@ -1,14 +1,14 @@
 import mitsuba
 import pytest
-import enoki as ek
 
 import numpy as np
 import os
 
-def test01_numpy_conversion(variant_scalar_rgb, np_rng):
+
+def test01_numpy_conversion(variants_all_scalar, np_rng):
     from mitsuba.render import VolumeGrid
 
-    a = np_rng.random((4, 8, 16, 3)).astype(np.float32)
+    a = np_rng.random((4, 8, 16, 3))
     grid = VolumeGrid(a)
     assert np.allclose(a, np.array(grid))
     assert np.allclose(np.max(a), grid.max())
@@ -23,11 +23,11 @@ def test01_numpy_conversion(variant_scalar_rgb, np_rng):
     assert np.allclose(a.shape[3], grid.channel_count())
 
 
-def test02_read_write(variant_scalar_rgb, tmpdir, np_rng):
+def test02_read_write(variants_all_scalar, tmpdir, np_rng):
     from mitsuba.render import VolumeGrid
 
     tmp_file = os.path.join(str(tmpdir), "out.vol")
-    grid = np_rng.random((4, 8, 16, 3)).astype(np.float32)
+    grid = np_rng.random((4, 8, 16, 3))
     VolumeGrid(grid).write(tmp_file)
     loaded = VolumeGrid(tmp_file)
     assert np.allclose(np.array(loaded), grid)