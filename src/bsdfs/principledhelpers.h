8af35ef85df9c402ee23b9010d509942b29d8711
Ekrem Fatih YILMAZER <ekremfathyilmazer@gmail.com>
2022-01-10 13:04:52 +0300

    Add the principled BSDF

---
+++ src/bsdfs/principledhelpers.h
@@ -0,0 +1,332 @@
+NAMESPACE_BEGIN(mitsuba)
+/**
+ * \brief GTR1_isotropic Microfacet Distribution class
+ *
+ * This class implements GTR1 Microfacet Distribution Methods
+ * for sampling routines of clearcoat lobe in the pricipled BSDF.
+ *
+ * Based on the paper
+ *
+ *   "Physically Based Shading at Disney"
+ *    by Burley Brent
+ *
+ * Although it is a Microfacet distribution, it is not added in Microfacet
+ * Plugin of Mitsuba since only the Principled BSDF uses it. Also,
+ * visible normal sampling procedure is not applied as in Microfacet Plugin
+ * because the clearcoat lobe of the principled BSDF has low energy compared to the other
+ * lobes and visible normal sampling would not increase the sampling performance
+ * considerably.
+ */
+template <typename Float, typename Spectrum>
+class GTR1Isotropic {
+public:
+    MTS_IMPORT_TYPES();
+    /**
+     * Create an isotropic microfacet distribution for clearcoat lobe
+     * based on GTR1.
+     * \param m_alpha
+     *     The roughness of the surface.
+     */
+    GTR1Isotropic(Float alpha) : m_alpha(alpha){};
+
+    Float eval(const Vector3f &m) const {
+        Float cos_theta  = Frame3f::cos_theta(m),
+        cos_theta2 = ek::sqr(cos_theta), alpha2 = ek ::sqr(m_alpha);
+
+        Float result = (alpha2 - 1.f) / (ek::Pi<Float> * ek::log(alpha2) *
+                (1.f + (alpha2 - 1.f) * cos_theta2));
+
+        return ek::select(result * cos_theta > 1e-20f, result, 0.f);
+    }
+
+    Float pdf(const Vector3f &m) const {
+        return ek::select(m.z() < 0.f, 0.f, Frame3f::cos_theta(m) * eval(m));
+    }
+
+    Normal3f sample(const Point2f &sample) const {
+        auto [sin_phi, cos_phi] = ek::sincos((2.f * ek::Pi<Float>) *sample.x());
+        Float alpha2            = ek::sqr(m_alpha);
+
+        Float cos_theta2 =
+                (1.f - ek::pow(alpha2, 1.f - sample.y())) / (1.f - alpha2);
+
+        Float sin_theta = ek::sqrt(ek::max(0.f, 1.f - cos_theta2)),
+        cos_theta = ek::sqrt(ek::max(0.f, cos_theta2));
+
+        return Normal3f(cos_phi * sin_theta, sin_phi * sin_theta, cos_theta);
+    }
+
+private:
+    Float m_alpha;
+};
+
+/**
+ * \brief Separable shadowing-masking for GGX. Mitsuba does not have a GGX1
+ * support in microfacet so it is added in principled material plugin.
+ * \param wi
+ *     Incident Direction.
+ * \param wo
+ *     Outgoing direction.
+ * \param wh
+ *     Halfway vector.
+ * \param alpha
+ *     Roughness of the clearcoat lobe.
+ * \return Shadowing-Masking term for GGX. Used in clearcoat lobe.
+ */
+template<typename Float>
+Float clearcoat_G(const Vector<Float,3> &wi, const Vector<Float,3> &wo,
+                  const Vector<Float,3> &wh, const Float &alpha) {
+    return smith_ggx1(wi, wh, alpha) * smith_ggx1(wo, wh, alpha);
+}
+
+/**
+ * \brief Calculates Smith ggx shadowing-masking function. Used in
+ * separable masking-shadowing term calculation.
+ * \param v
+ *     Direction for the calculation of the function.
+ * \param wh
+ *     Halfway vector.
+ * \param alpha
+ *     Roughness of the clearcoat lobe.
+ * \return Smith ggx1 shadowing-masking function.
+ */
+template<typename Float>
+Float smith_ggx1(const Vector<Float,3> &v, const Vector<Float,3> &wh,
+                 const Float &alpha) {
+    using Frame3f     = Frame<Float>;
+    Float alpha_2     = ek::sqr(alpha),
+    cos_theta   = ek::abs(Frame3f::cos_theta(v)),
+    cos_theta_2 = ek::sqr(cos_theta),
+    tan_theta_2 = (1.0f - cos_theta_2) / cos_theta_2;
+
+    Float result =
+            2.0f * ek::rcp(1.0f + ek::sqrt(1.0f + alpha_2 * tan_theta_2));
+
+    // Perpendicular incidence -- no shadowing/masking
+    ek::masked(result, ek::eq(v.z(), 1.f)) = 1.f;
+    /* Ensure consistent orientation (can't see the back
+       of the microfacet from the front and vice versa) */
+    ek::masked(result, ek::dot(v, wh) * Frame3f::cos_theta(v) <= 0.f) = 0.f;
+    return result;
+}
+
+/**
+ * \brief Get the flag which determines whether the corresponding
+ * feature is going to be implemented or not.
+ * \param name
+ *     Name of the feature.
+ * \param props
+ *     Given properties.
+ * \return the flag of the feature.
+ */
+bool get_flag(const std::string &name, const Properties &props) {
+    if (props.has_property(name)) {
+        if (props.type(name) == Properties::Type::Float &&
+        std::stof(props.as_string(name)) == 0.0f)
+            return false;
+        else
+            return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * \brief Computes the schlick weight for Fresnel Schlick approximation.
+ * \param cos_i
+ *     Incident angle of the ray based on microfacet normal.
+ * \return schlick weight
+ */
+template <typename Float>
+Float schlick_weight(Float cos_i) {
+    Float m = ek::clamp(1.0f - cos_i, 0.0f, 1.0f);
+    return ek::sqr(ek::sqr(m)) * m;
+}
+
+/**
+ * \brief Schlick Approximation for Fresnel Reflection coefficient F = R0 +
+ * (1-R0) (1-cos^5(i)). Transmitted ray's angle should be used for eta<1.
+ * \param R0
+ *     Incident specular. (Fresnel term when incident ray is aligned with
+ *     the surface normal.)
+ * \param cos_theta_i
+ *     Incident angle of the ray based on microfacet normal.
+ * \return Schlick approximation result.
+ */
+template <typename T,typename Float>
+T calc_schlick(T R0, Float cos_theta_i,Float eta){
+    ek::mask_t<Float> outside_mask = cos_theta_i >= 0.0f;
+    Float rcp_eta     = ek::rcp(eta),
+    eta_it      = ek::select(outside_mask, eta, rcp_eta),
+    eta_ti      = ek::select(outside_mask, rcp_eta, eta);
+
+    Float cos_theta_t_sqr = ek::fnmadd(
+            ek::fnmadd(cos_theta_i, cos_theta_i, 1.0f), ek::sqr(eta_ti), 1.0f);
+    Float cos_theta_t = ek::safe_sqrt(cos_theta_t_sqr);
+    return ek::select(
+            eta_it > 1.0f,
+            ek::lerp(schlick_weight(ek::abs(cos_theta_i)), 1.0f, R0),
+            ek::lerp(schlick_weight(cos_theta_t), 1.0f, R0));
+}
+
+/**
+ * \brief Approximation of incident specular based on relative index of
+ * refraction.
+ * \param eta
+ *     Relative index of refraction.
+ * \return Incident specular
+ */
+template <typename Float>
+Float schlick_R0_eta(Float eta){
+    return ek::sqr((eta - 1.0f) / (eta + 1.0f));
+}
+
+/**
+ * \brief Computes a mask for macro-micro surface incompatibilities.
+ * \param m
+ *     Micro surface normal.
+ * \param wi
+ *     Incident direction.
+ * \param wo
+ *     Outgoing direction.
+ * \param cos_theta_i
+ *     Incident angle
+ * \param reflection
+ *     Flag for determining reflection or refraction case.
+ * \return  Macro-micro surface compatibility mask.
+ */
+template <typename Float>
+ek::mask_t<Float> mac_mic_compatibility(const Vector<Float,3> &m,
+                                        const Vector<Float,3> &wi,
+                                        const Vector<Float,3> &wo,
+                                        const Float &cos_theta_i,
+                                        bool reflection) {
+    if (reflection) {
+        return (ek::dot(wi, ek::mulsign(m, cos_theta_i)) > 0.0f) &&
+        (ek::dot(wo, ek::mulsign(m, cos_theta_i)) > 0.0f);
+    } else {
+        return (ek::dot(wi, ek::mulsign(m, cos_theta_i)) > 0.0f) &&
+        (ek::dot(wo, ek::mulsign_neg(m, cos_theta_i)) > 0.0f);
+    }
+}
+
+/**
+ * \brief  Modified fresnel function for the principled material. It blends
+ * metallic and dielectric responses (not true metallic). spec_tint portion
+ * of the dielectric response is tinted towards base_color. Schlick
+ * approximation is used for spec_tint and metallic parts whereas dielectric
+ * part is calculated with the true fresnel dielectric implementation.
+ * \param F_dielectric
+ *     True dielectric response.
+ * \param metallic
+ *     Metallic weight.
+ * \param spec_tint
+ *     Specular tint weight.
+ * \param base_color
+ *     Base color of the material.
+ * \param lum
+ *     Luminance of the base color.
+ * \param cos_theta_i
+ *     Incident angle of the ray based on microfacet normal.
+ * \param front_side
+ *     Mask for front side of the macro surface.
+ * \param bsdf
+ *     Weight of the BSDF major lobe.
+ * \return Fresnel term of principled BSDF with metallic and dielectric response
+ * combined.
+ */
+template<typename Float,typename T>
+T principled_fresnel(const Float &F_dielectric, const Float &metallic,
+                     const Float &spec_tint,
+                     const T &base_color,
+                     const Float &lum, const Float &cos_theta_i,
+                     const ek::mask_t<Float> &front_side,
+                     const Float &bsdf, const Float &eta,
+                     bool has_metallic, bool has_spec_tint) {
+    // Outside mask based on micro surface
+    ek::mask_t<Float> outside_mask = cos_theta_i >= 0.0f;
+    Float rcp_eta = ek::rcp(eta);
+    Float eta_it  = ek::select(outside_mask, eta, rcp_eta);
+    T F_schlick(0.0f);
+
+    // Metallic component based on Schlick.
+    if (has_metallic) {
+        F_schlick += metallic * calc_schlick<T>(
+                base_color, cos_theta_i,eta);
+    }
+
+    // Tinted dielectric component based on Schlick.
+    if (has_spec_tint) {
+        T c_tint       =
+                ek::select(lum > 0.0f, base_color / lum, 1.0f);
+        T F0_spec_tint =
+                c_tint * schlick_R0_eta(eta_it);
+        F_schlick +=
+                (1.0f - metallic) * spec_tint *
+                calc_schlick<T>(F0_spec_tint, cos_theta_i,eta);
+    }
+
+    // Front side fresnel.
+    T F_front =
+            (1.0f - metallic) * (1.0f - spec_tint) * F_dielectric + F_schlick;
+    /* For back side there is no tint or metallic, just true dielectric
+       fresnel.*/
+    return ek::select(front_side, F_front, bsdf * F_dielectric);
+}
+
+/**
+* \brief  Modified Fresnel function for thin film approximation. It
+* calculates the tinted Fresnel factor with Schlick Approximation.
+* \param F_dielectric
+*     True dielectric response.
+* \param spec_tint
+*     Specular tint weight.
+* \param base_color
+*     Base color of the material.
+* \param lum
+*     Luminance of the base color.
+* \param cos_theta_i
+*     Incident angle of the ray based on microfacet normal.
+* \param eta_t
+*     Relative index of Refraction of the thin Film
+* \return Fresnel term of the thin BSDF with normal and tinted response
+* combined.
+*/
+template<typename Float,typename T>
+T thin_fresnel(const Float &F_dielectric, const Float &spec_tint,
+             const T &base_color, const Float &lum,
+             const Float &cos_theta_i, const Float &eta_t,
+             bool has_spec_tint) {
+    T F_schlick(0.0f);
+    // Tinted dielectric component based on Schlick.
+    if (has_spec_tint) {
+        T c_tint       = ek::select(lum > 0.0f, base_color / lum, 1.0f);
+        T F0_spec_tint = c_tint * schlick_R0_eta(eta_t);
+        F_schlick = calc_schlick<T>(F0_spec_tint, cos_theta_i,eta_t);
+    }
+    return ek::lerp(F_dielectric,F_schlick,spec_tint);
+}
+
+/**
+ * \brief Calculates the microfacet distribution parameters based on
+ * Disney Course Notes.
+ * \param anisotropic
+ *     Anisotropy weight.
+ * \param roughness
+ *     Roughness parameter of the material.
+ * \return Microfacet Distribution roughness parameters: alpha_x, alpha_y.
+ */
+template<typename Float>
+std::pair<Float, Float> calc_dist_params(Float anisotropic,
+                                         Float roughness,
+                                         bool has_anisotropic){
+    Float roughness_2 = ek::sqr(roughness);
+    if (!has_anisotropic) {
+        Float a = ek::max(0.001f, roughness_2);
+        return { a, a };
+    }
+    Float aspect = ek::sqrt(1.0f - 0.9f * anisotropic);
+    return { ek::max(0.001f, roughness_2 / aspect),
+             ek::max(0.001f, roughness_2 * aspect) };
+}
+NAMESPACE_END(mitsuba)
---
+++ src/bsdfs/tests/test_principledthin.py
@@ -0,0 +1,119 @@
+import pytest
+import enoki as ek
+from mitsuba.python.chi2 import ChiSquareTest, BSDFAdapter, SphericalDomain
+
+def test01_chi2_thin_front_side(variants_vec_backends_once_rgb):
+    from mitsuba.core import ScalarVector3f
+    # front_side thin
+    xml = """<float name="roughness" value="0.6"/>
+             <float name="anisotropic" value="0.5"/>
+             <float name="spec_trans" value="0.4"/>
+             <float name="eta" value="1.3296"/>
+             <float name="diff_trans" value="0.6"/>
+          """
+    wi = ek.normalize(ScalarVector3f([1, 0, 1]))
+    sample_func, pdf_func = BSDFAdapter("principledthin", xml, wi=wi)
+    chi2 = ChiSquareTest(
+        domain=SphericalDomain(),
+        sample_func=sample_func,
+        pdf_func=pdf_func,
+        sample_dim=3
+    )
+    assert chi2.run()
+
+
+def test02_chi2_thin_back_side(variants_vec_backends_once_rgb):
+    from mitsuba.core import ScalarVector3f
+    # back side thin
+    xml = """<float name="roughness" value="0.6"/>
+             <float name="anisotropic" value="0.5"/>
+             <float name="spec_trans" value="0.6"/>
+             <float name="eta" value="1.3296"/>
+             <float name="diff_trans" value="0.9"/>
+        """
+    wi = ek.normalize(ScalarVector3f([1, 0, -1]))
+    sample_func, pdf_func = BSDFAdapter("principledthin", xml, wi=wi)
+    chi2 = ChiSquareTest(
+        domain=SphericalDomain(),
+        sample_func=sample_func,
+        pdf_func=pdf_func,
+        sample_dim=3
+    )
+    assert chi2.run()
+
+
+def test03_eval_pdf_thin(variant_scalar_rgb):
+    from mitsuba.core import Frame3f, load_string
+    from mitsuba.render import BSDFContext, BSDFFlags, SurfaceInteraction3f
+
+    # The true values are defined by the first implementation in order to
+    # prevent unwanted changes.
+    pdf_true = [
+        0.18230389058589935,
+        0.17071931064128876,
+        0.1604636013507843,
+        0.15113641321659088,
+        0.1424213945865631,
+        0.13407252728939056,
+        0.1259022206068039,
+        0.1177704930305481,
+        0.10957615822553635,
+        0.1012495756149292,
+        0.09274674206972122,
+        0.0840444564819336,
+        0.07513649761676788,
+        0.06603056192398071,
+        0.05674567073583603,
+        0.04731012135744095,
+        0.03775978833436966,
+        0.028136683627963066,
+        0.01848774217069149,
+        0.008863822557032108]
+    evaluate_true = [
+        0.04944333806633949,
+        0.04872140288352966,
+        0.047846242785453796,
+        0.0468028225004673,
+        0.04557877406477928,
+        0.04416417330503464,
+        0.042551249265670776,
+        0.04073421657085419,
+        0.03870923072099686,
+        0.036474503576755524,
+        0.03403010591864586,
+        0.0313769206404686,
+        0.028513599187135696,
+        0.025431113317608833,
+        0.02210502326488495,
+        0.01848825439810753,
+        0.014510626904666424,
+        0.01009628176689148,
+        0.005216196645051241,
+        3.899415020768372e-18]
+
+    bsdf = load_string("""<bsdf version='2.0.0' type='principledthin'>
+                      <float name="eta" value="1.5"/>
+                      <float name="anisotropic" value="0.5"/>
+                      <float name="sheen" value="0.5"/>
+                      <float name="sheen_tint" value="0.2"/>
+                      <float name="spec_trans" value="0.5"/>
+                      <float name="flatness" value="0.5"/>
+                      <float name="diff_trans" value="0.6"/>
+                      </bsdf>
+                      """)
+
+    si = SurfaceInteraction3f()
+    si.p = [0, 0, 0]
+    si.n = [0, 0, 1]
+    si.wi = [1, 0, 1]
+    si.sh_frame = Frame3f(si.n)
+
+    ctx = BSDFContext()
+    pdf = []
+    evaluate = []
+    for i in range(20):
+        theta = i / 19.0 * (ek.Pi / 2)
+        wo = [ek.sin(theta), 0, ek.cos(theta)]
+        assert ek.allclose(bsdf.pdf(ctx, si, wo=wo), pdf_true[i])
+        assert ek.allclose(bsdf.eval(ctx, si, wo=wo)[0], evaluate_true[i])
+        
