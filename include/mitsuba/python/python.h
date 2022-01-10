0cb22ddeea1b1448043b4b11aa8f3c59f3b1d16b
Wenzel Jakob <wenzel.jakob@epfl.ch>
2022-01-10 23:47:31 +0100

    updated Enoki

--- include/mitsuba/python/python.h
+++ include/mitsuba/python/python.h
@@ -190,7 +190,7 @@ template <typename Array> void bind_enoki_ptr_array(py::class_<Array> &cls) {
     if constexpr (ek::is_jit_array_v<Array>) {
         cls.def("index", [](const Array &a) { return a.index(); });
         cls.def("label_", [](const Array &a) { return a.label_(); });
-        cls.def("set_label_", [](const Array &a, const char *label) { a.set_label_(label); });
+        cls.def("set_label_", [](Array &a, const char *label) { a.set_label_(label); });
         cls.def("set_index_", [](Array &a, uint32_t index) { *ek::detach(a).index_ptr() = index; });
     }
 
