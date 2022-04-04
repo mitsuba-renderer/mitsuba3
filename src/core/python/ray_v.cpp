e940662bb71175b1d5547000a26024a1a904b0f4
Delio Vicini <delio.vicini@gmail.com>
2022-04-04 14:32:36 +0200

    Add Ray2f python bindings

--- src/core/python/ray_v.cpp
+++ src/core/python/ray_v.cpp
@@ -1,30 +1,40 @@
 #include <mitsuba/core/ray.h>
 #include <mitsuba/python/python.h>
 
-MI_PY_EXPORT(Ray) {
+template<typename Ray>
+void bind_ray(py::module &m, const char *name) {
     MI_PY_IMPORT_TYPES()
-    {
-        auto ray = py::class_<Ray3f>(m, "Ray3f", D(Ray))
+    using Vector = typename Ray::Vector;
+    using Point  = typename Ray::Point;
+
+    MI_PY_CHECK_ALIAS(Ray, name) {
+        auto ray = py::class_<Ray>(m, name, D(Ray))
             .def(py::init<>(), "Create an unitialized ray")
-            .def(py::init<const Ray3f &>(), "Copy constructor", "other"_a)
-            .def(py::init<Point3f, Vector3f, Float, const Wavelength &>(),
+            .def(py::init<const Ray &>(), "Copy constructor", "other"_a)
+            .def(py::init<Point, Vector, Float, const Wavelength &>(),
                  D(Ray, Ray, 5),
                  "o"_a, "d"_a, "time"_a=0.0, "wavelengths"_a=Wavelength())
-            .def(py::init<Point3f, Vector3f, Float, Float, const Wavelength &>(),
+            .def(py::init<Point, Vector, Float, Float, const Wavelength &>(),
                  D(Ray, Ray, 6),
                 "o"_a, "d"_a, "maxt"_a, "time"_a, "wavelengths"_a)
-            .def(py::init<const Ray3f &, Float>(),
+            .def(py::init<const Ray &, Float>(),
                 D(Ray, Ray, 7), "other"_a, "maxt"_a)
-            .def("__call__", &Ray3f::operator(), D(Ray, operator, call), "t"_a)
-            .def_field(Ray3f, o,           D(Ray, o))
-            .def_field(Ray3f, d,           D(Ray, d))
-            .def_field(Ray3f, maxt,        D(Ray, maxt))
-            .def_field(Ray3f, time,        D(Ray, time))
-            .def_field(Ray3f, wavelengths, D(Ray, wavelengths))
-            .def_repr(Ray3f);
-
-        MI_PY_DRJIT_STRUCT(ray, Ray3f, o, d, maxt, time, wavelengths)
+            .def("__call__", &Ray::operator(), D(Ray, operator, call), "t"_a)
+            .def_field(Ray, o,           D(Ray, o))
+            .def_field(Ray, d,           D(Ray, d))
+            .def_field(Ray, maxt,        D(Ray, maxt))
+            .def_field(Ray, time,        D(Ray, time))
+            .def_field(Ray, wavelengths, D(Ray, wavelengths))
+            .def_repr(Ray);
+        MI_PY_DRJIT_STRUCT(ray, Ray, o, d, maxt, time, wavelengths)
     }
+}
+
+MI_PY_EXPORT(Ray) {
+    MI_PY_IMPORT_TYPES()
+
+    bind_ray<Ray<Point2f, Spectrum>>(m, "Ray2f");
+    bind_ray<Ray3f>(m, "Ray3f");
 
     {
         auto raydiff = py::class_<RayDifferential3f, Ray3f>(m, "RayDifferential3f", D(RayDifferential))
