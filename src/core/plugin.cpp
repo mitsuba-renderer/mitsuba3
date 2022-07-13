188261b3dd39ac3fb44b6558cba957b1486a2eaf
Sebastien Speierer <sebastieneps@gmail.com>
2022-07-13 11:28:47 +0200

    Another attempt to fix scene destruction

--- src/core/plugin.cpp
+++ src/core/plugin.cpp
@@ -121,7 +121,6 @@ struct PluginManager::PluginManagerPrivate {
 };
 
 ref<PluginManager> PluginManager::m_instance = new PluginManager();
-bool PluginManager::m_alive = true;
 
 PluginManager::PluginManager() : d(new PluginManagerPrivate()) { }
 
@@ -129,7 +128,6 @@ PluginManager::~PluginManager() {
     std::lock_guard<std::mutex> guard(d->m_mutex);
     for (auto &pair: d->m_plugins)
         delete pair.second;
-    m_alive = false;
 }
 
 void PluginManager::ensure_plugin_loaded(const std::string &name) {
