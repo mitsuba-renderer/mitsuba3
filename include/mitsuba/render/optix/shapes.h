56dcab1ac8d9233cd5ee029f65b33ac1ede0dc61
Sebastien Speierer <sebastieneps@gmail.com>
2022-09-02 14:13:30 +0200

    Update Dr.Jit Optix API and fix multithreading issues

--- include/mitsuba/render/optix/shapes.h
+++ include/mitsuba/render/optix/shapes.h
@@ -18,21 +18,21 @@
 
 NAMESPACE_BEGIN(mitsuba)
 /// List of the custom shapes supported by OptiX
-static std::string custom_optix_shapes[] = {
+static std::string CUSTOM_OPTIX_SHAPE_NAMES[] = {
     "Disk", "Rectangle", "Sphere", "Cylinder",
 };
-static constexpr size_t custom_optix_shapes_count = std::size(custom_optix_shapes);
+static constexpr size_t CUSTOM_OPTIX_SHAPE_COUNT = std::size(CUSTOM_OPTIX_SHAPE_NAMES);
 
 /// Retrieve index of custom shape descriptor in the list above for a given shape
 template <typename Shape>
 size_t get_shape_descr_idx(Shape *shape) {
     std::string name = shape->class_()->name();
-    for (size_t i = 0; i < custom_optix_shapes_count; i++) {
-        if (custom_optix_shapes[i] == name)
+    for (size_t i = 0; i < CUSTOM_OPTIX_SHAPE_COUNT; i++) {
+        if (CUSTOM_OPTIX_SHAPE_NAMES[i] == name)
             return i;
     }
     Throw("Unexpected shape: %s. Couldn't be found in the "
-          "'custom_optix_shapes' table.", name);
+          "'CUSTOM_OPTIX_SHAPE_NAMES' table.", name);
 }
 
 /// Stores two OptiXTraversables: one for the meshes and one for the custom shapes (e.g. sphere)
