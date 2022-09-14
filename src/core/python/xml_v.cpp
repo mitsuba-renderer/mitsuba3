8faf1ba54d631978454738e695dee6164c22e123
Sebastien Speierer <sebastieneps@gmail.com>
2022-09-14 16:46:39 +0200

    Enable differentiation of Sphere to_world parameter

--- src/core/python/xml_v.cpp
+++ src/core/python/xml_v.cpp
@@ -266,7 +266,7 @@ std::vector<ref<Object>> load_dict(const std::string &dict_key,
             }
 
             // Nested dict with type == "ref" specify a reference to another
-            // object previously instanciated
+            // object previously instantiated
             if (type2 == "ref") {
                 if (is_scene)
                     Throw("Reference found at the scene level: %s", key);
