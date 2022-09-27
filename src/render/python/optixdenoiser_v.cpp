10dad687782915d616c95014af39973f3ff2f257
Nicolas Roussel <n.roussel13@gmail.com>
2022-09-27 16:46:48 +0200

    Change interface from pointers to references in OptixDenoiser

--- src/render/python/optixdenoiser_v.cpp
+++ src/render/python/optixdenoiser_v.cpp
@@ -9,25 +9,43 @@ MI_PY_EXPORT(OptixDenoiser) {
         .def(py::init<const ScalarVector2u &, bool, bool, bool>(),
              "input_size"_a, "albedo"_a = false, "normals"_a = false,
              "temporal"_a = false, D(OptixDenoiser, OptixDenoiser))
-        .def("__call__",
-             py::overload_cast<const TensorXf &, bool, const TensorXf *,
-                               const TensorXf *, const Transform4f *,
-                               const TensorXf *, const TensorXf *>(
-             &OptixDenoiser::operator(), py::const_),
-             "noisy"_a, "denoise_alpha"_a = true, "albedo"_a = nullptr,
-             "normals"_a = nullptr, "n_frame"_a = nullptr, "flow"_a = nullptr,
-             "previous_denoised"_a = nullptr,
-             D(OptixDenoiser, operator_call))
-        .def("__call__",
-             py::overload_cast<const ref<Bitmap> &, bool, const std::string &,
-                               const std::string &, const Transform4f &,
-                               const std::string &, const std::string &,
-                               const std::string &>(
-             &OptixDenoiser::operator(), py::const_),
-             "noisy"_a, "denoise_alpha"_a = true, "albedo_ch"_a = "",
-             "normals_ch"_a = "", "n_frame"_a = Transform4f(), "flow_ch"_a = "",
-             "previous_denoised_ch"_a = "", "noisy_ch"_a = "<root>",
-             D(OptixDenoiser, operator_call, 2));
+        .def(
+            "__call__",
+            [](const OptixDenoiser &denoiser, const TensorXf &noisy,
+               bool denoise_alpha, const TensorXf &albedo,
+               const TensorXf &normals, const py::object &transform,
+               const TensorXf &flow, const TensorXf &previous_denoised) {
+                Transform4f to_sensor;
+                if (!transform.is(py::none()))
+                    to_sensor = transform.cast<Transform4f>();
+
+                return denoiser(noisy, denoise_alpha, albedo, normals,
+                                to_sensor, flow, previous_denoised);
+            },
+            "noisy"_a, "denoise_alpha"_a = true, "albedo"_a = TensorXf(),
+            "normals"_a = TensorXf(), "to_sensor"_a = py::none(),
+            "flow"_a = TensorXf(), "previous_denoised"_a = TensorXf(),
+            D(OptixDenoiser, operator_call))
+        .def(
+            "__call__",
+            [](const OptixDenoiser &denoiser, const ref<Bitmap> &noisy,
+               bool denoise_alpha, const std::string &albedo_ch,
+               const std::string &normals_ch, const py::object &transform,
+               const std::string &flow_ch,
+               const std::string &previous_denoised_ch,
+               const std::string &noisy_ch) {
+                Transform4f to_sensor;
+                if (!transform.is(py::none()))
+                    to_sensor = transform.cast<Transform4f>();
+
+                return denoiser(noisy, denoise_alpha, albedo_ch, normals_ch,
+                                to_sensor, flow_ch, previous_denoised_ch,
+                                noisy_ch);
+            },
+            "noisy"_a, "denoise_alpha"_a = true, "albedo_ch"_a = "",
+            "normals_ch"_a = "", "to_sensor"_a = py::none(), "flow_ch"_a = "",
+            "previous_denoised_ch"_a = "", "noisy_ch"_a = "<root>",
+            D(OptixDenoiser, operator_call, 2));
 }
 
 #endif // defined(MI_ENABLE_CUDA)
