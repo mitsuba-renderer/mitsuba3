46bac78e26d80545e6c133cb2c1aec3151f0499b
Sebastien Speierer <sebastieneps@gmail.com>
2022-10-12 09:24:10 +0200

    Revert "Fix cleanup function callbacks in Python"
    This reverts commit d214202e8bca93fc810fb99bb3a536f25b0432cf.

--- src/python/main.cpp
+++ src/python/main.cpp
@@ -127,16 +127,21 @@ PYBIND11_MODULE(mitsuba_ext, m) {
     MI_PY_IMPORT(Sensor);
     MI_PY_IMPORT(FilmFlags);
 
-    // Register a cleanup callback function
-    auto atexit = py::module_::import("atexit");
-    atexit.attr("register")(py::cpp_function([]() {
-        Profiler::static_shutdown();
-        Bitmap::static_shutdown();
-        Logger::static_shutdown();
-        Thread::static_shutdown();
-        Class::static_shutdown();
-        Jit::static_shutdown();
-    }));
+    /* Register a cleanup callback function that is invoked when
+       the 'mitsuba::Object' Python type is garbage collected */
+    py::cpp_function cleanup_callback(
+        [](py::handle weakref) {
+            Profiler::static_shutdown();
+            Bitmap::static_shutdown();
+            Logger::static_shutdown();
+            Thread::static_shutdown();
+            Class::static_shutdown();
+            Jit::static_shutdown();
+            weakref.dec_ref();
+        }
+    );
+
+    (void) py::weakref(m.attr("Object"), cleanup_callback).release();
 
     // Change module name back to correct value
     m.attr("__name__") = "mitsuba_ext";
