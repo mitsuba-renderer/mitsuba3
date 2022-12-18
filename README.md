10d3514a0295cad4ac6d440c7ff326561c6da6a2
Wenzel Jakob <wenzel.jakob@epfl.ch>
2022-12-18 21:41:19 +0100

    fixed HG issues in issue #455

--- README.md
+++ README.md
@@ -134,11 +134,11 @@ Significant features and/or improvements to the code were contributed by
 When using Mitsuba 3 in academic projects, please cite:
 
 ```bibtex
-@software{jakob2022mitsuba3,
+@software{Mitsuba3,
     title = {Mitsuba 3 renderer},
     author = {Wenzel Jakob and Sébastien Speierer and Nicolas Roussel and Merlin Nimier-David and Delio Vicini and Tizian Zeltner and Baptiste Nicolet and Miguel Crespo and Vincent Leroy and Ziyi Zhang},
     note = {https://mitsuba-renderer.org},
-    version = {3.0.1},
-    year = 2022,
+    version = {3.1.1},
+    year = 2022
 }
 ```
--- src/phase/tests/test_hg.py
+++ src/phase/tests/test_hg.py
@@ -1,12 +1,15 @@
 import mitsuba as mi
+import pytest
 
 
 def test01_create(variant_scalar_rgb):
     p = mi.load_dict({"type": "hg", "g": 0.4})
     assert p is not None
 
-def test02_chi2(variants_vec_backends_once_rgb):
-    sample_func, pdf_func = mi.chi2.PhaseFunctionAdapter("hg", '<float name="g" value="0.6"/>')
+
+@pytest.mark.parametrize('g', ['0.6', '-0.6'])
+def test02_chi2(variants_vec_backends_once_rgb, g):
+    sample_func, pdf_func = mi.chi2.PhaseFunctionAdapter("hg", f'<float name="g" value="{g}"/>')
 
     chi2 = mi.chi2.ChiSquareTest(
         domain=mi.chi2.SphericalDomain(),
