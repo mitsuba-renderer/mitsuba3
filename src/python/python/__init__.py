a8c8e0cdbddc42787655346425d27ecccca34f42
Vincent Leroy <vincent.leroy@rayference.eu>
2023-04-26 13:45:42 +0200

    Add mi.variant_context()

--- src/python/python/__init__.py
+++ src/python/python/__init__.py
@@ -1,4 +1,4 @@
-from .util import traverse, SceneParameters, render, cornell_box
+from .util import traverse, SceneParameters, render, cornell_box, variant_context
 from . import chi2
 from . import xml
 from . import ad
