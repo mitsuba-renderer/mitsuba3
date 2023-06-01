ca3b8699e97b3e0f7d016ec86c0a818493d18b1b
Nicolas Roussel <n.roussel13@gmail.com>
2023-06-01 17:16:39 +0200

    Fix `LargeSteps` documentation, add updated shape optimization tutorial

--- src/python/python/ad/largesteps.py
+++ src/python/python/ad/largesteps.py
@@ -2,13 +2,14 @@ from __future__ import annotations as __annotations__ # Delayed parsing of type 
 
 import mitsuba as mi
 import drjit as dr
-import numpy as np
 
 def mesh_laplacian(n_verts, faces, lambda_):
     """
     Compute the index and data arrays of the (combinatorial) Laplacian matrix of
     a given mesh.
     """
+    import numpy as np
+
     # Neighbor indices
     ii = faces[:, [1, 2, 0]].flatten()
     jj = faces[:, [2, 0, 1]].flatten()
@@ -33,7 +34,7 @@ class SolveCholesky(dr.CustomOp):
 
     def eval(self, solver, u):
         self.solver = solver
-        x = dr.empty(dr.cuda.TensorXf, shape=u.shape)
+        x = dr.empty(mi.TensorXf, shape=u.shape)
         solver.solve(u, x)
         return mi.TensorXf(x)
 
@@ -43,7 +44,7 @@ class SolveCholesky(dr.CustomOp):
         self.set_grad_out(x)
 
     def backward(self):
-        x = dr.empty(dr.cuda.TensorXf, shape=self.grad_out().shape)
+        x = dr.empty(mi.TensorXf, shape=self.grad_out().shape)
         self.solver.solve(self.grad_out(), x)
         self.set_grad_in('u', x)
 
@@ -65,23 +66,20 @@ class LargeSteps():
 
     It can then convert vertex coordinates back and forth between their
     cartesian and differential representations. Both transformations are
-    differentiable, which allows optimizing meshes using the differential form
-    as a latent variable.
+    differentiable, meshes can therefore be optimized by using the differential
+    form as a latent variable.
     """
     def __init__(self, verts, faces, lambda_=19.0):
         """
         Build the system matrix and its Cholesky factorization.
 
-        Params
-        ------
+        Parameter ``verts`` (``mitsuba.Float``):
+            Vertex coordinates of the mesh.
 
-        verts: mi.Float
-            The vertex coordinates of the mesh.
+        Parameter ``faces`` (``mitsuba.UInt``):
+            Face indices of the mesh.
 
-        faces: mi.UInt
-            The face indices of the mesh.
-
-        lambda_: Float
+        Parameter ``lambda_`` (``float``):
             The hyper parameter λ. This controls how much gradients are diffused
             on the surface. this value should increase with the tesselation of
             the mesh.
@@ -93,6 +91,7 @@ class LargeSteps():
             from cholespy import CholeskySolverF as CholeskySolver
 
         from cholespy import MatrixType
+        import numpy as np
 
         v = verts.numpy().reshape((-1,3))
         f = faces.numpy().reshape((-1,3))
@@ -106,7 +105,7 @@ class LargeSteps():
         self.inverse = mi.UInt(inverse_v)
         self.n_verts = v_unique.shape[0]
 
-        # CHOLMOD expects matrices without duplicate entries as input, so we need to sum them manually
+        # Solver expects matrices without duplicate entries as input, so we need to sum them manually
         indices, values = mesh_laplacian(self.n_verts, f_unique, lambda_)
         indices_unique, inverse_idx = np.unique(indices, axis=1, return_inverse=True)
 
@@ -126,20 +125,12 @@ class LargeSteps():
         This method typically only needs to be called once per mesh, to obtain
         the latent variable before optimization.
 
-        Params
-        ------
+        Parameter ``v`` (``mitsuba.Float``):
+            Vertex coordinates of the mesh.
 
-        v: Float
-            The vertex coordinates of the mesh.
-
-        Returns
-        -------
-
-        u: Float
-            The differential form of v.
+        Returns ``mitsuba.Float`:
+            Differential form of v.
         """
-        # TODO: support arbitrary components, not just 3
-
         # Manual matrix-vector multiplication
         v_unique = dr.gather(mi.Point3f, dr.unravel(mi.Point3f, v), self.index)
         row_prod = dr.gather(mi.Point3f, v_unique, self.cols.array) * self.data.array
@@ -159,17 +150,11 @@ class LargeSteps():
         This method is typically called at each iteration of the optimization,
         to update the mesh coordinates before rendering.
 
-        Params
-        ------
+        Parameter ``u`` (``mitsuba.Float``):
+            Differential form of v.
 
-        u: Float
-            The differential form of v.
-
-        Returns
-        -------
-
-        v: Float
-            The vertex coordinates of the mesh.
+        Returns ``mitsuba.Float`:
+            Vertex coordinates of the mesh.
         """
         v_unique = dr.unravel(mi.Point3f, dr.custom(SolveCholesky, self.solver, mi.TensorXf(u, (self.n_verts, 3))).array)
         return dr.ravel(dr.gather(mi.Point3f, v_unique, self.inverse))
