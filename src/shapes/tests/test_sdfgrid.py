SDFGRid: Graceful handling when Float type is double precision. Fix OptiX leaks for instance AS inputs. Add instance test

src/shapes/tests/test_sdfgrid.py
@@ -5,7 +5,7 @@ import mitsuba as mi
 # TODO: change variants once they're all implemented
 
 def test01_create(variant_scalar_rgb):
-    for normal_method in ["analytic", "smooth", "falcao"]:
+    for normal_method in ["analytic", "smooth"]:
         s = mi.load_dict({
             "type" : "sdfgrid",
             "normals" : normal_method
@@ -87,6 +87,70 @@ def test04_ray_intersect(variants_all_ad_rgb):
                 direction = mi.Vector3f(0, 0, -1)
                 ray = mi.Ray3f(o=origin, d=direction)
 
+                si_found = s.ray_test(ray)
+                assert si_found == (x >= 0 and x <= 1 and y >= 0 and y<= 1)
+
+                if si_found[0]:
+                    si = s.ray_intersect(ray, mi.RayFlags.All, True)
+
+                    assert dr.allclose(si.t, 2 + y)
+                    assert dr.allclose(si.n, [0, 1 / np.sqrt(2), 1 / np.sqrt(2)])
+                    assert dr.allclose(si.p, ray.o - mi.Vector3f(0, 0, 2 + y))
+
+
+def test05_ray_intersect_instancing(variants_all_ad_rgb):
+    pytest.importorskip("numpy")
+    import numpy as np
+
+
+    mi.set_log_level(mi.LogLevel.Info)
+    # Diagonal plane
+    sdf_grid = np.array([
+        -np.sqrt(2)/2, -np.sqrt(2)/2, # z = 0, y = 0
+        0, 0, # z = 0, y = 1
+        0, 0, # z = 1, y = 0
+        np.sqrt(2)/2, np.sqrt(2)/2 # z = 1, y = 1
+    ]).reshape((2, 2, 2, 1))
+
+    instance_translations = [mi.ScalarVector3f([0.0, 0.0, 0]), mi.ScalarVector3f([8.0, 0.0, 0.0])]
+
+    s = mi.load_dict({
+        "type" : "scene",
+        'shape_group': {
+            'type': 'shapegroup',
+            'sdf': {
+                'type': 'sdfgrid'
+            }
+        },
+        'first_sdf': {
+            'type': 'instance',
+            'to_world': mi.ScalarTransform4f.translate(instance_translations[0]),
+            'shapegroup': {
+                'type': 'ref',
+                'id': 'shape_group'
+            }
+        },
+        'second_sdf': {
+            'type': 'instance',
+            'to_world': mi.ScalarTransform4f.translate(instance_translations[1]),
+            'shapegroup': {
+                'type': 'ref',
+                'id': 'shape_group'
+            }
+        }
+    })
+
+    params = mi.traverse(s)
+    params['shape_group.sdf.grid'] = mi.TensorXf(sdf_grid)
+    params.update()
+
+    n = 10
+    for translate in instance_translations:
+        for x in dr.linspace(mi.Float, -2, 2, n):
+            for y in dr.linspace(mi.Float, -2, 2, n):
+                origin = translate + mi.Vector3f(x, y, 3)
+                direction = mi.Vector3f(0, 0, -1)
+                ray = mi.Ray3f(o=origin, d=direction)
                 si_found = s.ray_test(ray)
                 assert si_found == (x >= 0 and x <= 1 and y >= 0 and y<= 1)
 