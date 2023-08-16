f727b4ad50f2dcaafcb90099ca3de207749a89bb
Sebastien Speierer <speierers@meta.com>
2023-08-16 11:02:21 +0200

    Check that dict keys do not contains . character

--- src/core/python/xml_v.cpp
+++ src/core/python/xml_v.cpp
@@ -189,7 +189,7 @@ ref<Object> create_texture_from(const py::dict &dict, bool within_emitter) {
     if (type == "rgb") {
         if (dict.size() != 2) {
             Throw("'rgb' dictionary should always contain 2 entries "
-                    "('type' and 'value'), got %u.", dict.size());
+                  "('type' and 'value'), got %u.", dict.size());
         }
         // Read info from the dictionary
         Properties::Color3f color(0.f);
@@ -206,7 +206,7 @@ ref<Object> create_texture_from(const py::dict &dict, bool within_emitter) {
     } else if (type == "spectrum") {
         if (dict.size() != 2) {
             Throw("'spectrum' dictionary should always contain 2 "
-                    "entries ('type' and 'value'), got %u.", dict.size());
+                  "entries ('type' and 'value'), got %u.", dict.size());
         }
         // Read info from the dictionary
         Properties::Float const_value(1);
@@ -295,6 +295,12 @@ void parse_dictionary(DictParseContext &ctx,
         SET_PROPS(ScalarColor3f, ScalarColor3f, set_color);
         SET_PROPS(ScalarArray3f, ScalarArray3f, set_array3f);
         SET_PROPS(ScalarTransform4f, ScalarTransform4f, set_transform);
+
+        if (key.find('.') != std::string::npos) {
+            Throw("The object key '%s' contains a '.' character, which is "
+                  "already used as a delimiter in the object path in the scene."
+                  " Please use '_' instead.", key);
+        }
 
         // Parse nested dictionary
         if (py::isinstance<py::dict>(value)) {
