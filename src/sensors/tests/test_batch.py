495d963f0608613b0b938c05b64192a854b5bad9
Nicolas Roussel <n.roussel13@gmail.com>
2023-08-28 13:35:24 +0200

    Add `BatchSensor::sample_ray`

--- src/sensors/tests/test_batch.py
+++ src/sensors/tests/test_batch.py
@@ -63,3 +63,46 @@ def test01_create(variant_scalar_rgb, s_open, s_time):
     assert dr.allclose(camera.shutter_open_time(), s_time)
     assert not camera.needs_aperture_sample()
     assert camera.bbox() == mi.BoundingBox3f(*origins)
+
+
+@pytest.mark.parametrize("s_open", [0.0, 1.5])
+@pytest.mark.parametrize("s_time", [0.0, 3.0])
+def test02_sample_ray(variants_vec_spectral, s_open, s_time):
+    """Check the correctness of the sample_ray() method"""
+    near_clip = 1.0
+    camera0 = create_perspective(origins[0], directions[0], s_open=s_open, s_close=s_open + s_time)
+    camera1 = create_perspective(origins[1], directions[1], s_open=s_open, s_close=s_open + s_time)
+    camera = create_batch([camera0, camera1], 512, 256, s_open, s_open + s_time)
+
+    time = 0.5
+    wav_sample = [0.5, 0.33, 0.1]
+    pos_sample = [[0.2, 0.1, 0.6], [0.6, 0.9, 0.2]]
+    aperture_sample = 0 # Not being used
+
+    ray, spec_weight = camera.sample_ray(time, wav_sample, pos_sample, aperture_sample)
+
+    # Importance sample wavelength and weight
+    wav, spec = mi.sample_rgb_spectrum(mi.sample_shifted(wav_sample))
+
+    assert dr.allclose(ray.wavelengths, wav)
+    assert dr.allclose(spec_weight, spec)
+    assert dr.allclose(ray.time, time)
+
+    inv_z0 = dr.rcp((camera0.world_transform().inverse() @ ray.d).z)
+    inv_z1 = dr.rcp((camera1.world_transform().inverse() @ ray.d).z)
+    o0 = mi.Point3f(origins[0]) + near_clip * inv_z0 * mi.Vector3f(ray.d)
+    o1 = mi.Point3f(origins[1]) + near_clip * inv_z1 * mi.Vector3f(ray.d)
+    o = o0
+    dr.scatter(o, dr.gather(mi.Point3f, o1, 2), 2)
+    assert dr.allclose(ray.o, o, atol=1e-4)
+
+    # Check that a [(2*i + 1)/(2 * N), 0.5] for i = 0, 1, ..., #cameras
+    # `position_sample` generates a ray that points in the camera direction
+    position_sample = mi.Point2f([0.25, 0.25, 0.75], [0.5, 0.5, 0.5])
+    ray, _ = camera.sample_ray(0, 0, position_sample, 0)
+    direction = dr.zeros(mi.Vector3f, 3)
+    dr.scatter(direction, mi.Vector3f(directions[0]), mi.UInt32([0, 1]))
+    dr.scatter(direction, mi.Vector3f(directions[1]), mi.UInt32([2]))
+    print(f"{ray.d=}")
+    print(f"{direction=}")
+    assert dr.allclose(ray.d, direction, atol=1e-7)
