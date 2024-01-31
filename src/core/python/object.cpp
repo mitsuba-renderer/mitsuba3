acd5d39a9849d1c3a5460a7cb65a6f4e5715b723
Nicolas Roussel <n.roussel13@gmail.com>
2024-01-31 09:58:37 +0100

    Use nanobind's `intrusive_counter` for `Object` reference counting

--- src/core/python/object.cpp
+++ src/core/python/object.cpp
@@ -39,24 +39,28 @@ MI_PY_EXPORT(Object) {
             return cast_object(pmgr.create_object(props, class_));
         }, D(PluginManager, create_object));
 
-    py::class_<Object, ref<Object>>(m, "Object", D(Object))
-        .def(py::init<>(), D(Object, Object))
-        .def(py::init<const Object &>(), D(Object, Object, 2))
+    nb::class_<Object>(
+            m, "Object",
+            nb::intrusive_ptr<Object>(
+                [](Object *o, PyObject *po) noexcept { o->set_self_py(po); }),
+            D(Object)
+        )
+        .def(nb::init<>(), D(Object, Object))
+        .def(nb::init<const Object &>(), D(Object, Object, 2))
         .def_method(Object, id)
         .def_method(Object, set_id, "id"_a)
-        .def_method(Object, ref_count)
         .def_method(Object, inc_ref)
-        .def_method(Object, dec_ref, "dealloc"_a = true)
-        .def("expand", [](const Object &o) -> py::list {
+        .def_method(Object, dec_ref)
+        .def("expand", [](const Object &o) -> nb::list {
             auto result = o.expand();
-            py::list l;
+            nb::list l;
             for (Object *o2: result)
                 l.append(cast_object(o2));
             return l;
         }, D(Object, expand))
         .def_method(Object, traverse, "cb"_a)
-        .def_method(Object, parameters_changed, "keys"_a = py::list())
-        .def_property_readonly("ptr", [](Object *self) { return (uintptr_t) self; })
-        .def("class_", &Object::class_, py::return_value_policy::reference, D(Object, class))
+        .def_method(Object, parameters_changed, "keys"_a = nb::list())
+        .def_prop_ro("ptr", [](Object *self) { return (uintptr_t) self; })
+        .def("class_", &Object::class_, nb::rv_policy::copy, D(Object, class))
         .def("__repr__", &Object::to_string, D(Object, to_string));
 }
