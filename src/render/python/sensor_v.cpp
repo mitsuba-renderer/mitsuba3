8228af99af56dd91ce4bfdf10446e3b0f6d024f8
Rami Tabbara <rtabbara@gmail.com>
2024-02-09 15:11:25 +0100

    Add Sensor bindings

--- src/render/python/sensor_v.cpp
+++ src/render/python/sensor_v.cpp
@@ -1,11 +1,15 @@
 #include <mitsuba/render/sensor.h>
 #include <mitsuba/core/properties.h>
 #include <mitsuba/python/python.h>
+#include <nanobind/trampoline.h>
+#include <nanobind/stl/string.h>
+#include <drjit/python.h>
 
 /// Trampoline for derived types implemented in Python
 MI_VARIANT class PySensor : public Sensor<Float, Spectrum> {
 public:
     MI_IMPORT_TYPES(Sensor)
+    NB_TRAMPOLINE(Sensor, 13);
 
     PySensor(const Properties &props) : Sensor(props) { }
 
@@ -13,14 +17,14 @@ public:
     sample_ray(Float time, Float sample1, const Point2f &sample2,
                const Point2f &sample3, Mask active) const override {
         using Return = std::pair<Ray3f, Spectrum>;
-        PYBIND11_OVERRIDE_PURE(Return, Sensor, sample_ray, time, sample1, sample2, sample3, active);
+        NB_OVERRIDE_PURE(sample_ray, time, sample1, sample2, sample3, active);
     }
 
     std::pair<RayDifferential3f, Spectrum>
     sample_ray_differential(Float time, Float sample1, const Point2f &sample2,
                             const Point2f &sample3, Mask active) const override {
         using Return = std::pair<RayDifferential3f, Spectrum>;
-        PYBIND11_OVERRIDE(Return, Sensor, sample_ray_differential, time, sample1, sample2, sample3, active);
+        NB_OVERRIDE(sample_ray_differential, time, sample1, sample2, sample3, active);
     }
 
     std::pair<DirectionSample3f, Spectrum>
@@ -28,58 +32,58 @@ public:
                      const Point2f &sample,
                      Mask active) const override {
         using Return = std::pair<DirectionSample3f, Spectrum>;
-        PYBIND11_OVERRIDE_PURE(Return, Sensor, sample_direction, ref, sample, active);
+        NB_OVERRIDE_PURE(sample_direction, ref, sample, active);
     }
 
     Float pdf_direction(const Interaction3f &ref,
                         const DirectionSample3f &ds,
                         Mask active) const override {
-        PYBIND11_OVERRIDE_PURE(Float, Sensor, pdf_direction, ref, ds, active);
+        NB_OVERRIDE_PURE(pdf_direction, ref, ds, active);
     }
 
     Spectrum eval_direction(const Interaction3f &ref,
                             const DirectionSample3f &ds,
                             Mask active)  const override {
-        PYBIND11_OVERRIDE_PURE(Spectrum, Sensor, eval_direction, ref, ds, active);
+        NB_OVERRIDE_PURE(eval_direction, ref, ds, active);
     }
 
     std::pair<PositionSample3f, Float>
     sample_position(Float time, const Point2f &sample,
                     Mask active) const override {
         using Return = std::pair<PositionSample3f, Float>;
-        PYBIND11_OVERRIDE_PURE(Return, Sensor, sample_position, time, sample, active);
+        NB_OVERRIDE_PURE(sample_position, time, sample, active);
     }
 
     Float pdf_position(const PositionSample3f &ps,
                                Mask active) const override {
-        PYBIND11_OVERRIDE_PURE(Float, Sensor, pdf_position, ps, active);
+        NB_OVERRIDE_PURE(pdf_position, ps, active);
     }
 
     Spectrum eval(const SurfaceInteraction3f &si, Mask active) const override {
-        PYBIND11_OVERRIDE_PURE(Spectrum, Sensor, eval, si, active);
+        NB_OVERRIDE_PURE(eval, si, active);
     }
 
     std::pair<Wavelength, Spectrum>
     sample_wavelengths(const SurfaceInteraction3f &si, Float sample,
                        Mask active) const override {
         using Return = std::pair<Wavelength, Spectrum>;
-        PYBIND11_OVERRIDE(Return, Sensor, sample_wavelengths, si, sample, active);
+        NB_OVERRIDE(sample_wavelengths, si, sample, active);
     }
 
     ScalarBoundingBox3f bbox() const override {
-        PYBIND11_OVERRIDE_PURE(ScalarBoundingBox3f, Sensor, bbox,);
+        NB_OVERRIDE_PURE(bbox);
     }
 
     std::string to_string() const override {
-        PYBIND11_OVERRIDE_PURE(std::string, Sensor, to_string,);
+        NB_OVERRIDE_PURE(to_string);
     }
 
     void traverse(TraversalCallback *cb) override {
-        PYBIND11_OVERRIDE(void, Sensor, traverse, cb);
+        NB_OVERRIDE(traverse, cb);
     }
 
     void parameters_changed(const std::vector<std::string> &keys) override {
-        PYBIND11_OVERRIDE(void, Sensor, parameters_changed, keys);
+        NB_OVERRIDE(parameters_changed, keys);
     }
 
     using Sensor::m_needs_sample_2;
@@ -147,9 +151,6 @@ template <typename Ptr, typename Cls> void bind_sensor_generic(Cls &cls) {
             "si"_a, "sample"_a, "active"_a = true,
             D(Endpoint, sample_wavelengths))
     .def("shape", [](Ptr ptr) { return ptr->shape(); }, D(Endpoint, shape));
-
-    if constexpr (dr::is_array_v<Ptr>)
-        bind_drjit_ptr_array(cls);
 }
 
 MI_PY_EXPORT(Sensor) {
@@ -157,27 +158,28 @@ MI_PY_EXPORT(Sensor) {
     using PySensor = PySensor<Float, Spectrum>;
 
     auto sensor = MI_PY_TRAMPOLINE_CLASS(PySensor, Sensor, Endpoint)
-        .def(py::init<const Properties&>())
+        .def(nb::init<const Properties&>())
         .def("sample_ray_differential", &Sensor::sample_ray_differential,
              "time"_a, "sample1"_a, "sample2"_a, "sample3"_a, "active"_a = true,
              D(Sensor, sample_ray_differential))
         .def_method(Sensor, shutter_open)
         .def_method(Sensor, shutter_open_time)
         .def_method(Sensor, needs_aperture_sample)
-        .def("film", py::overload_cast<>(&Sensor::film, py::const_), D(Sensor, film))
-        .def("sampler", py::overload_cast<>(&Sensor::sampler, py::const_), D(Sensor, sampler))
-        .def_readwrite("m_needs_sample_2", &PySensor::m_needs_sample_2)
-        .def_readwrite("m_needs_sample_3", &PySensor::m_needs_sample_3)
-        .def_readwrite("m_film", &PySensor::m_film);
+        .def("film", nb::overload_cast<>(&Sensor::film, nb::const_), D(Sensor, film))
+        .def("sampler", nb::overload_cast<>(&Sensor::sampler, nb::const_), D(Sensor, sampler))
+        .def_rw("m_needs_sample_2", &PySensor::m_needs_sample_2)
+        .def_rw("m_needs_sample_3", &PySensor::m_needs_sample_3)
+        .def_rw("m_film", &PySensor::m_film);
 
     bind_sensor_generic<Sensor *>(sensor);
 
     if constexpr (dr::is_array_v<SensorPtr>) {
-        py::object dr       = py::module_::import("drjit"),
+        nb::object dr       = nb::module_::import_("drjit"),
                    dr_array = dr.attr("ArrayBase");
 
-        py::class_<SensorPtr> cls(m, "SensorPtr", dr_array);
-        bind_sensor_generic<SensorPtr>(cls);
+        dr::ArrayBinding b;
+        auto sensor_ptr = dr::bind_array_t<SensorPtr>(b, m, "SensorPtr");
+        bind_sensor_generic<SensorPtr>(sensor_ptr);
     }
 
     MI_PY_REGISTER_OBJECT("register_sensor", Sensor)
