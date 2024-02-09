884f9b73d2c2cc3fcdf971fce5111ff43553047f
Nicolas Roussel <n.roussel13@gmail.com>
2024-02-09 15:28:36 +0100

    Bindings for Film

--- src/render/python/film_v.cpp
+++ src/render/python/film_v.cpp
@@ -6,62 +6,65 @@
 #include <mitsuba/render/scene.h>
 #include <mitsuba/render/spiral.h>
 #include <mitsuba/python/python.h>
+#include <nanobind/trampoline.h>
+#include <nanobind/stl/string.h>
+#include <drjit/python.h>
 
 /// Trampoline for derived types implemented in Python
 MI_VARIANT class PyFilm : public Film<Float, Spectrum> {
 public:
     MI_IMPORT_TYPES(Film, ImageBlock)
+    NB_TRAMPOLINE(Film, 11);
 
     PyFilm(const Properties &props) : Film(props) { }
 
     size_t base_channels_count() const override {
-        PYBIND11_OVERRIDE_PURE(size_t, Film, base_channels_count);
+        NB_OVERRIDE_PURE(base_channels_count);
     }
 
     size_t prepare(const std::vector<std::string> &aovs) override {
-        PYBIND11_OVERRIDE_PURE(size_t, Film, prepare, aovs);
+        NB_OVERRIDE_PURE(prepare, aovs);
     }
 
     void put_block(const ImageBlock *block) override {
-        PYBIND11_OVERRIDE_PURE(void, Film, put_block, block);
+        NB_OVERRIDE_PURE(put_block, block);
     }
 
     void clear() override {
-        PYBIND11_OVERRIDE_PURE(void, Film, clear);
+        NB_OVERRIDE_PURE(clear);
     }
 
     TensorXf develop(bool raw = false) const override {
-        PYBIND11_OVERRIDE_PURE(TensorXf, Film, develop, raw);
+        NB_OVERRIDE_PURE(develop, raw);
     }
 
     ref<Bitmap> bitmap(bool raw = false) const override {
-        PYBIND11_OVERRIDE_PURE(ref<Bitmap>, Film, bitmap, raw);
+        NB_OVERRIDE_PURE(bitmap, raw);
     }
 
     void write(const fs::path &path) const override {
-        PYBIND11_OVERRIDE_PURE(void, Film, write, path);
+        NB_OVERRIDE_PURE(write, path);
     }
 
     void schedule_storage() override {
-        PYBIND11_OVERRIDE_PURE(void, Film, schedule_storage,);
+        NB_OVERRIDE_PURE(schedule_storage);
     }
 
     void prepare_sample(const UnpolarizedSpectrum &spec,
                         const Wavelength &wavelengths,
                         Float* aovs, Float weight = 1.f,
                         Float alpha = 1.f, Mask active = true) const override {
-        PYBIND11_OVERRIDE_PURE(void, Film, prepare_sample, spec,
-                               wavelengths, aovs, weight, alpha, active);
+        NB_OVERRIDE_PURE(prepare_sample, spec, wavelengths, aovs, weight, alpha, active);
     }
 
     ref<ImageBlock> create_block(const ScalarVector2u &size = 0,
                                  bool normalize = false,
                                  bool border = false) override {
-        PYBIND11_OVERRIDE_PURE(ref<ImageBlock>, Film, create_block, size, normalize, border);
+        NB_OVERRIDE_PURE(create_block, size, normalize, border);
     }
 
     std::string to_string() const override {
-        PYBIND11_OVERRIDE_PURE(std::string, Film, to_string,);
+        NB_OVERRIDE_PURE(to_string);
     }
 
     using Film::m_flags;
@@ -80,8 +83,8 @@ MI_PY_EXPORT(Film) {
     m.def("has_flag", [](uint32_t flags, FilmFlags f) {return has_flag(flags, f);});
     m.def("has_flag", [](UInt32   flags, FilmFlags f) {return has_flag(flags, f);});
 
-    MI_PY_TRAMPOLINE_CLASS(PyFilm, Film, Object)
-        .def(py::init<const Properties &>(), "props"_a)
+    auto film = MI_PY_TRAMPOLINE_CLASS(PyFilm, Film, Object)
+        .def(nb::init<const Properties &>(), "props"_a)
         .def_method(Film, prepare, "aovs"_a)
         .def_method(Film, put_block, "block"_a)
         .def_method(Film, clear)
@@ -114,11 +117,12 @@ MI_PY_EXPORT(Film) {
             "spec"_a, "wavelengths"_a, "nChannels"_a,
             "weight"_a = 1.f, "alpha"_a = 1.f, "active"_a = true,
             D(Film, prepare_sample))
-        .def_method(Film, create_block, "size"_a = ScalarVector2u(0, 0),
-                    "normalize"_a = false, "borders"_a = false)
+        //FIXME
+        //.def_method(Film, create_block, "size"_a = ScalarVector2u(0, 0),
+        //            "normalize"_a = false, "borders"_a = false)
         .def_method(Film, schedule_storage)
         .def_method(Film, sensor_response_function)
         .def_method(Film, flags);
 
-    MI_PY_REGISTER_OBJECT("register_film", Film)
+    //MI_PY_REGISTER_OBJECT("register_film", Film)
 }
