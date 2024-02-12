f70ef15004ab3540c60590680e2dbbf1e9170887
Nicolas Roussel <n.roussel13@gmail.com>
2024-02-12 10:04:24 +0100

    Cornell box stuff (temporary)

--- src/python/python/util.py
+++ src/python/python/util.py
@@ -606,21 +606,24 @@ def cornell_box():
             'type': 'diffuse',
             'reflectance': {
                 'type': 'rgb',
-                'value': [0.885809, 0.698859, 0.666422],
+                #'value': mi.ScalarColor3f([0.885809, 0.698859, 0.666422]),
+                'value': mi.ScalarColor3d(0.885809, 0.698859, 0.666422),
             }
         },
         'green': {
             'type': 'diffuse',
             'reflectance': {
                 'type': 'rgb',
-                'value': [0.105421, 0.37798, 0.076425],
+                #'value': [0.105421, 0.37798, 0.076425],
+                'value': mi.ScalarColor3d(0.105421, 0.37798, 0.076425),
             }
         },
         'red': {
             'type': 'diffuse',
             'reflectance': {
                 'type': 'rgb',
-                'value': [0.570068, 0.0430135, 0.0443706],
+                #'value': [0.570068, 0.0430135, 0.0443706],
+                'value': mi.ScalarColor3d(0.570068, 0.0430135, 0.0443706),
             }
         },
         # -------------------- Light --------------------
@@ -635,7 +638,8 @@ def cornell_box():
                 'type': 'area',
                 'radiance': {
                     'type': 'rgb',
-                    'value': [18.387, 13.9873, 6.75357],
+                    #'value': [18.387, 13.9873, 6.75357],
+                    'value': mi.ScalarColor3d(18.387, 13.9873, 6.75357),
                 }
             }
         },
