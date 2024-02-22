4f33901c1874c470a92dedec90f87704a9072285
Rami Tabbara <rtabbara@gmail.com>
2024-02-22 14:53:29 +0100

    Add formatter bindings

--- src/core/python/formatter.cpp
+++ src/core/python/formatter.cpp
@@ -5,29 +5,24 @@
 // Trampoline for derived types implemented in Python
 class PyFormatter : public Formatter {
 public:
-    using Formatter::Formatter;
+    NB_TRAMPOLINE(Formatter, 1);
 
-    virtual std::string format(mitsuba::LogLevel level, const Class *class_,
+    std::string format(mitsuba::LogLevel level, const Class *class_,
             const Thread *thread, const char *file, int line,
             const std::string &msg) override {
-        PYBIND11_OVERRIDE_PURE(
-            std::string,   // Return value
-            Formatter,     // Parent class
-            format,        // Function
-            level, class_, thread, file, line, msg  // Arguments
-        );
+        NB_OVERRIDE_PURE(format, level, class_, thread, file, line, msg);
     }
 };
 
 MI_PY_EXPORT(Formatter) {
     MI_PY_TRAMPOLINE_CLASS(PyFormatter, Formatter, Object)
-        .def(py::init<>())
+        .def(nb::init<>())
         .def_method(Formatter, format, "level"_a, "class_"_a,
             "thread"_a, "file"_a, "line"_a,
             "msg"_a);
 
     MI_PY_CLASS(DefaultFormatter, Formatter)
-        .def(py::init<>())
+        .def(nb::init<>())
         .def_method(DefaultFormatter, has_date)
         .def_method(DefaultFormatter, set_has_date)
         .def_method(DefaultFormatter, has_thread)
--- src/python/main.cpp
+++ src/python/main.cpp
@@ -15,7 +15,7 @@ MI_PY_DECLARE(Cast);
 MI_PY_DECLARE(Appender);
 MI_PY_DECLARE(ArgParser);
 MI_PY_DECLARE(Bitmap);
-//MI_PY_DECLARE(Formatter);
+MI_PY_DECLARE(Formatter);
 //MI_PY_DECLARE(FileResolver);
 //MI_PY_DECLARE(Logger);
 //MI_PY_DECLARE(MemoryMappedFile);
@@ -135,7 +135,7 @@ NB_MODULE(mitsuba_ext, m) {
     MI_PY_IMPORT(rfilter);
 //    MI_PY_IMPORT(Stream);
     MI_PY_IMPORT(Bitmap);
-//    MI_PY_IMPORT(Formatter);
+    MI_PY_IMPORT(Formatter);
 //    MI_PY_IMPORT(FileResolver);
 //    MI_PY_IMPORT(Logger);
 //    MI_PY_IMPORT(MemoryMappedFile);
