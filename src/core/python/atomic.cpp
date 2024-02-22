b077927868da3499af384a3ac8aea5f5d4044c99
Rami Tabbara <rtabbara@gmail.com>
2024-02-22 14:12:24 +0100

    Add atomic bindings

--- src/core/python/atomic.cpp
+++ src/core/python/atomic.cpp
@@ -4,12 +4,12 @@
 MI_PY_EXPORT(atomic) {
     using AtomicFloat = mitsuba::AtomicFloat<>;
 
-    py::class_<AtomicFloat>(m, "AtomicFloat", D(AtomicFloat))
-        .def(py::init<float>(), D(AtomicFloat, AtomicFloat))
-        .def(py::self += float(), D(AtomicFloat, operator, iadd))
-        .def(py::self -= float(), D(AtomicFloat, operator, isub))
-        .def(py::self *= float(), D(AtomicFloat, operator, imul))
-        .def(py::self /= float(), D(AtomicFloat, operator, idiv))
+    nb::class_<AtomicFloat>(m, "AtomicFloat", D(AtomicFloat))
+        .def(nb::init<float>(), D(AtomicFloat, AtomicFloat))
+        .def(nb::self += float(), D(AtomicFloat, operator, iadd))
+        .def(nb::self -= float(), D(AtomicFloat, operator, isub))
+        .def(nb::self *= float(), D(AtomicFloat, operator, imul))
+        .def(nb::self /= float(), D(AtomicFloat, operator, idiv))
         .def("__float__", [](const AtomicFloat &af) { return (float) af; },
             D(AtomicFloat, operator, T0));
 }
