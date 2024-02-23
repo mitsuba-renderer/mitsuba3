b70a422738952d09a4225a5a634f5f7f0e927eea
Nicolas Roussel <n.roussel13@gmail.com>
2024-02-23 11:42:48 +0100

    Fix bindings for `Bitmap`

--- src/core/python/bitmap.cpp
+++ src/core/python/bitmap.cpp
@@ -1,13 +1,97 @@
-// #include <ostream>
-
 #include <mitsuba/core/bitmap.h>
 #include <mitsuba/core/filesystem.h>
 #include <mitsuba/core/stream.h>
 #include <mitsuba/core/mstream.h>
 #include <mitsuba/python/python.h>
+#include <string>
+#include <nanobind/ndarray.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
 #include <nanobind/stl/pair.h>
+#include <drjit/python.h>
+
+struct fp16 {
+    unsigned char d[2];
+};
+static_assert(sizeof(fp16) == 2);
+
+namespace nanobind {
+    template <> struct ndarray_traits<fp16> {
+        static constexpr bool is_complex = false;
+        static constexpr bool is_float   = true;
+        static constexpr bool is_bool    = false;
+        static constexpr bool is_int     = false;
+        static constexpr bool is_signed  = true;
+    };
+}
+
+void from_cpu_dlpack(Bitmap *b, nb::ndarray<nb::ndim<3>, nb::device::cpu> data,
+                     nb::object pixel_format_,
+                     const std::vector<std::string> &channel_names) {
+        using Float = typename Bitmap::Float;
+        MI_IMPORT_CORE_TYPES()
+
+        size_t shape[3];
+        for (size_t i = 0; i < 3; ++i)
+            shape[i] = data.shape(i);
+
+        auto dtype = data.dtype();
+        Struct::Type component_format;
+        if (dtype == nb::dtype<fp16>())
+            component_format = Struct::Type::Float16;
+        else if (dtype == nb::dtype<float>())
+            component_format = Struct::Type::Float32;
+        else if (dtype == nb::dtype<double>())
+            component_format = Struct::Type::Float64;
+        else if (dtype == nb::dtype<int8_t>())
+            component_format = Struct::Type::Int8;
+        else if (dtype == nb::dtype<uint8_t>())
+            component_format = Struct::Type::UInt8;
+        else if (dtype == nb::dtype<int16_t>())
+            component_format = Struct::Type::Int16;
+        else if (dtype == nb::dtype<uint16_t>())
+            component_format = Struct::Type::UInt16;
+        else if (dtype == nb::dtype<int32_t>())
+            component_format = Struct::Type::Int32;
+        else if (dtype == nb::dtype<uint32_t>())
+            component_format = Struct::Type::UInt32;
+        else if (dtype == nb::dtype<int64_t>())
+            component_format = Struct::Type::Int64;
+        else if (dtype == nb::dtype<uint64_t>())
+            component_format = Struct::Type::UInt64;
+        else
+            throw nb::type_error("Invalid component format");
+
+        Bitmap::PixelFormat pixel_format = Bitmap::PixelFormat::Y;
+        size_t channel_count             = 1;
+        channel_count                    = shape[2];
+        switch (channel_count) {
+            case 1:
+                pixel_format = Bitmap::PixelFormat::Y;
+                break;
+            case 2:
+                pixel_format = Bitmap::PixelFormat::YA;
+                break;
+            case 3:
+                pixel_format = Bitmap::PixelFormat::RGB;
+                break;
+            case 4:
+                pixel_format = Bitmap::PixelFormat::RGBA;
+                break;
+            default:
+                pixel_format = Bitmap::PixelFormat::MultiChannel;
+                break;
+        }
+
+        if (!pixel_format_.is_none())
+            pixel_format = nb::cast<Bitmap::PixelFormat>(pixel_format_);
+
+        ScalarVector2u size(shape[1], shape[0]);
+        new (b) Bitmap(pixel_format, component_format, size, channel_count,
+                       channel_names);
+
+        memcpy(b->data(), data.data(), b->buffer_size());
+}
 
 MI_PY_EXPORT(Bitmap) {
     using Float = typename Bitmap::Float;
@@ -89,27 +173,27 @@ MI_PY_EXPORT(Bitmap) {
             "clamp"_a = std::make_pair(-dr::Infinity<ScalarFloat>, dr::Infinity<ScalarFloat>),
             D(Bitmap, resample, 2)
         )
-        //.def("convert",
-        //    [](const Bitmap& b,
-        //       nb::object pf, nb::object cf, nb::object srgb,
-        //       Bitmap::AlphaTransform alpha_transform) {
-        //           Bitmap::PixelFormat pixel_format = b.pixel_format();
-        //           if (!pf.is(nb::none()))
-        //               pixel_format = pf.cast<Bitmap::PixelFormat>();
-        //           Struct::Type component_format = b.component_format();
-        //           if (!cf.is(nb::none()))
-        //               component_format = cf.cast<Struct::Type>();
-        //           bool srgb_gamma = b.srgb_gamma();
-        //           if (!srgb.is(nb::none()))
-        //               srgb_gamma = srgb.cast<bool>();
+        .def("convert",
+            [](const Bitmap& b,
+               nb::object pf, nb::object cf, nb::object srgb,
+               Bitmap::AlphaTransform alpha_transform) {
+                   Bitmap::PixelFormat pixel_format = b.pixel_format();
+                   if (!pf.is(nb::none()))
+                       pixel_format = nb::cast<Bitmap::PixelFormat>(pf);
+                   Struct::Type component_format = b.component_format();
+                   if (!cf.is(nb::none()))
+                       component_format = nb::cast<Struct::Type>(cf);
+                   bool srgb_gamma = b.srgb_gamma();
+                   if (!srgb.is(nb::none()))
+                       srgb_gamma = nb::cast<bool>(srgb);
 
-        //           nb::gil_scoped_release release;
-        //           return b.convert(pixel_format, component_format, srgb_gamma,
-        //                            alpha_transform);
-        //    },
-        //     D(Bitmap, convert),
-        //     "pixel_format"_a = nb::none(), "component_format"_a = nb::none(),
-        //     "srgb_gamma"_a = nb::none(), "alpha_transform"_a = Bitmap::AlphaTransform::Empty)
+                   nb::gil_scoped_release release;
+                   return b.convert(pixel_format, component_format, srgb_gamma,
+                                    alpha_transform);
+            },
+             D(Bitmap, convert),
+             "pixel_format"_a = nb::none(), "component_format"_a = nb::none(),
+             "srgb_gamma"_a = nb::none(), "alpha_transform"_a = Bitmap::AlphaTransform::Empty)
         .def("convert", nb::overload_cast<Bitmap *>(&Bitmap::convert, nb::const_),
              D(Bitmap, convert, 2), "target"_a,
              nb::call_guard<nb::gil_scoped_release>())
@@ -132,19 +216,19 @@ MI_PY_EXPORT(Bitmap) {
         .def(nb::self == nb::self)
         .def(nb::self != nb::self);
 
-    //auto type_ = m.attr("Struct").attr("Type");
-    //bitmap.attr("UInt8")   = type_.attr("UInt8");
-    //bitmap.attr("Int8")    = type_.attr("Int8");
-    //bitmap.attr("UInt16")  = type_.attr("UInt16");
-    //bitmap.attr("Int16")   = type_.attr("Int16");
-    //bitmap.attr("UInt32")  = type_.attr("UInt32");
-    //bitmap.attr("Int32")   = type_.attr("Int32");
-    //bitmap.attr("UInt64")  = type_.attr("UInt64");
-    //bitmap.attr("Int64")   = type_.attr("Int64");
-    //bitmap.attr("Float16") = type_.attr("Float16");
-    //bitmap.attr("Float32") = type_.attr("Float32");
-    //bitmap.attr("Float64") = type_.attr("Float64");
-    //bitmap.attr("Invalid") = type_.attr("Invalid");
+    auto type_ = m.attr("Struct").attr("Type");
+    bitmap.attr("UInt8")   = type_.attr("UInt8");
+    bitmap.attr("Int8")    = type_.attr("Int8");
+    bitmap.attr("UInt16")  = type_.attr("UInt16");
+    bitmap.attr("Int16")   = type_.attr("Int16");
+    bitmap.attr("UInt32")  = type_.attr("UInt32");
+    bitmap.attr("Int32")   = type_.attr("Int32");
+    bitmap.attr("UInt64")  = type_.attr("UInt64");
+    bitmap.attr("Int64")   = type_.attr("Int64");
+    bitmap.attr("Float16") = type_.attr("Float16");
+    bitmap.attr("Float32") = type_.attr("Float32");
+    bitmap.attr("Float64") = type_.attr("Float64");
+    bitmap.attr("Invalid") = type_.attr("Invalid");
 
     bitmap.def(nb::init<const fs::path &, Bitmap::FileFormat>(), "path"_a,
             "format"_a = Bitmap::FileFormat::Auto,
@@ -211,140 +295,49 @@ MI_PY_EXPORT(Bitmap) {
         });
 
     /**
-     * Tiny wrapper class around nb::object to force the Overload Resolution
-     * Order of pybind11 to pick any other Bitmap constructors if possible
-     * before the one defined below.
+     * Python only constructor for any CPU array type
      */
-    struct PyObjectWrapper {
-        PyObjectWrapper(nb::object o) : obj(o) {}
-        nb::object obj;
-    };
-    nb::class_<PyObjectWrapper>(m, "PyObjectWrapper").def(nb::init<nb::object>());
-    nb::implicitly_convertible<nb::object, PyObjectWrapper>();
+    bitmap.def(
+        "__init__",
+        [](Bitmap *b, nb::ndarray<nb::ndim<3>, nb::device::cpu> data,
+           nb::object pixel_format_,
+           const std::vector<std::string> &channel_names) {
+            from_cpu_dlpack(b, data, pixel_format_, channel_names);
+        },
+        "array"_a, "pixel_format"_a = nb::none(),
+        "channel_names"_a = std::vector<std::string>(),
+        "Initialize a Bitmap from any array that implements the buffer or "
+        "DLPack protocol.");
 
     /**
-     * Python only constructor for any array type that implements the array
-     * interface protocol.
+     * Python only constructor for Dr.Jit tensor types
      */
-    //bitmap.def(nb::init([](PyObjectWrapper obj_wrapper, nb::object pixel_format_,
-    //                       const std::vector<std::string> &channel_names) {
-    //        nb::object obj = obj_wrapper.obj;
-    //        if (!nb::hasattr(obj, "__array_interface__"))
-    //            throw nb::type_error("Array should define __array_interface__!");
+    bitmap.def(
+        "__init__",
+        [](Bitmap *b, nb::handle_t<dr::ArrayBase> h, nb::object pixel_format_,
+           const std::vector<std::string> &channel_names) {
+            nb::handle type = h.type();
+            dr::ArrayMeta m = nb::type_supplement<dr::ArraySupplement>(type);
 
-    //        nb::dict interface = obj.attr("__array_interface__");
-    //        auto typestr = interface["typestr"].template cast<std::string>();
-    //        auto shape   = interface["shape"].template cast<nb::tuple>();
-    //        auto data    = interface["data"].template cast<nb::tuple>();
-    //        void* ptr    = (void*) data[0].cast<size_t>();
+            if (!m.is_tensor)
+                throw nb::type_error(
+                    "Should only be used with Dr.Jit Tensor types!");
 
-    //        Struct::Type component_format;
-    //        if (typestr == "<f2")
-    //            component_format = Struct::Type::Float16;
-    //        else if (typestr == "<f4")
-    //            component_format = Struct::Type::Float32;
-    //        else if (typestr == "<f8")
-    //            component_format = Struct::Type::Float64;
-    //        else if (typestr.length() == 3 && typestr.substr(1) == "i1")
-    //            component_format = Struct::Type::Int8;
-    //        else if (typestr.length() == 3 && typestr.substr(1) == "u1")
-    //            component_format = Struct::Type::UInt8;
-    //        else if (typestr == "<i2")
-    //            component_format = Struct::Type::Int16;
-    //        else if (typestr == "<u2")
-    //            component_format = Struct::Type::UInt16;
-    //        else if (typestr == "<i4")
-    //            component_format = Struct::Type::Int32;
-    //        else if (typestr == "<u4")
-    //            component_format = Struct::Type::UInt32;
-    //        else if (typestr == "<i8")
-    //            component_format = Struct::Type::Int64;
-    //        else if (typestr == "<u8")
-    //            component_format = Struct::Type::UInt64;
-    //        else
-    //            throw nb::type_error("Invalid component format");
-
-    //        size_t ndim = shape.size();
-    //        if (ndim != 2 && ndim != 3)
-    //            throw nb::type_error("Expected an array of size 2 or 3");
-
-    //        Bitmap::PixelFormat pixel_format = Bitmap::PixelFormat::Y;
-    //        size_t channel_count = 1;
-    //        if (ndim == 3) {
-    //            channel_count = shape[2].cast<size_t>();
-    //            switch (channel_count) {
-    //                case 1:  pixel_format = Bitmap::PixelFormat::Y; break;
-    //                case 2:  pixel_format = Bitmap::PixelFormat::YA; break;
-    //                case 3:  pixel_format = Bitmap::PixelFormat::RGB; break;
-    //                case 4:  pixel_format = Bitmap::PixelFormat::RGBA; break;
-    //                default: pixel_format = Bitmap::PixelFormat::MultiChannel; break;
-    //            }
-    //        }
-
-    //        if (!pixel_format_.is_none())
-    //            pixel_format = pixel_format_.cast<Bitmap::PixelFormat>();
-
-    //        Vector2u size(shape[1].cast<size_t>(), shape[0].cast<size_t>());
-    //        Bitmap* bitmap = new Bitmap(pixel_format, component_format, size,
-    //                                 channel_count, channel_names);
-
-    //        bool is_contiguous = true;
-    //        if (interface.contains("strides") && !interface["strides"].is_none()) {
-    //            auto strides = interface["strides"].template cast<nb::tuple>();
-
-    //            // Stride information might be given although it's contiguous
-    //            size_t bytes_per_value = bitmap->bytes_per_pixel() / bitmap->channel_count();
-    //            int64_t current_stride = bytes_per_value;
-    //            for (size_t i = ndim; i > 0; --i) {
-    //                if (current_stride != strides[i - 1].cast<int64_t>()) {
-    //                    is_contiguous = false;
-    //                    break;
-    //                }
-    //                current_stride *= shape[i - 1].cast<size_t>();
-    //            }
-
-    //            // Need to shuffle memory
-    //            if (!is_contiguous) {
-    //                size_t num_values = size.x() * size.y();
-    //                if (ndim == 3)
-    //                    num_values *= shape[2].cast<size_t>();
-
-    //                std::unique_ptr<size_t[]> shape_cum = std::make_unique<size_t[]>(ndim);
-    //                shape_cum[ndim - 1] = 1;
-    //                for (size_t j = ndim - 1; j > 0; --j) {
-    //                    size_t shape_j = shape[j].cast<size_t>();
-    //                    shape_cum[j - 1] = shape_j * shape_cum[j];
-    //                }
-
-    //                for (size_t i = 0; i < num_values; ++i) {
-    //                    unsigned char* src = (unsigned char*) ptr;
-    //                    for (size_t j = ndim; j > 0; --j) {
-    //                        int64_t stride_j = strides[j - 1].cast<int64_t>();
-    //                        size_t shape_j = shape[j - 1].cast<size_t>();
-    //                        int64_t offset_j = ((i / shape_cum[j - 1]) % shape_j) * stride_j;
-    //                        src += offset_j;
-    //                    }
-    //                    unsigned char *dest = (unsigned char *) bitmap->data() + i * bytes_per_value;
-    //                    memcpy(dest, src, bytes_per_value);
-    //                }
-    //            }
-    //        }
-
-    //        if (is_contiguous)
-    //            memcpy(bitmap->data(), ptr, bitmap->buffer_size());
-
-    //        return bitmap;
-    //    }),
-    //    "array"_a,
-    //    "pixel_format"_a = nb::none(),
-    //    "channel_names"_a = std::vector<std::string>(),
-    //    "Initialize a Bitmap from any array that implements ``__array_interface__``");
+            using ArrayType = nb::ndarray<nb::ndim<3>, nb::device::cpu>;
+            ArrayType data = nb::cast<ArrayType>(h.attr("numpy")());
+            from_cpu_dlpack(b, data, pixel_format_, channel_names);
+        },
+        "array"_a, "pixel_format"_a = nb::none(),
+        "channel_names"_a = std::vector<std::string>(),
+        "Initialize a Bitmap from any array that implements the buffer or "
+        "DLPack protocol.");
 
     bitmap.def("_repr_html_", [](const Bitmap &_bitmap) -> nb::object {
         if (_bitmap.pixel_format() == Bitmap::PixelFormat::MultiChannel)
             return nb::none();
 
-        ref<Bitmap> bitmap = _bitmap.convert(Bitmap::PixelFormat::RGB, Struct::Type::UInt16, true);
+        ref<Bitmap> bitmap = _bitmap.convert(Bitmap::PixelFormat::RGB,
+                                             Struct::Type::UInt16, true);
         ref<MemoryStream> s = new MemoryStream(bitmap->buffer_size());
         bitmap->write(s, Bitmap::FileFormat::PNG);
         s->seek(0);
@@ -358,7 +351,7 @@ MI_PY_EXPORT(Bitmap) {
 
         std::stringstream out;
         out << "<img src=\"data:image/png;base64, ";
-        out << s_bytes.str().substr(2, s_bytes.str().size()-3) << "\"";
+        out << s_bytes.str().substr(2, s_bytes.str().size() - 3) << "\"";
         out << "width=\"250vm\"";
         out << " />";
 
