3343211e25f0ab07667cf860b166c8d5b363c495
Nicolas Roussel <n.roussel13@gmail.com>
2024-04-10 10:58:11 +0200

    Fixes to `test_ptracer.py`

--- src/integrators/tests/test_ptracer.py
+++ src/integrators/tests/test_ptracer.py
@@ -10,7 +10,6 @@ def create_test_scene(max_depth=4, emitter='area',
 
     integrator = {
         'type': 'ptracer',
-        'samples_per_pass': 16,
         'rr_depth': 9,
         'max_depth': max_depth,
         'hide_emitters': hide_emitters,
@@ -20,7 +19,7 @@ def create_test_scene(max_depth=4, emitter='area',
         'integrator': integrator,
         'sensor': {
             'type': 'perspective',
-            'to_world': mi.ScalarTransform4f.look_at(
+            'to_world': mi.ScalarTransform4f().look_at(
                 origin=(2, 0, 0),
                 target=(0, 0, 0),
                 up=(0, 1, 0),
@@ -31,7 +30,8 @@ def create_test_scene(max_depth=4, emitter='area',
             'film': {
                 'type': 'hdrfilm',
                 'width': 8, 'height': 8,
-                # 'width': 128, 'height': 128,
+                #'width': 1, 'height': 1,
+                #'width': 128, 'height': 128,
                 'rfilter': {'type': 'box'}
             },
         }
@@ -46,7 +46,7 @@ def create_test_scene(max_depth=4, emitter='area',
     if emitter in ('area', 'texturedarea'):
         emitter = {
             'type': 'rectangle',
-            'to_world': mi.ScalarTransform4f.look_at(
+            'to_world': mi.ScalarTransform4f().look_at(
                 origin=(0, 0, 0),
                 target=(1, 0, 0),
                 up=(0, 1, 0),
@@ -65,7 +65,7 @@ def create_test_scene(max_depth=4, emitter='area',
     elif emitter == 'directionalarea':
         emitter = {
             'type': 'rectangle',
-            'to_world': mi.ScalarTransform4f.look_at(
+            'to_world': mi.ScalarTransform4f().look_at(
                 origin=(4, 1, 0),
                 target=(0, 0, 0),
                 up=(0, 1, 0),
@@ -82,7 +82,7 @@ def create_test_scene(max_depth=4, emitter='area',
     elif emitter == 'directional':
         emitter = {
             'type': 'directional',
-            'to_world': mi.ScalarTransform4f.rotate(axis=(0, 1, 0), angle=240)
+            'to_world': mi.ScalarTransform4f().rotate(axis=(0, 1, 0), angle=240)
         }
         # Need a receiver plane to bounce light from that emitter
         shape = 'receiver'
@@ -97,7 +97,7 @@ def create_test_scene(max_depth=4, emitter='area',
     if shape == 'receiver':
         scene['receiver'] = {
             'type': 'rectangle',
-            'to_world': mi.ScalarTransform4f.look_at(
+            'to_world': mi.ScalarTransform4f().look_at(
                 origin=(0, 0, 0),
                 target=(1, 0, 0),
                 up=(0, 1, 0),
@@ -140,6 +140,7 @@ def test03_render_hide_emitters(variants_all, emitter):
     image = integrator.render(scene, seed=0, spp=4, develop=True)
     assert dr.all(dr.ravel(image) == 0)
 
+
 @pytest.mark.slow
 @pytest.mark.parametrize('develop', [False, True])
 def test04_render_no_emitters(variants_all, develop):
@@ -152,7 +153,7 @@ def test04_render_no_emitters(variants_all, develop):
     if not develop:
         assert len(dr.shape(image)) == 0
         image = scene.sensors()[0].film().develop()
-    assert dr.all((image == 0) | dr.isnan(image))
+    assert dr.all((image == 0) | dr.isnan(image), axis=None)
 
 
 def test05_render_crop_film(variants_all):
@@ -213,13 +214,12 @@ def test06_ptracer_gradients(variants_all_ad_rgb):
         image = integrator.render(scene, seed=0, spp=64)
 
     dr.enable_grad(image)
-    loss = dr.mean(image)
+    loss = dr.mean(image, axis=None)
     dr.backward(loss)
     adjoint = dr.grad(image)
     dr.set_grad(image, 0.0)
 
-    integrator.render_backward(scene, grad_in=adjoint, seed=3,
-                               spp=64, params=params)
+    integrator.render_backward(scene, grad_in=adjoint, seed=3, spp=64, params=params)
     g = dr.grad(params[key])
     assert dr.shape(g) == dr.shape(params[key])
     assert dr.allclose(g, 0.33647, atol=1e-5) # TODO improve this test (don't use hardcoded value)
