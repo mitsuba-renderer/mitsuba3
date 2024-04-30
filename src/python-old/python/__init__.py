57ba6531c9a4cb594c8cb1e5cac24b22e46c5194
Nicolas Roussel <n.roussel13@gmail.com>
2024-04-30 10:45:05 +0200

    Delete `python-old`

--- src/python-old/python/__init__.py
+++
@@ -1,4 +0,0 @@
-from .util import traverse, SceneParameters, render, cornell_box, variant_context
-from . import chi2
-from . import xml
-from . import ad
--- src/python-old/python/ad/__init__.py
+++
@@ -1,5 +0,0 @@
-from .largesteps import LargeSteps
-from .integrators import *
-from .optimizers import *
-from .guiding import *
-from .projective import *
