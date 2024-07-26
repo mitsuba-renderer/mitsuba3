a624f9aecb0b50c5f5f1150d9a65a6b710e5e80a
Nicolas Roussel <n.roussel13@gmail.com>
2024-07-26 16:55:52 +0200

    Remove "python-old" folders

--- src/core/python-old/vector_v.cpp
+++
@@ -1,9 +0,0 @@
-#include <mitsuba/core/vector.h>
-#include <mitsuba/core/fwd.h>
-#include <mitsuba/python/python.h>
-
-MI_PY_EXPORT(vector) {
-    MI_PY_IMPORT_TYPES()
-    m.def("coordinate_system", &coordinate_system<Vector<Float, 3>>, "n"_a,
-          D(coordinate_system));
-}
