21a60b4588571e6820a96c710de48f4399c44558
Rami Tabbara <rtabbara@gmail.com>
2024-09-17 14:44:12 +0200

    Fix passing sampler state within symbolic loops

--- src/samplers/ldsampler.cpp
+++ src/samplers/ldsampler.cpp
@@ -152,11 +152,13 @@ public:
     }
 
     void traverse_1_cb_ro(void *payload, void (*fn)(void *, uint64_t)) const override {
-        dr::traverse_1_fn_ro(m_scramble_seed, payload, fn);
+        auto fields = dr::make_tuple(m_scramble_seed, m_dimension_index);
+        dr::traverse_1_fn_ro(fields, payload, fn);
     }
 
     void traverse_1_cb_rw(void *payload, uint64_t (*fn)(void *, uint64_t)) override {
-        dr::traverse_1_fn_rw(m_scramble_seed, payload, fn);
+        auto fields = dr::tie(m_scramble_seed, m_dimension_index);
+        dr::traverse_1_fn_rw(fields, payload, fn);
     }
 
     std::string to_string() const override {
