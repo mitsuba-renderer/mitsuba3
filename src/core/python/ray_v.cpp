36090bf9e7305b3b4991ee18695bd911859ffbaf
Merlin Nimier-David <merlin.nimier@gmail.com>
2024-10-31 17:48:09 +0100

    Fix small compilation warnings (Clang 19)

--- src/core/python/ray_v.cpp
+++ src/core/python/ray_v.cpp
@@ -5,21 +5,26 @@
 template<typename Ray>
 void bind_ray(nb::module_ &m, const char *name) {
     MI_PY_IMPORT_TYPES()
-    using Vector = typename Ray::Vector;
-    using Point  = typename Ray::Point;
+    // Re-import this specific `Ray`'s types in cased of mixed precision
+    // between Float and Spectrum.
+    using RayFloat       = typename Ray::Float;
+    using RayScalarFloat = typename Ray::ScalarFloat;
+    using RayWavelength  = typename Ray::Wavelength;
+    using Vector         = typename Ray::Vector;
+    using Point          = typename Ray::Point;
 
     MI_PY_CHECK_ALIAS(Ray, name) {
         auto ray = nb::class_<Ray>(m, name, D(Ray))
             .def(nb::init<>(), "Create an uninitialized ray")
             .def(nb::init<const Ray &>(), "Copy constructor", "other"_a)
-            .def(nb::init<Point, Vector, Float, const Wavelength &>(),
+            .def(nb::init<Point, Vector, RayFloat, const RayWavelength &>(),
                  D(Ray, Ray, 2),
-                 "o"_a, "d"_a, "time"_a=0.0, "wavelengths"_a=Wavelength())
-            .def(nb::init<Point, Vector, Float, Float, const Wavelength &>(),
+                 "o"_a, "d"_a, "time"_a=(RayScalarFloat) 0.0, "wavelengths"_a=RayWavelength())
+            .def(nb::init<Point, Vector, RayFloat, RayFloat, const RayWavelength &>(),
                  D(Ray, Ray, 3),
-                "o"_a, "d"_a, "maxt"_a, "time"_a, "wavelengths"_a)
-            .def(nb::init<const Ray &, Float>(),
-                D(Ray, Ray, 4), "other"_a, "maxt"_a)
+                 "o"_a, "d"_a, "maxt"_a, "time"_a, "wavelengths"_a)
+            .def(nb::init<const Ray &, RayFloat>(),
+                 D(Ray, Ray, 4), "other"_a, "maxt"_a)
             .def("__call__", &Ray::operator(), D(Ray, operator, call), "t"_a)
             .def_field(Ray, o,           D(Ray, o))
             .def_field(Ray, d,           D(Ray, d))
@@ -45,7 +50,7 @@ MI_PY_EXPORT(Ray) {
             .def(nb::init<const Ray3f &>(), "ray"_a)
             .def(nb::init<Point3f, Vector3f, Float, const Wavelength &>(),
                  "Initialize without differentials.",
-                 "o"_a, "d"_a, "time"_a=0.0, "wavelengths"_a=Wavelength())
+                 "o"_a, "d"_a, "time"_a=(ScalarFloat) 0.0, "wavelengths"_a=Wavelength())
             .def("scale_differential", &RayDifferential3f::scale_differential,
                  "amount"_a, D(RayDifferential, scale_differential))
             .def_field(RayDifferential3f, o_x, D(RayDifferential, o_x))
