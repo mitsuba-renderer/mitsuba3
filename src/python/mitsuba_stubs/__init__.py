45293d6371a2071798a5631c69b527d24cf8352f
Nicolas Roussel <n.roussel13@gmail.com>
2024-11-18 11:38:52 +0100

    Add stub generation for `filesystem` and `detail` subodules

--- src/python/mitsuba_stubs/__init__.py
+++ src/python/mitsuba_stubs/__init__.py
@@ -51,11 +51,13 @@ except:
     mi.set_variant(v)
 
 sys.modules[__name__] = sys.modules['mitsuba']
-sys.modules[__name__ +'.math']      = mi.math
-sys.modules[__name__ +'.spline']    = mi.spline
-sys.modules[__name__ +'.warp']      = mi.warp
-sys.modules[__name__ +'.quad']      = mi.quad
-sys.modules[__name__ +'.mueller']   = mi.mueller
-sys.modules[__name__ +'.misc']      = mi.misc
-sys.modules[__name__ +'.python']    = mi.python
+sys.modules[__name__ +'.math']       = mi.math
+sys.modules[__name__ +'.spline']     = mi.spline
+sys.modules[__name__ +'.warp']       = mi.warp
+sys.modules[__name__ +'.quad']       = mi.quad
+sys.modules[__name__ +'.mueller']    = mi.mueller
+sys.modules[__name__ +'.misc']       = mi.misc
+sys.modules[__name__ +'.python']     = mi.python
+sys.modules[__name__ +'.detail']     = mi.detail
+sys.modules[__name__ +'.filesystem'] = mi.filesystem
 
