947fb17fbc40ff218689b65621104d030f0982f3
Sebastien Speierer <sebastieneps@gmail.com>
2025-01-15 14:35:51 +0100

    Add Shape::eval_attribute_X

--- include/mitsuba/render/shape.h
+++ include/mitsuba/render/shape.h
@@ -799,6 +799,21 @@ public:
     virtual Color3f eval_attribute_3(const std::string &name,
                                      const SurfaceInteraction3f &si,
                                      Mask active = true) const;
+/**
+     * \brief Evaluate a dynamic shape attribute at the given surface interaction.
+     *
+     * \param name
+     *     Name of the attribute to evaluate
+     *
+     * \param si
+     *     Surface interaction associated with the query
+     *
+     * \return
+     *     An dynamic array of attribute values
+     */
+    virtual dr::DynamicArray<Float> eval_attribute_X(const std::string &name,
+                                     const SurfaceInteraction3f &si,
+                                     Mask active = true) const;
 
     /**
      * \brief Parameterize the mesh using UV values
@@ -1142,6 +1157,7 @@ MI_CALL_TEMPLATE_BEGIN(Shape)
     DRJIT_CALL_METHOD(eval_attribute)
     DRJIT_CALL_METHOD(eval_attribute_1)
     DRJIT_CALL_METHOD(eval_attribute_3)
+    DRJIT_CALL_METHOD(eval_attribute_X)
     DRJIT_CALL_METHOD(eval_parameterization)
     DRJIT_CALL_METHOD(ray_intersect_preliminary)
     DRJIT_CALL_METHOD(ray_intersect)
