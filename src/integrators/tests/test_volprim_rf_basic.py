1fd265a6d61ac3b2d4b1e0851d3a68c3e0fd64b8
Sebastien Speierer <sebastieneps@gmail.com>
2025-01-22 09:22:39 +0100

    Add regression tests

--- src/integrators/tests/test_volprim_rf_basic.py
+++ src/integrators/tests/test_volprim_rf_basic.py
@@ -1,5 +1,10 @@
+import pytest, os
+
 import drjit as dr
 import mitsuba as mi
+
+from mitsuba.scalar_rgb import Transform4f as T
+from mitsuba.testing import RenderingRegressionTest
 
 class EllipsoidsFactory:
     '''
@@ -33,63 +38,179 @@ class EllipsoidsFactory:
         albedos = mi.TensorXf(np.array(self.albedos))
         return centers, scales, quats, sigmats, albedos
 
-def test01_ellipsoids_depth(variants_vec_backends_once_rgb):
+@pytest.mark.parametrize("shape_type", ['ellipsoids', 'ellipsoidsmesh'])
+def test01_render_span(variants_all_ad_rgb, regression_test_options, shape_type):
     factory = EllipsoidsFactory()
-
     N = 12
     for i in range(N):
-        s = 0.15
         r = 2.5
         t = i / float(N)
+        s = 0.05 + t * 0.2
         factory.add(
             mean=[r * dr.sin(t * dr.two_pi), r * dr.cos(t * dr.two_pi), 0.01 * t],
-            scale=[s, 4*s, s],
-            sigmat=1.0 * t,
+            scale=[s, 3*s, s],
             euler=[0.0, 0.0, -360 * t],
             albedo=[dr.abs(dr.sin(t * dr.pi)), 1.0 - t, t]
         )
-
     centers, scales, quaternions, sigmats, albedos = factory.build()
 
-    scene = mi.load_dict({
-        'type': 'scene',
-        'integrator': {
-            'type': 'volprim_rf_basic',
-            'max_depth': 16,
-        },
-
-        'sensor': {
-            'type': 'perspective',
-            'fov': 80,
-            'near_clip': 0.01,
-            'far_clip': 1000.0,
-            'to_world': mi.ScalarTransform4f().look_at(
-                origin=[0, 0, -5],
-                target=[0, 0, 0],
-                up=[0, 1, 0]
-            ),
-            'film': {
-                'type': 'hdrfilm',
-                'width': 512,
-                'height': 512,
-                'filter': { 'type': 'box' },
-                'pixel_format': 'rgb'
+    test = RenderingRegressionTest(
+        name=f'volprim_rf_basic_span[{shape_type}]',
+        scene=mi.load_dict({
+            'type': 'scene',
+            'integrator': {
+                'type': 'moment',
+                'nested': {
+                    'type': 'volprim_rf_basic',
+                    'max_depth': 8,
+                }
+            },
+            'sensor': {
+                'type': 'perspective',
+                'to_world': T().look_at(
+                    origin=[0, 0, -5],
+                    target=[0, 0, 0],
+                    up=[0, 1, 0]),
+                'fov': 61.9275,
+                'myfilm': {
+                    'type': 'hdrfilm',
+                    'rfilter': { 'type': 'tent' },
+                    'width': 128, 'height': 128,
+                    'pixel_format': 'rgb',
+                },
+            },
+            'shape': {
+                'type': shape_type,
+                'centers': centers,
+                'scales': scales,
+                'quaternions': quaternions,
+                'opacities': sigmats,
+                'sh_coeffs': albedos,
             }
-        },
+        }),
+        test_spp=2048,
+        pixel_success_rate=0.99,
+        references_folder=os.path.join(os.path.dirname(__file__), '../../../resources/data/tests/scenes/volprim_rf_basic/refs'),
+        **regression_test_options
+    )
 
-        'primitives': {
-            'type': 'ellipsoids',
-            # 'type': 'ellipsoidsmesh',
-            # 'shell': 'uv_sphere',
-            # 'shell': 'ico_sphere',
-            'centers': centers,
-            'scales': scales,
-            'quaternions': quaternions,
-            'opacities': sigmats,
-            'sh_coeffs': albedos,
-            'extent': 3.0,
-        }
-    })
+    assert test.run()
 
-    img = mi.render(scene, spp=64)
-    mi.Bitmap(img).write('primitives.exr')
+@pytest.mark.parametrize("srgb_primitives", [True, False])
+def test02_render_stack(variants_all_ad_rgb, regression_test_options, srgb_primitives):
+    factory = EllipsoidsFactory()
+    N = 12
+    for i in range(N):
+        r = 1.0
+        t = i / float(N)
+        s = 0.15 + t * 0.15
+        factory.add(
+            mean=[r * dr.sin(t * dr.two_pi), r * dr.cos(t * dr.two_pi), 0.2 * t],
+            scale=[s, 8*s, s],
+            sigmat=0.5,
+            euler=[0.0, 0.0, -360 * t + 45],
+            albedo=[dr.abs(dr.sin(t * dr.pi)), 1.0 - t, t]
+        )
+    centers, scales, quaternions, sigmats, albedos = factory.build()
+
+    test = RenderingRegressionTest(
+        name=f'volprim_rf_basic_stack[srgb_primitives={srgb_primitives}]',
+        scene=mi.load_dict({
+            'type': 'scene',
+            'integrator': {
+                'type': 'moment',
+                'nested': {
+                    'type': 'volprim_rf_basic',
+                    'max_depth': 32,
+                    'srgb_primitives': srgb_primitives
+                }
+            },
+            'sensor': {
+                'type': 'perspective',
+                'to_world': T().look_at(
+                    origin=[0, 0, -5],
+                    target=[0, 0, 0],
+                    up=[0, 1, 0]),
+                'fov': 61.9275,
+                'myfilm': {
+                    'type': 'hdrfilm',
+                    'rfilter': { 'type': 'tent' },
+                    'width': 128, 'height': 128,
+                    'pixel_format': 'rgb',
+                },
+            },
+            'shape': {
+                'type': 'ellipsoidsmesh',
+                'shell': 'uv_sphere',
+                'centers': centers,
+                'scales': scales,
+                'quaternions': quaternions,
+                'opacities': sigmats,
+                'sh_coeffs': albedos,
+            },
+        }),
+        test_spp=2048,
+        pixel_success_rate=0.99,
+        references_folder=os.path.join(os.path.dirname(__file__), '../../../resources/data/tests/scenes/volprim_rf_basic/refs'),
+        **regression_test_options
+    )
+
+    assert test.run()
+
+@pytest.mark.parametrize("shape_type", ['ellipsoids', 'ellipsoidsmesh'])
+def test03_render_depth(variants_all_ad_rgb, regression_test_options, shape_type):
+    factory = EllipsoidsFactory()
+    N = 12
+    for i in range(N):
+        r = 2.5
+        t = i / float(N)
+        s = 0.05 + t * 0.2
+        factory.add(
+            mean=[r * dr.sin(t * dr.two_pi), r * dr.cos(t * dr.two_pi), 0.01 * t],
+            scale=[s, 3*s, s],
+            euler=[0.0, 0.0, -360 * t],
+            albedo=[dr.abs(dr.sin(t * dr.pi)), 1.0 - t, t]
+        )
+    centers, scales, quaternions, sigmats, albedos = factory.build()
+
+    test = RenderingRegressionTest(
+        name=f'volprim_rf_basic_depth[{shape_type}]',
+        scene=mi.load_dict({
+            'type': 'scene',
+            'integrator': {
+                'type': 'moment',
+                'nested': {
+                    'type': 'depth',
+                }
+            },
+            'sensor': {
+                'type': 'perspective',
+                'to_world': T().look_at(
+                    origin=[0, 0, -5],
+                    target=[0, 0, 0],
+                    up=[0, 1, 0]),
+                'fov': 61.9275,
+                'myfilm': {
+                    'type': 'hdrfilm',
+                    'rfilter': { 'type': 'box' },
+                    'width': 128, 'height': 128,
+                    'pixel_format': 'rgb',
+                },
+            },
+            'shape': {
+                'type': shape_type,
+                'centers': centers,
+                'scales': scales,
+                'quaternions': quaternions,
+                'opacities': sigmats,
+                'sh_coeffs': albedos,
+            }
+        }),
+        test_spp=16,
+        ref_spp=16,
+        pixel_success_rate=0.95,
+        references_folder=os.path.join(os.path.dirname(__file__), '../../../resources/data/tests/scenes/volprim_rf_basic/refs'),
+        **regression_test_options
+    )
+
+    assert test.run()
