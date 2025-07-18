50b5a85be183247c47ba4df3773804d649125897
Ziyi - Station <ziyi.zhang@epfl.ch>
2025-07-18 19:05:50 +0200

    Add sample_to_camera & update jacobian code

--- src/render/python/sensor_v.cpp
+++ src/render/python/sensor_v.cpp
@@ -194,8 +194,8 @@ MI_PY_EXPORT(Sensor) {
         .def_method(ProjectiveCamera, near_clip)
         .def_method(ProjectiveCamera, far_clip)
         .def_method(ProjectiveCamera, focus_distance)
-        .def("projection_transform", &ProjectiveCamera::projection_transform,
-             "Return the projection matrix of this sensor (camera space to sample space transformation)");
+        .def("projection_transform", &ProjectiveCamera::projection_transform)
+        .def("sample_to_camera", &ProjectiveCamera::sample_to_camera);
 
     m.def("perspective_projection", &perspective_projection<Float>,
           "film_size"_a, "crop_size"_a, "crop_offset"_a, "fov_x"_a, "near_clip"_a, "far_clip"_a,
