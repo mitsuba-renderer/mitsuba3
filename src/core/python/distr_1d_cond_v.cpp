36991a9b33264c4d3dfac67ef31d6b493b8b04d9
Miguel Crespo <mcrescas@gmail.com>
2025-09-16 10:46:54 +0200

    Expose bindings for both Float and UnpolarizedSpectrum of the Conditional Distributions

--- src/core/python/distr_1d_cond_v.cpp
+++ src/core/python/distr_1d_cond_v.cpp
@@ -8,15 +8,14 @@
 #include <nanobind/stl/tuple.h>
 #include <nanobind/stl/vector.h>
 
-MI_PY_EXPORT(ConditionalIrregular1D) {
+template <typename Type>
+void bind_conditional_irregular_1d(nb::module_ &m, const char *name) {
     MI_PY_IMPORT_TYPES()
-
-    using TYPE                   = UnpolarizedSpectrum;
-    using ConditionalIrregular1D = mitsuba::ConditionalIrregular1D<TYPE>;
     using FloatStorage           = DynamicBuffer<Float>;
+    using ConditionalIrregular1D = mitsuba::ConditionalIrregular1D<Type>;
 
-    MI_PY_CHECK_ALIAS(ConditionalIrregular1D, "ConditionalIrregular1D") {
-        nb::class_<ConditionalIrregular1D>(m, "ConditionalIrregular1D")
+    MI_PY_CHECK_ALIAS(ConditionalIrregular1D, name) {
+        nb::class_<ConditionalIrregular1D>(m, name)
             .def(nb::init<>(), D(ConditionalIrregular1D))
             .def(nb::init<const FloatStorage &, const FloatStorage &,
                           const std::vector<FloatStorage> &, bool, bool>(),
@@ -24,29 +23,29 @@ MI_PY_EXPORT(ConditionalIrregular1D) {
                  "nearest"_a = false, D(ConditionalIrregular1D, ConditionalIrregular1D, 2))
             .def(
                 "eval_pdf",
-                [](const ConditionalIrregular1D *distr, TYPE x,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalIrregular1D *distr, Type x,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->eval_pdf(x, cond, active);
                 },
                 "x"_a, "cond"_a, "active"_a = true, D(ConditionalIrregular1D, eval_pdf))
             .def(
                 "eval_pdf_normalized",
-                [](const ConditionalIrregular1D *distr, TYPE x,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalIrregular1D *distr, Type x,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->eval_pdf_normalized(x, cond, active);
                 },
                 "x"_a, "cond"_a, "active"_a = true, D(ConditionalIrregular1D, eval_pdf_normalized))
             .def(
                 "sample_pdf",
-                [](const ConditionalIrregular1D *distr, TYPE u,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalIrregular1D *distr, Type u,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->sample_pdf(u, cond, active);
                 },
                 "u"_a, "cond"_a, "active"_a = true, D(ConditionalIrregular1D, sample_pdf))
             .def(
                 "integral",
                 [](const ConditionalIrregular1D *distr,
-                   std::vector<TYPE> cond) { return distr->integral(cond); },
+                   std::vector<Type> cond) { return distr->integral(cond); },
                 "cond"_a, D(ConditionalIrregular1D, integral))
             .def("tensor", &ConditionalIrregular1D::tensor,
                  nb::rv_policy::reference_internal, D(ConditionalIrregular1D, tensor))
@@ -60,15 +59,14 @@ MI_PY_EXPORT(ConditionalIrregular1D) {
     }
 }
 
-MI_PY_EXPORT(ConditionalRegular1D) {
+template <typename Type>
+void bind_conditional_regular_1d(nb::module_ &m, const char *name) {
     MI_PY_IMPORT_TYPES()
-
-    using TYPE                 = UnpolarizedSpectrum;
-    using ConditionalRegular1D = mitsuba::ConditionalRegular1D<TYPE>;
+    using ConditionalRegular1D = mitsuba::ConditionalRegular1D<Type>;
     using FloatStorage         = DynamicBuffer<Float>;
 
-    MI_PY_CHECK_ALIAS(ConditionalRegular1D, "ConditionalRegular1D") {
-        nb::class_<ConditionalRegular1D>(m, "ConditionalRegular1D")
+    MI_PY_CHECK_ALIAS(ConditionalRegular1D, name) {
+        nb::class_<ConditionalRegular1D>(m, name)
             .def(nb::init<>(), D(ConditionalRegular1D))
             .def(nb::init<const FloatStorage &, const ScalarVector2f &,
                           const std::vector<ScalarVector2f> &,
@@ -77,28 +75,28 @@ MI_PY_EXPORT(ConditionalRegular1D) {
                  "enable_sampling"_a = true, "nearest"_a = false, D(ConditionalRegular1D, ConditionalRegular1D, 2))
             .def(
                 "eval_pdf",
-                [](const ConditionalRegular1D *distr, TYPE x,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalRegular1D *distr, Type x,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->eval_pdf(x, cond, active);
                 },
                 "x"_a, "cond"_a, "active"_a = true, D(ConditionalRegular1D, eval_pdf))
             .def(
                 "eval_pdf_normalized",
-                [](const ConditionalRegular1D *distr, TYPE x,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalRegular1D *distr, Type x,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->eval_pdf_normalized(x, cond, active);
                 },
                 "x"_a, "cond"_a, "active"_a = true, D(ConditionalRegular1D, eval_pdf_normalized))
             .def(
                 "sample_pdf",
-                [](const ConditionalRegular1D *distr, TYPE u,
-                   std::vector<TYPE> cond, Mask active = true) {
+                [](const ConditionalRegular1D *distr, Type u,
+                   std::vector<Type> cond, Mask active = true) {
                     return distr->sample_pdf(u, cond, active);
                 },
                 "u"_a, "cond"_a, "active"_a = true, D(ConditionalRegular1D, sample_pdf))
             .def(
                 "integral",
-                [](const ConditionalRegular1D *distr, std::vector<TYPE> cond) {
+                [](const ConditionalRegular1D *distr, std::vector<Type> cond) {
                     return distr->integral(cond);
                 },
                 "cond"_a, D(ConditionalRegular1D, integral))
@@ -111,4 +109,18 @@ MI_PY_EXPORT(ConditionalRegular1D) {
             .def_field(ConditionalRegular1D, m_cdf, D(ConditionalRegular1D, m_cdf))
             .def_repr(ConditionalRegular1D);
     }
+}
+
+MI_PY_EXPORT(ConditionalIrregular1D) {
+    MI_PY_IMPORT_TYPES()
+
+    bind_conditional_irregular_1d<Float>(m, "ConditionalIrregular1D");
+    bind_conditional_irregular_1d<UnpolarizedSpectrum>(m, "ConditionalIrregular1DSpectrum");
+}
+
+MI_PY_EXPORT(ConditionalRegular1D) {
+    MI_PY_IMPORT_TYPES()
+
+    bind_conditional_regular_1d<Float>(m, "ConditionalRegular1D");
+    bind_conditional_regular_1d<UnpolarizedSpectrum>(m, "ConditionalRegular1DSpectrum");
 }
