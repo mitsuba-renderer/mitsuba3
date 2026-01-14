a3f4dd17e0315c321cb62db79c74feb7b941d4d7
Nicolas Roussel <n.roussel13@gmail.com>
2026-01-14 16:03:45 +0100

    tmp

--- src/textures/tests/test_bitmap.py
+++ src/textures/tests/test_bitmap.py
@@ -31,7 +31,6 @@ def test01_sample_position(variants_vec_backends_once, filter_type, wrap_mode):
 @fresolver_append_path
 def test02_eval_grad(variant_scalar_rgb, np_rng):
     # Tests evaluating the texture gradient under different rotation
-    import numpy as np
     delta = 1e-4
     si = mi.SurfaceInteraction3f()
     for u01 in np_rng.random((10, 1)):
@@ -56,8 +55,6 @@ def test02_eval_grad(variant_scalar_rgb, np_rng):
 @fresolver_append_path
 @pytest.mark.parametrize('wrap_mode', ['repeat', 'clamp', 'mirror'])
 def test03_wrap(variants_vec_backends_once_rgb, wrap_mode):
-    import numpy as np
-
     bitmap = mi.load_dict({
         "type"      : "bitmap",
         "filename"  : "resources/data/common/textures/noise_8x8.png",
@@ -133,8 +130,6 @@ def test03_wrap(variants_vec_backends_once_rgb, wrap_mode):
 
 @fresolver_append_path
 def test04_eval_rgb(variants_vec_backends_once_rgb):
-    import numpy as np
-
     # RGB image
     bitmap = mi.load_dict({
         'type' : 'bitmap',
@@ -164,7 +159,7 @@ def test04_eval_rgb(variants_vec_backends_once_rgb):
         'filename' : 'resources/data/common/textures/noise_02.png'
     })
 
-    x_res, y_res =  bitmap.resolution()
+    x_res, y_res = bitmap.resolution()
     # Coordinates of gray pixel: (0, 15)
     x = (1 / x_res) * 0 + (1 / (2 * x_res))
     y = (1 / y_res) * 15 + (1 / (2 * y_res))
@@ -177,7 +172,7 @@ def test04_eval_rgb(variants_vec_backends_once_rgb):
         color = bitmap.eval_3(si)
     spec = bitmap.eval(si)
 
-    expected = 0.5394
+    expected = 0.5395
     assert dr.allclose(expected, spec, atol=1e-04)
     assert dr.allclose(expected, mono, atol=1e-04)
 
