41a592a0e6293534c20a139a54275cce56fc5609
lnuic <12744626+lnuic@users.noreply.github.com>
2026-05-03 20:15:14 +0200

    MergeShape: use balanced tree reduction when merging mesh groups

--- src/shapes/merge.cpp
+++ src/shapes/merge.cpp
@@ -13,7 +13,9 @@ public:
     MergeShape(const Properties &props) {
         // Note: we are *not* calling the `Shape` constructor as we do not
         // want to accept various properties such as `to_world`.
-        std::unordered_map<Key, ref<Mesh>, key_hasher> tbl;
+        // Group meshes by merge key, then merge each group with a
+        // tree-balanced reduction (O(M log N) per group.
+        std::unordered_map<Key, std::vector<ref<Mesh>>, key_hasher> groups;
         size_t visited = 0, ignored = 0;
         Timer timer;
 
@@ -37,22 +39,35 @@ public:
             key.has_texcoords = mesh->has_vertex_texcoords();
             key.has_face_normals = mesh->has_face_normals();
 
-            auto [it, success] = tbl.try_emplace(key, mesh);
-            if (!success)
-                it->second = it->second->merge(mesh);
-
+            groups[key].push_back(ref<Mesh>(mesh));
             visited++;
         }
 
-        for (auto &kv : tbl) {
-            if (tbl.size() == 1 && !props.id().empty())
-                kv.second->set_id(props.id());
+        for (auto &kv : groups) {
+            ref<Mesh> merged = tree_merge(kv.second);
 
-            m_objects.push_back((ref<Object>) kv.second);
+            if (groups.size() == 1 && !props.id().empty())
+                merged->set_id(props.id());
+
+            m_objects.push_back((ref<Object>) merged);
         }
 
         Log(Info, "Collapsed %zu into %zu meshes. (took %s, %zu objects ignored)",
-            visited, tbl.size(), util::time_string((float) timer.value()), ignored);
+            visited, groups.size(), util::time_string((float) timer.value()),
+            ignored);
+    }
+
+    static ref<Mesh> tree_merge(std::vector<ref<Mesh>> meshes) {
+        while (meshes.size() > 1) {
+            std::vector<ref<Mesh>> next;
+            next.reserve((meshes.size() + 1) / 2);
+            for (size_t i = 0; i + 1 < meshes.size(); i += 2)
+                next.push_back(meshes[i]->merge(meshes[i + 1].get()));
+            if (meshes.size() % 2 == 1)
+                next.push_back(meshes.back());
+            meshes = std::move(next);
+        }
+        return meshes[0];
     }
 
     std::vector<ref<Object>> expand() const override {
