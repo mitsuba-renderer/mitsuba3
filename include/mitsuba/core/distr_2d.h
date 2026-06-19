d5a0e4910ac339818d9ab0dbd0c50e00e34a7ed6
Wenzel Jakob <wenzel.jakob@epfl.ch>
2026-06-19 21:20:17 +0200

    Fix race condition affecting ``envmap.cpp`` and ``Hierarchical2D`` (LLVM only)
    When used in LLVM mode, Hierarhical2D allocated a Dr.Jit buffer and
    directly wrote to it from the host thread. That is not allowed since the
    buffer may still be used by concurrently running kernels. The fix is
    to allocate a ``shared=true`` buffer that legalizes this.

--- include/mitsuba/core/distr_2d.h
+++ include/mitsuba/core/distr_2d.h
@@ -768,7 +768,7 @@ protected:
             : size(dr::prod(res)), width(res.x()) {
 
             uint32_t n = size * slices;
-            if constexpr (dr::is_cuda_v<Float> || dr::is_metal_v<Float>) {
+            if constexpr (dr::is_jit_v<Float>) {
                 data = dr::map<FloatStorage>(
                     jit_malloc(dr::backend_v<Float>, n * sizeof(ScalarFloat),
                                /* shared = */ 1),
@@ -780,7 +780,7 @@ protected:
         }
 
         void ready() {
-            if constexpr (dr::is_cuda_v<Float> || dr::is_metal_v<Float>)
+            if constexpr (dr::is_jit_v<Float>)
                 data = dr::migrate(data, dr::backend_v<Float>);
         }
 
