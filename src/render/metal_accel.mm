2c7b66fff8492e448c045e29e1bbc59267e91594
Lukas Lipp <llipp@cg.tuwien.ac.at>
2026-08-01 16:03:39 +0200

    fix metal caps

--- src/render/metal_accel.mm
+++ src/render/metal_accel.mm
@@ -433,7 +433,9 @@ build_impl(const std::vector<BlasEntry> &blases,
                                                           ? MTLCurveBasisBSpline
                                                           : MTLCurveBasisLinear;
                         gd.curveType                = MTLCurveTypeRound;
-                        gd.curveEndCaps             = MTLCurveEndCapsSphere;
+                        gd.curveEndCaps             = bspline
+                                                          ? MTLCurveEndCapsNone
+                                                          : MTLCurveEndCapsSphere;
                         gd.opaque                   = YES;
                         [geoms addObject: gd];
 
