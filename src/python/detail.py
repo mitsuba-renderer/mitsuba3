41d8b239569102715e4ca105c826418608541164
Wenzel Jakob <wenzel.jakob@epfl.ch>
2026-08-10 22:36:03 +0200

    Move the mitsuba alias layer from C++ to Python
    This commit deletes the ``mitsuba_alias`` C++ extension and moves the
    entire mechanism into ``mitsuba/__init__.py``. There is no meaurable
    performance cost.

--- src/python/detail.py
+++ src/python/detail.py
@@ -1,5 +1,31 @@
 """Mitsuba detail module - internal implementation details."""
 
+import typing
+
+# Callables that ``mitsuba.set_variant()`` invokes after a variant change
+_variant_callbacks = set()
+
+
+def add_variant_callback(callback: typing.Callable[[typing.Optional[str], str], None]) -> None:
+    """
+    Register a callable that runs each time the Mitsuba variant changes. It
+    receives the arguments ``old_variant: Optional[str], new_variant: str``.
+    The callbacks form a set, registering the same callable several times has
+    no further effect.
+    """
+    _variant_callbacks.add(callback)
+
+
+def remove_variant_callback(callback: typing.Callable[[typing.Optional[str], str], None]) -> None:
+    """Remove the given callable from the set of variant change callbacks."""
+    _variant_callbacks.discard(callback)
+
+
+def clear_variant_callbacks() -> None:
+    """Remove all variant change callbacks."""
+    _variant_callbacks.clear()
+
+
 class TransformWrapper:
     '''
     Helper functor that wraps Transform3f/Transform4f methods so that the following two
